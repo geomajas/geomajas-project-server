f2afe442bb1e046b1f1d586310468e1e8755532b
Pieter De Graef <pieter.degraef@geosparc.com>
2010-02-01 14:03:07 +0000

    MAJ-650: further work on the experimental API.

---
+++ geomajas/backend/geomajas-api/src/main/java/org/geomajas/layer/tile/RasterTile.java
@@ -0,0 +1,61 @@
+/*
+ * This file is part of Geomajas, a component framework for building
+ * rich Internet applications (RIA) with sophisticated capabilities for the
+ * display, analysis and management of geographic information.
+ * It is a building block that allows developers to add maps
+ * and other geographic data capabilities to their web applications.
+ *
+ * Copyright 2008-2010 Geosparc, http://www.geosparc.com, Belgium
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.geomajas.layer.tile;
+
+/**
+ * DTO version of a {@link RasterTile}. This object can be sent to the client.
+ *
+ * @author Pieter De Graef
+ */
+public class RasterTile extends Tile {
+
+	private static final long serialVersionUID = 151L;
+
+	private RasterImage featureImage;
+
+	private RasterImage labelImage;
+
+	// Constructors:
+
+	public RasterTile() {
+	}
+
+	// Getters and setters:
+
+	public RasterImage getFeatureImage() {
+		return featureImage;
+	}
+
+	public void setFeatureImage(RasterImage featureImage) {
+		this.featureImage = featureImage;
+	}
+
+	public RasterImage getLabelImage() {
+		return labelImage;
+	}
+
+	public void setLabelImage(RasterImage labelImage) {
+		this.labelImage = labelImage;
+	}
+}
---
+++ geomajas/backend/geomajas-impl/src/main/java/org/geomajas/internal/rendering/style/StyleFilterImpl.java
@@ -0,0 +1,110 @@
+/*
+ * This file is part of Geomajas, a component framework for building
+ * rich Internet applications (RIA) with sophisticated capabilities for the
+ * display, analysis and management of geographic information.
+ * It is a building block that allows developers to add maps
+ * and other geographic data capabilities to their web applications.
+ *
+ * Copyright 2008-2010 Geosparc, http://www.geosparc.com, Belgium
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.geomajas.internal.rendering.style;
+
+import org.geomajas.configuration.StyleInfo;
+import org.geomajas.rendering.style.StyleFilter;
+import org.geotools.filter.text.cql2.CQL;
+import org.geotools.filter.text.cql2.CQLException;
+import org.opengis.filter.Filter;
+
+/**
+ * <p>
+ * Default implementation of the StyleFilter interface. Transforms a <code>LayerStyleDefinition</code> into a
+ * <code>StyleFilter</code>, by parsing the formula into a <code>Filter</code> object. Only feature that accept this
+ * filter should recieve the style contained in the style definition.
+ * </p>
+ *
+ * @author Pieter De Graef
+ */
+public class StyleFilterImpl implements StyleFilter {
+
+	/**
+	 * The opengis filter object, parsed from the <code>LayerStyleDefinition</code>'s formula.
+	 */
+	private Filter filter;
+
+	/**
+	 * The style definition object contains styling information, a name, an id, ...
+	 */
+	private StyleInfo definition;
+
+	// -------------------------------------------------------------------------
+	// Constructors:
+	// -------------------------------------------------------------------------
+
+	/**
+	 * The default constructor. Creates an invisible style, with the default ID. Try to avoid this.
+	 */
+	public StyleFilterImpl() {
+		this.filter = Filter.INCLUDE;
+		StyleInfo style = new StyleInfo();
+		style.setFillColor("#FFFFFF");
+		style.setFillOpacity(0);
+		style.setStrokeColor("#FFFFFF");
+		style.setStrokeOpacity(0);
+		style.setId(DEFAULT_STYLE_ID);
+		style.setName("invisible-style");
+		style.setFormula("");
+	}
+
+	/**
+	 * The recommended constructor. Takes a style definition object and tries to parse the formula to an opengis filter
+	 * object.
+	 *
+	 * @param definition
+	 *            The style definition.
+	 */
+	public StyleFilterImpl(StyleInfo definition) {
+		try {
+			String formula = definition.getFormula();
+			if (null != formula && formula.length() > 0) {
+				this.filter = CQL.toFilter(definition.getFormula());
+			} else {
+				this.filter = Filter.INCLUDE;
+			}
+		} catch (CQLException e) {
+			this.filter = Filter.INCLUDE;
+		}
+		this.definition = definition;
+	}
+
+	// -------------------------------------------------------------------------
+	// Getters:
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Return the styling definition.
+	 */
+	public StyleInfo getStyleDefinition() {
+		return definition;
+	}
+
+	/**
+	 * Return the filter object associated with the style.
+	 */
+	public Filter getFilter() {
+		return filter;
+	}
+}
