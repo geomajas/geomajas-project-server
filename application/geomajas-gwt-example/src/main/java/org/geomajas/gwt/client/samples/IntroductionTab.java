0402f165c1ebafffecfd4075030beda41a18699a
Pieter De Graef <pieter.degraef@geosparc.com>
2010-02-10 13:51:31 +0000

    MAJ-583: Geomajas sample test case application - initial commit (part 2??)

---
+++ application/geomajas-gwt-example/src/main/java/org/geomajas/gwt/client/samples/IntroductionTab.java
@@ -0,0 +1,72 @@
+/*
+ * This file is part of Geomajas, a component framework for building
+ * rich Internet applications (RIA) with sophisticated capabilities for the
+ * display, analysis and management of geographic information.
+ * It is a building block that allows developers to add maps
+ * and other geographic data capabilities to their web applications.
+ *
+ * Copyright 2008-2010 Geosparc, http://www.geosparc.com, Belgium
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.geomajas.gwt.client.samples;
+
+import org.geomajas.gwt.client.samples.base.SamplePanel;
+
+import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.HTMLPane;
+import com.smartgwt.client.widgets.Img;
+import com.smartgwt.client.widgets.layout.VLayout;
+
+/**
+ * <p>
+ * First test.....not really used....
+ * </p>
+ * 
+ * @author Pieter De Graef
+ */
+public class IntroductionTab extends SamplePanel {
+
+	public IntroductionTab() {
+		super("Introduction", "Introduction", "/images/geomajas_favicon.jpg");
+	}
+
+	public Canvas getViewPanel() {
+		VLayout layout = new VLayout();
+		layout.setWidth100();
+		layout.setHeight100();
+		
+		Img logo = new Img("[ISOMORPHIC]/geomajas/temp/geomajas_logo.png");
+		logo.setWidth(400);
+		logo.setLayoutAlign(Alignment.CENTER);
+		
+		HTMLPane pane = new HTMLPane();
+		pane.setContentsURL("/pages/list.html");
+		
+		layout.addMember(logo);
+		layout.addMember(pane);
+		
+		return layout;
+	}
+
+	public String getDescription() {
+		return null;
+	}
+
+	public String[] getConfigurationFiles() {
+		return null;
+	}
+}
---
+++ application/geomajas-gwt-example/src/main/java/org/geomajas/gwt/client/samples/OpenStreetMapSample.java
@@ -0,0 +1,74 @@
+/*
+ * This file is part of Geomajas, a component framework for building
+ * rich Internet applications (RIA) with sophisticated capabilities for the
+ * display, analysis and management of geographic information.
+ * It is a building block that allows developers to add maps
+ * and other geographic data capabilities to their web applications.
+ *
+ * Copyright 2008-2010 Geosparc, http://www.geosparc.com, Belgium
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.geomajas.gwt.client.samples;
+
+import org.geomajas.gwt.client.controller.PanController;
+import org.geomajas.gwt.client.samples.base.SamplePanel;
+import org.geomajas.gwt.client.samples.i18n.I18nProvider;
+import org.geomajas.gwt.client.widget.MapWidget;
+
+import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.layout.VLayout;
+
+/**
+ * <p>
+ * Sample that shows a map with an OpenStreetMap layer.
+ * </p>
+ * 
+ * @author Pieter De Graef
+ */
+public class OpenStreetMapSample extends SamplePanel {
+
+	private MapWidget map;
+
+	public OpenStreetMapSample() {
+		super("OSM", I18nProvider.getSampleMessages().osmTitle(), "/images/application_view_list.png");
+	}
+
+	public Canvas getViewPanel() {
+		VLayout layout = new VLayout();
+		layout.setWidth100();
+		layout.setHeight100();
+		
+		// Map with ID osmMap is defined in the XML configuration. (mapOsm.xml)
+		map = new MapWidget("osmMap");
+		
+		// Set a panning controller on the map:
+		map.setController(new PanController(map));
+		layout.addMember(map);
+
+		// Specific to these samples; calls the MapWidget.initialize method when ready. 
+		registerMap(map); 
+		return layout;
+	}
+
+	public String getDescription() {
+		return I18nProvider.getSampleMessages().osmDescription();
+	}
+
+	public String[] getConfigurationFiles() {
+		return new String[] { "/org/geomajas/gwt/samples/mapwidget/layerOsm.xml",
+				"/org/geomajas/gwt/samples/mapwidget/mapOsm.xml" };
+	}
+}
