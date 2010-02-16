8ee32c7361923aa24be7b25ba5049ccee43dc1b5
Joachim Van der Auwera <joachim@progs.be>
2010-02-16 15:36:01 +0000

    MAJ-645 apply security, test skeleton

---
+++ test/geomajas-test-integration/src/test/java/org/geomajas/test/security/VectorLayerServiceTest.java
@@ -0,0 +1,147 @@
+/*
+ * This file is part of Geomajas, a component framework for building
+ * rich Internet applications (RIA) with sophisticated capabilities for the
+ * display, analysis and management of geographic information.
+ * It is a building block that allows developers to add maps
+ * and other geographic data capabilities to their web applications.
+ *
+ * Copyright 2008-2010 Geosparc, http://www.geosparc.com, Belgium
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.geomajas.test.security;
+
+import junit.framework.Assert;
+import org.geomajas.command.CommandDispatcher;
+import org.geomajas.command.CommandResponse;
+import org.geomajas.plugin.springsecurity.command.dto.LoginRequest;
+import org.geomajas.plugin.springsecurity.command.dto.LoginResponse;
+import org.geomajas.security.SecurityManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Tests for proper application of security in {@link org.geomajas.test.security.VectorLayerServiceTest}.
+ *
+ * @author Joachim Van der Auwera
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/org/geomajas/spring/geomajasContext.xml",
+		"/org/geomajas/test/security/vectorLayerSecurity.xml"})
+public class VectorLayerServiceTest {
+
+	private static final String LAYER_ID = "beans";
+	private static final String STRING_ATTR = "stringAttr";
+	private static final double ALLOWANCE = .00000001;
+
+	@Autowired
+	private SecurityManager securityManager;
+
+	@Autowired
+	private CommandDispatcher commandDispatcher;
+
+	// assure we are logged in as a specific user to set correct authorizations
+	public void login(String name) {
+		LoginRequest request = new LoginRequest();
+		request.setLogin(name);
+		request.setPassword(name);
+		CommandResponse response = commandDispatcher.execute("command.Login", request, null, "en");
+		Assert.assertFalse(response.isError());
+		Assert.assertTrue(response instanceof LoginResponse);
+		securityManager.createSecurityContext(((LoginResponse)response).getToken());
+	}
+
+	@Test
+	public void testGetFeaturesInvisibleLayer() {
+		// verify features are accessible when layer is visible
+		login("luc");
+		// @todo
+
+		// verify features are not accessible when layer is invisible
+		login("marino");
+		// @todo
+	}
+
+	/*
+	@Test
+	public void testGetFeaturesSecurityFilter() {
+		login("");
+		// @todo
+	}
+
+	@Test
+	public void testGetFeaturesVisibleArea() {
+		login("");
+		// @todo
+	}
+
+	@Test
+	public void testGetFeaturesFeatureAuthorization() {
+		login("");
+		// @todo
+	}
+
+	@Test
+	public void testGetFeaturesAttributeAuthorization() {
+		login("");
+		// @todo
+	}
+
+	@Test
+	public void testGetBoundsVisibleArea() {
+		login("");
+		// @todo
+	}
+
+	@Test
+	public void testSaveOrUpdateAreaWritable() {
+		login("");
+		// @todo
+	}
+
+	@Test
+	public void testSaveOrUpdateFeatureWritable() {
+		login("");
+		// @todo
+	}
+
+	@Test
+	public void testSaveOrUpdateAttributeWritable() {
+		login("");
+		// @todo
+	}
+
+	@Test
+	public void testSaveOrUpdateCreateDeleteArea() {
+		login("");
+		// @todo
+	}
+
+	@Test
+	public void testSaveOrUpdateCreateDeleteFeature() {
+		login("");
+		// @todo
+	}
+
+	@Test
+	public void testSaveOrUpdateCreateDeleteAttribute() {
+		login("");
+		// @todo
+	}
+	*/
+}
