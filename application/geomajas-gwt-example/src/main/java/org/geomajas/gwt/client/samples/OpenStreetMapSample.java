06c38330ba7912702569d9fa5152a9cf98b774ff
Jan De Moerloose <jan.demoerloose@geosparc.com>
2010-02-16 09:31:45 +0000

    MAJ-654: client and server configuration split up, gwt samples

--- application/geomajas-gwt-example/src/main/java/org/geomajas/gwt/client/samples/OpenStreetMapSample.java
+++ application/geomajas-gwt-example/src/main/java/org/geomajas/gwt/client/samples/OpenStreetMapSample.java
@@ -52,7 +52,7 @@ public class OpenStreetMapSample extends SamplePanel {
 		layout.setHeight100();
 		
 		// Map with ID osmMap is defined in the XML configuration. (mapOsm.xml)
-		map = new MapWidget("osmMap","gwt-samples");
+		map = new MapWidget("osmMap", "gwt-samples");
 
 		// Set a panning controller on the map:
 		map.setController(new PanController(map));
