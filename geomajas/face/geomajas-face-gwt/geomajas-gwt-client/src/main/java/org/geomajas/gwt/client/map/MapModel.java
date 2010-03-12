03205bcbb2986a0f91b486780af3c86f8c45310c
Pieter De Graef <pieter.degraef@geosparc.com>
2010-03-12 11:33:47 +0000

    Fix for MAJ-797:  Initial bounds on a map can be larger than the maximum bounds the user is allowed to see.

--- geomajas/face/geomajas-face-gwt/geomajas-gwt-client/src/main/java/org/geomajas/gwt/client/map/MapModel.java
+++ geomajas/face/geomajas-face-gwt/geomajas-gwt-client/src/main/java/org/geomajas/gwt/client/map/MapModel.java
@@ -222,7 +222,6 @@ public class MapModel implements Paintable, MapViewChangedHandler, HasFeatureSel
 		mapView.setMaximumScale(mapInfo.getMaximumScale());
 		Bbox initialBounds = new Bbox(mapInfo.getInitialBounds().getX(), mapInfo.getInitialBounds().getY(), mapInfo
 				.getInitialBounds().getWidth(), mapInfo.getInitialBounds().getHeight());
-		mapView.applyBounds(initialBounds, MapView.ZoomOption.LEVEL_CLOSEST);
 		removeAllLayers();
 		Bbox maxBounds = new Bbox(initialBounds);
 		for (ClientLayerInfo layerInfo : mapInfo.getLayers()) {
@@ -230,6 +229,7 @@ public class MapModel implements Paintable, MapViewChangedHandler, HasFeatureSel
 			maxBounds = maxBounds.union(new Bbox(layerInfo.getMaxExtent()));
 		}
 		mapView.setMaxBounds(maxBounds);
+		mapView.applyBounds(initialBounds, MapView.ZoomOption.LEVEL_CLOSEST);
 		handlerManager.fireEvent(new MapModelEvent());
 		initialized = true;
 	}
