16370ba2fbc14800ee1b8400a8189ccc8aa6f786
Jan De Moerloose <jan.demoerloose@geosparc.com>
2010-06-21 11:32:31 +0000

    SPRINT-4: upgrade printing backend to 1.7.0

--- plugin/geomajas-plugin-printing/geomajas-plugin-printing/src/test/java/org/geomajas/extension/printing/document/DefaultDocumentTest.java
+++ plugin/geomajas-plugin-printing/geomajas-plugin-printing/src/test/java/org/geomajas/extension/printing/document/DefaultDocumentTest.java
@@ -22,47 +22,54 @@
  */
 package org.geomajas.extension.printing.document;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
-import java.io.StringReader;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-
-import org.geomajas.extension.printing.component.PageComponent;
-import org.geomajas.extension.printing.configuration.DefaultConfigurationVisitor;
 import org.geomajas.layer.VectorLayerService;
+import org.geomajas.plugin.printing.configuration.DefaultConfigurationVisitor;
+import org.geomajas.plugin.printing.document.DefaultDocument;
+import org.geomajas.security.SecurityManager;
 import org.geomajas.service.ConfigurationService;
 import org.geomajas.service.FilterService;
 import org.geomajas.service.GeoService;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { 
+		"/org/geomajas/spring/geomajasContext.xml",
+		"/org/geomajas/plugin/printing/printing.xml",
+		"/org/geomajas/testdata/layerBluemarble.xml",
+		"/org/geomajas/testdata/layerCountries.xml",
+		"/org/geomajas/testdata/simplemixedContext.xml"})
+@Transactional(rollbackFor = {org.geomajas.global.GeomajasException.class})
 public class DefaultDocumentTest {
 
+	@Autowired
 	private ConfigurationService runtime;
 
+	@Autowired
 	private GeoService geoService;
 
+	@Autowired
 	private FilterService filterCreator;
 
+	@Autowired
 	private VectorLayerService layerService;
 
+	@Autowired
+	private SecurityManager securityManager;
+
 	@Before
-	public void setUp() throws Exception {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[] {
-				"org/geomajas/spring/geomajasContext.xml", "org/geomajas/testdata/layerBluemarble.xml",
-				"org/geomajas/testdata/layerCountries.xml", "org/geomajas/testdata/simplemixedContext.xml" });
-		// load the configuration (context-wide object);
-		runtime = applicationContext.getBean("service.ConfigurationService", ConfigurationService.class);
-		geoService = applicationContext.getBean("service.GeoService", GeoService.class);
-		filterCreator = applicationContext.getBean("service.FilterService", FilterService.class);
-		layerService = applicationContext.getBean("layer.VectorLayerService", VectorLayerService.class);
+	public void login() {
+		// assure security context is set
+		securityManager.createSecurityContext(null);
 	}
 
 	@Test
@@ -70,55 +77,12 @@ public class DefaultDocumentTest {
 		DefaultDocument document = new DefaultDocument("A4", runtime, null, getDefaultVisitor(-31.44, -37.43, 80.83f),
 				geoService, filterCreator, layerService);
 		document.render();
-
-		JAXBContext context = JAXBContext.newInstance("org.geomajas.extension.printing.component", getClass()
-				.getClassLoader());
-		Marshaller m = context.createMarshaller();
-		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		m.marshal(document.getPage(), baos);
-		baos.flush();
-		Unmarshaller u = context.createUnmarshaller();
-		Object result = u.unmarshal(new StringReader(baos.toString()));
-		PageComponent pageCopy = (PageComponent) result;
-
-		SinglePageDocument doc = new SinglePageDocument(pageCopy, runtime, null);
-		doc.render();
 		FileOutputStream fo = new FileOutputStream("target/test.pdf");
-		doc.getOutputStream().writeTo(fo);
+		document.getOutputStream().writeTo(fo);
 		fo.flush();
 		fo.close();
 	}
 
-	/*
-	 * @Test public void testWrite() throws Exception { PageComponent page = new PageComponent("A4", true);
-	 * page.setTag(PrintTemplate.PAGE); MapComponent map = createMap(-570765.9534524073, 5067494.962772554, 2000000);
-	 * ImageComponent northarrow = createArrow(); ScaleBarComponent bar = createBar(); LabelComponent title =
-	 * createTitle();
-	 * 
-	 * LegendComponent legend = new LegendComponent(); map.addComponent(bar); map.addComponent(legend);
-	 * map.addComponent(northarrow); page.addComponent(map); page.addComponent(title);
-	 * 
-	 * HibernateUtil.getInstance().beginTransaction(); if (HibernateTransactionInterceptor.isHibernateEnabled()) {
-	 * PrintTemplateDAO dao = PrintConfiguration.getDAO();
-	 * 
-	 * PrintTemplate template = new PrintTemplate(); template.setDataSourceName("Viewports1"); template.setPage(page);
-	 * template.encode(); List<String> names = dao.findAllNames(); // dao.makePersistent(template); template =
-	 * PrintTemplate.createDefaultTemplate("A4"); template.setDataSourceName("Default"); template.encode(); //
-	 * dao.makePersistent(template); HibernateUtil.getInstance().commitTransaction();
-	 * HibernateUtil.getInstance().getCurrentSession().close(); } }
-	 */
-
-	/*
-	 * @Test public void testRead() throws Exception { Application app = createApplication();
-	 * HibernateUtil.getInstance().beginTransaction(); if (HibernateTransactionInterceptor.isHibernateEnabled()) {
-	 * PrintTemplateDAO dao = PrintConfiguration.getDAO(); List<PrintTemplate> templates = dao.findAll();
-	 * 
-	 * for (PrintTemplate template : templates) { // decode the page template.decode(); // calculate the sizes (if not
-	 * already calculated !) SinglePageDocument document = new SinglePageDocument(template.getPage(), app, null);
-	 * document.setLayoutOnly(true); document.render(); } HibernateUtil.getInstance().commitTransaction();
-	 * HibernateUtil.getInstance().getCurrentSession().close(); } }
-	 */
 
 	private DefaultConfigurationVisitor getDefaultVisitor(double x, double y, float widthInUnits) {
 		// 842, 595
---
+++ plugin/geomajas-plugin-printing/geomajas-plugin-printing/src/test/java/org/geomajas/extension/printing/service/PrintServiceImplTest.java
@@ -0,0 +1,45 @@
+package org.geomajas.extension.printing.service;
+
+import java.util.List;
+
+import org.geomajas.plugin.printing.configuration.PrintTemplate;
+import org.geomajas.plugin.printing.service.PrintService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vividsolutions.jts.util.Assert;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/org/geomajas/spring/geomajasContext.xml",
+		"/org/geomajas/plugin/printing/printing.xml" })
+@Transactional(rollbackFor = {org.geomajas.global.GeomajasException.class})
+public class PrintServiceImplTest {
+
+	@Autowired
+	private PrintService printService;
+		
+	private final Logger log = LoggerFactory.getLogger(PrintServiceImplTest.class);
+
+	@Before
+	public void loadTemplates() throws Exception{
+		PrintTemplate template = PrintTemplate.createDefaultTemplate("A4", true);
+		template.setName("test");
+		printService.saveOrUpdateTemplate(template);
+		log.info(template.getPageXml());
+		
+	}
+
+	@Test
+	public void testGetAllTemplates() throws Exception {
+		List<PrintTemplate> templates = printService.getAllTemplates();
+		Assert.equals(11, templates.size());
+	}
+
+}
