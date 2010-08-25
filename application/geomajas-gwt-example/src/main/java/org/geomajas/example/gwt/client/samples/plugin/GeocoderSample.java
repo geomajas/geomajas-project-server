0066d5eb3d75be0185d664e1620ebec49741e556
Joachim Van der Auwera <joachim@progs.be>
2010-08-25 17:30:02 +0000

    GWTSHOW-21 select which service to use

--- application/geomajas-gwt-example/src/main/java/org/geomajas/example/gwt/client/samples/plugin/GeocoderSample.java
+++ application/geomajas-gwt-example/src/main/java/org/geomajas/example/gwt/client/samples/plugin/GeocoderSample.java
@@ -23,7 +23,15 @@
 
 package org.geomajas.example.gwt.client.samples.plugin;
 
+import com.smartgwt.client.data.DataSource;
+import com.smartgwt.client.data.DataSourceField;
+import com.smartgwt.client.data.Record;
+import com.smartgwt.client.data.fields.DataSourceTextField;
 import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.form.DynamicForm;
+import com.smartgwt.client.widgets.form.fields.SelectItem;
+import com.smartgwt.client.widgets.form.fields.events.ChangeEvent;
+import com.smartgwt.client.widgets.form.fields.events.ChangeHandler;
 import com.smartgwt.client.widgets.layout.VLayout;
 import org.geomajas.example.gwt.client.samples.base.SamplePanel;
 import org.geomajas.example.gwt.client.samples.base.SamplePanelFactory;
@@ -34,13 +42,16 @@ import org.geomajas.plugin.geocoder.client.GeocoderWidget;
 
 /**
  * Sample to demonstrate default printing action.
- * 
+ *
  * @author Jan De Moerloose
- * 
+ *
  */
 public class GeocoderSample extends SamplePanel {
 
 	public static final String TITLE = "Geocoder";
+
+	private static final String FIELD_LABEL = "label";
+	private static final String FIELD_REGEX = "regex";
 
 	public static final SamplePanelFactory FACTORY = new SamplePanelFactory() {
 
@@ -61,12 +72,49 @@ public class GeocoderSample extends SamplePanel {
 
 		final Toolbar toolbar = new Toolbar(map);
 		toolbar.setButtonSize(Toolbar.BUTTON_SIZE_BIG);
-		GeocoderWidget geocoderWidget = new GeocoderWidget(map, "description", "Geocoder");
+		final GeocoderWidget geocoderWidget = new GeocoderWidget(map, "description", "Geocoder");
 		toolbar.addMember(geocoderWidget);
+
+		SelectItem geocoderSource = new SelectItem();
+		geocoderSource.setOptionDataSource(getGeocoderSelectDataSource());
+		geocoderSource.setDisplayField(FIELD_LABEL);
+		geocoderSource.setValueField(FIELD_REGEX);
+		geocoderSource.addChangeHandler(new ChangeHandler() {
+			public void onChange(ChangeEvent event) {
+				geocoderWidget.setServicePattern((String) event.getValue());
+			}
+		});
+		DynamicForm geocoderSourceForm = new DynamicForm();
+		geocoderSourceForm.setFields(geocoderSource);
+		toolbar.addMember(geocoderSourceForm);
 
 		layout.addMember(toolbar);
 		layout.addMember(map);
 		return layout;
+	}
+
+	private DataSource getGeocoderSelectDataSource() {
+		DataSource dataSource = new DataSource();
+
+		DataSourceField label = new DataSourceTextField(FIELD_LABEL);
+		DataSourceField regex = new DataSourceTextField(FIELD_REGEX);
+		dataSource.setFields(label, regex);
+
+		Record record;
+		record = new Record();
+		record.setAttribute(FIELD_LABEL, "Yahoo! PlaceFinder");
+		record.setAttribute(FIELD_REGEX, "yahoo");
+		dataSource.addData(new Record());
+		record = new Record();
+		record.setAttribute(FIELD_LABEL, "GeoNames");
+		record.setAttribute(FIELD_REGEX, "geonames");
+		dataSource.addData(new Record());
+		record = new Record();
+		record.setAttribute(FIELD_LABEL, "offline");
+		record.setAttribute(FIELD_REGEX, "offline");
+		dataSource.addData(new Record());
+
+		return dataSource;
 	}
 
 	public String getDescription() {
@@ -78,9 +126,9 @@ public class GeocoderSample extends SamplePanel {
 	}
 
 	public String[] getConfigurationFiles() {
-		return new String[] { "classpath:org/geomajas/example/gwt/clientcfg/geocoder.xml",
+		return new String[]{"classpath:org/geomajas/example/gwt/clientcfg/geocoder.xml",
 				"classpath:org/geomajas/example/gwt/servercfg/raster/layerOsm.xml",
-				"classpath:org/geomajas/example/gwt/clientcfg/layer/mapOsm.xml" };
+				"classpath:org/geomajas/example/gwt/clientcfg/layer/mapOsm.xml"};
 	}
 
 	public String ensureUserLoggedIn() {
