4e3dfc9712824d9136ebb77ea15aba5f9e85e454
Joachim Van der Auwera <joachim@progs.be>
2010-10-08 06:36:53 +0000

    CACHE-3 more tests

---
+++ plugin/geomajas-plugin-caching/caching/src/test/java/org/geomajas/plugin/caching/step/GetFeaturesTest.java
@@ -0,0 +1,105 @@
+/*
+ * This file is part of Geomajas, a component framework for building
+ * rich Internet applications (RIA) with sophisticated capabilities for the
+ * display, analysis and management of geographic information.
+ * It is a building block that allows developers to add maps
+ * and other geographic data capabilities to their web applications.
+ *
+ * Copyright 2008-2010 Geosparc, http://www.geosparc.com, Belgium
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.geomajas.plugin.caching.step;
+
+import com.vividsolutions.jts.geom.Envelope;
+import org.geomajas.global.GeomajasConstant;
+import org.geomajas.layer.VectorLayerService;
+import org.geomajas.layer.feature.InternalFeature;
+import org.geomajas.plugin.caching.service.CacheCategory;
+import org.geomajas.plugin.caching.service.CacheContext;
+import org.geomajas.plugin.caching.service.CacheManagerServiceImpl;
+import org.geomajas.plugin.caching.service.DummyCacheService;
+import org.geomajas.service.GeoService;
+import org.geomajas.service.TestRecorder;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * Tests for the cached variant of the GetBounds pipeline.
+ *
+ * @author Joachim Van der Auwera
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/org/geomajas/spring/geomajasContext.xml",
+		"/META-INF/geomajasContext.xml", "/org/geomajas/plugin/caching/DefaultCachedPipelines.xml",
+		"/pipelineContext.xml", "/org/geomajas/testdata/layerBeans.xml", "/org/geomajas/spring/testRecorder.xml"})
+public class GetFeaturesTest {
+
+	private static final String LAYER_BEANS = "beans";
+	private static final double DELTA = 1e-10;
+
+	@Autowired
+	private TestRecorder recorder;
+
+	@Autowired
+	private CacheManagerServiceImpl cacheManager;
+
+	@Autowired
+	private VectorLayerService vectorLayerService;
+
+	@Autowired
+	private GeoService geoService;
+
+	@Autowired
+	private org.geomajas.security.SecurityManager securityManager;
+
+	@Test
+	public void testFeatures() throws Exception {
+		securityManager.createSecurityContext(null); // assure a security context exists for this thread
+		List<InternalFeature> features;
+
+		// first run, this should put things in the cache
+		recorder.clear();
+		features = vectorLayerService.getFeatures(LAYER_BEANS, geoService.getCrs("EPSG:4326"), null, null,
+				GeomajasConstant.FEATURE_INCLUDE_NONE);
+		Assert.assertNotNull(features);
+		Assert.assertEquals(3, features.size());
+		Assert.assertEquals("", recorder.matches(CacheCategory.FEATURE,
+				"Put item in cache"));
+
+		// verify that data is in the cache
+		DummyCacheService cache = (DummyCacheService)cacheManager.getCacheForTesting(LAYER_BEANS, CacheCategory.FEATURE);
+		Assert.assertEquals(1, cache.size());
+		String key = cache.getKey();
+		FeaturesCacheContainer bcc = (FeaturesCacheContainer) cache.getObject();
+		bcc.getFeatures().remove(2);
+
+		// get bounds again, the result should be different because we changed the cached value
+		recorder.clear();
+		features = vectorLayerService.getFeatures(LAYER_BEANS, geoService.getCrs("EPSG:4326"), null, null,
+				GeomajasConstant.FEATURE_INCLUDE_NONE);
+		Assert.assertNotNull(features);
+		Assert.assertEquals(2, features.size());
+		Assert.assertEquals("", recorder.matches(CacheCategory.FEATURE,
+				"Got item from cache",
+				"Put item in cache"));
+	}
+}
