8100144645f39e1564439d6c79467204e17d61c2
Joachim Van der Auwera <joachim@progs.be>
2010-10-15 08:31:30 +0000

    CACHE-3 hook into layer invalidation

---
+++ plugin/geomajas-plugin-caching/caching/src/test/java/org/geomajas/plugin/caching/service/LayerInvalidateTest.java
@@ -0,0 +1,60 @@
+/*
+ * This file is part of Geomajas, a component framework for building
+ * rich Internet applications (RIA) with sophisticated capabilities for the
+ * display, analysis and management of geographic information.
+ * It is a building block that allows developers to add maps
+ * and other geographic data capabilities to their web applications.
+ *
+ * Copyright 2008-2010 Geosparc, http://www.geosparc.com, Belgium
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.geomajas.plugin.caching.service;
+
+import junit.framework.Assert;
+import org.geomajas.service.ConfigurationService;
+import org.geomajas.service.TestRecorder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Check that layer invalidation test is properly called.
+ *
+ * @author Joachim Van der Auwera
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/org/geomajas/spring/geomajasContext.xml",
+		"/org/geomajas/testdata/layerBeans.xml", "/org/geomajas/spring/testRecorder.xml"})
+public class LayerInvalidateTest {
+
+	private static final String LAYER_BEANS = "beans";
+
+	@Autowired
+	private ConfigurationService configurationService;
+
+	@Autowired
+	private TestRecorder recorder;
+
+	@Test
+	public void invalidateTest() throws Exception {
+		recorder.clear();
+		configurationService.invalidateLayer(LAYER_BEANS);
+		Assert.assertEquals("", recorder.matches(LAYER_BEANS, "Layer invalidated"));
+	}
+
+}
