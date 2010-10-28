97558ad32f0e5d902407ca8d1f4b09ccbd8ec1f3
Pieter De Graef <pieter.degraef@geosparc.com>
2010-10-28 13:02:46 +0000

    GWT-151: smartGWT scripts are not cached but completely reloaded each time

--- backend/common-servlet/src/main/java/org/geomajas/servlet/CacheFilter.java
+++ backend/common-servlet/src/main/java/org/geomajas/servlet/CacheFilter.java
@@ -1,15 +1,41 @@
+/*
+ * This file is part of Geomajas, a component framework for building
+ * rich Internet applications (RIA) with sophisticated capabilities for the
+ * display, analysis and management of geographic information.
+ * It is a building block that allows developers to add maps
+ * and other geographic data capabilities to their web applications.
+ *
+ * Copyright 2008-2010 Geosparc, http://www.geosparc.com, Belgium
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.geomajas.servlet;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 
 /**
  * {@link Filter} to add cache control headers for caching all files comply to one of the following:
@@ -40,7 +66,7 @@ public class CacheFilter implements Filter {
 
 	private final String[] noCache = new String[] { ".nocache." };
 
-	private final String[] toCache = new String[] { ".cache.", "ISC_", ".png", ".jpg", ".gif" };
+	private final String[] toCache = new String[] { ".cache.", ".js", ".png", ".jpg", ".gif", ".css", ".html" };
 
 	// ------------------------------------------------------------------------
 	// Filter implementation:
@@ -54,14 +80,19 @@ public class CacheFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException,
 			ServletException {
-		String requestUri = ((HttpServletRequest) request).getRequestURI();
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
 
 		boolean notCached = false;
+		boolean tryToCompress = false;
+		String requestUri = httpRequest.getRequestURI();
+
 		for (String noc : noCache) {
 			// Should we set the "no-cache" headers?
 			if (requestUri.contains(noc)) {
-				configureNoCaching((HttpServletResponse) response);
+				configureNoCaching(httpResponse);
 				notCached = true;
+				tryToCompress = true;
 				continue;
 			}
 		}
@@ -71,13 +102,25 @@ public class CacheFilter implements Filter {
 			for (String c : toCache) {
 				// Should we set the "cache" headers?
 				if (requestUri.contains(c)) {
-					configureCaching((HttpServletResponse) response);
+					configureCaching(httpResponse);
+					tryToCompress = true;
 					continue;
 				}
 			}
 		}
 
-		filterChain.doFilter(request, response);
+		// Check if the file needs compression:
+		String encodings = httpRequest.getHeader("Accept-Encoding");
+		if (tryToCompress && encodings != null && encodings.indexOf("gzip") != -1) {
+			GzipServletResponseWrapper responseWrapper = new GzipServletResponseWrapper(httpResponse);
+			try {
+				filterChain.doFilter(request, responseWrapper);
+			} finally {
+				responseWrapper.finish();
+			}
+		} else {
+			filterChain.doFilter(request, response);
+		}
 	}
 
 	// ------------------------------------------------------------------------
@@ -105,5 +148,111 @@ public class CacheFilter implements Filter {
 
 		// HTTP 1.1 header
 		response.setHeader(HTTP_CACHE_CONTROL_HEADER, "max-age=" + CACHE_DURATION_IN_SECOND);
+	}
+
+	// ------------------------------------------------------------------------
+	// Private class GzipResponseStream (stolen from the ResourceServlet)
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Assure resources are gzip compressed.
+	 * 
+	 * @author Pieter De Graef
+	 */
+	private class GzipResponseStream extends ServletOutputStream {
+
+		private ByteArrayOutputStream byteStream;
+
+		private GZIPOutputStream gzipStream;
+
+		private boolean closed;
+
+		private HttpServletResponse response;
+
+		private ServletOutputStream servletStream;
+
+		public GzipResponseStream(HttpServletResponse response) throws IOException {
+			super();
+			closed = false;
+			this.response = response;
+			this.servletStream = response.getOutputStream();
+			byteStream = new ByteArrayOutputStream();
+			gzipStream = new GZIPOutputStream(byteStream);
+		}
+
+		public void close() throws IOException {
+			if (closed) {
+				throw new IOException("This output stream has already been closed");
+			}
+			gzipStream.finish();
+
+			byte[] bytes = byteStream.toByteArray();
+
+			response.setContentLength(bytes.length);
+			response.addHeader("Content-Encoding", "gzip");
+			servletStream.write(bytes);
+			servletStream.flush();
+			servletStream.close();
+			closed = true;
+		}
+
+		public void flush() throws IOException {
+			if (closed) {
+				throw new IOException("Cannot flush a closed output stream");
+			}
+			gzipStream.flush();
+		}
+
+		public void write(int b) throws IOException {
+			if (closed) {
+				throw new IOException("Cannot write to a closed output stream");
+			}
+			gzipStream.write((byte) b);
+		}
+
+		public void write(byte[] b) throws IOException {
+			write(b, 0, b.length);
+		}
+
+		public void write(byte[] b, int off, int len) throws IOException {
+			if (closed) {
+				throw new IOException("Cannot write to a closed output stream");
+			}
+			gzipStream.write(b, off, len);
+		}
+	}
+
+	// ------------------------------------------------------------------------
+	// Private class GzipServletResponseWrapper
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Wrapper around a response that uses a GZIP stream.
+	 * 
+	 * @author Pieter De Graef
+	 */
+	private class GzipServletResponseWrapper extends HttpServletResponseWrapper {
+
+		private ServletOutputStream stream;
+
+		public GzipServletResponseWrapper(HttpServletResponse response) {
+			super(response);
+		}
+
+		public void finish() {
+			if (stream != null) {
+				try {
+					stream.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+
+		public ServletOutputStream getOutputStream() throws IOException {
+			if (stream == null) {
+				stream = new GzipResponseStream((HttpServletResponse) getResponse());
+			}
+			return stream;
+		}
 	}
 }
