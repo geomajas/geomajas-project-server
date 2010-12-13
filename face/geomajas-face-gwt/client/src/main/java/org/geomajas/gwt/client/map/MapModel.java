cfb832829d11947ee3662d6bbb218cb6de861bc3
Jan De Moerloose <jan.demoerloose@geosparc.com>
2010-12-13 11:39:21 +0000

    GWT-168: forgot to add layers when maxbounds was set !

--- face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/map/MapModel.java
+++ face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/map/MapModel.java
@@ -270,13 +270,16 @@ public class MapModel implements Paintable, MapViewChangedHandler, HasFeatureSel
 			}
 			mapView.setResolutions(realResolutions);
 			mapView.setMaximumScale(scaleConfigurationInfo.getMaximumScale().getPixelPerUnit());
+			// replace layers by new layers
 			removeAllLayers();
+			for (ClientLayerInfo layerInfo : mapInfo.getLayers()) {
+				addLayer(layerInfo);
+			}
 			Bbox maxBounds = new Bbox(mapInfo.getMaxBounds());
 			Bbox initialBounds = new Bbox(mapInfo.getInitialBounds());
 			// if the max bounds was not configured, take the union of initial and layer bounds
 			if (maxBounds.isAll()) {
 				for (ClientLayerInfo layerInfo : mapInfo.getLayers()) {
-					addLayer(layerInfo);
 					maxBounds = (Bbox) initialBounds.clone();
 					maxBounds = maxBounds.union(new Bbox(layerInfo.getMaxExtent()));
 				}
