a18d475a06d20978bfc819299445af4685e062ea
Joachim Van der Auwera <joachim@progs.be>
2011-01-12 21:31:21 +0000

    MAj-1054 update copyright/license text, dojo stuff

--- face/geomajas-face-dojo/client/src/main/resources/geomajas/map/print/nls/nl/printTemplateManager.js
+++ face/geomajas-face-dojo/client/src/main/resources/geomajas/map/print/nls/nl/printTemplateManager.js
@@ -1,4 +1,15 @@
 /*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+/*
  *     This software project is called Geomajas, a component framework for building
  *     rich Internet applications (RIA) with sophisticated capabilities for the
  *     display, analysis and management of geographic information.
