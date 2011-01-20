d56505427e280f4ae0db3f435feee3da4391f369
Pieter De Graef <pieter.degraef@geosparc.com>
2011-01-20 08:33:19 +0000

    Updating the GWT archetype to make it better looking

--- build-tools/geomajas-gwt-archetype/src/main/resources/archetype-resources/src/main/java/client/Application.java
+++ build-tools/geomajas-gwt-archetype/src/main/resources/archetype-resources/src/main/java/client/Application.java
@@ -14,33 +14,23 @@
 
 package ${package}.client;
 
+import ${package}.client.gui.SearchPanel;
+import org.geomajas.gwt.client.gfx.style.ShapeStyle;
+import org.geomajas.gwt.client.map.event.MapModelEvent;
+import org.geomajas.gwt.client.map.event.MapModelHandler;
+import org.geomajas.gwt.client.widget.LayerTree;
+import org.geomajas.gwt.client.widget.Legend;
+import org.geomajas.gwt.client.widget.MapWidget;
+import org.geomajas.gwt.client.widget.OverviewMap;
+import org.geomajas.gwt.client.widget.Toolbar;
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.smartgwt.client.types.Side;
 import com.smartgwt.client.types.VisibilityMode;
-import com.smartgwt.client.widgets.Img;
 import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.SectionStack;
 import com.smartgwt.client.widgets.layout.SectionStackSection;
 import com.smartgwt.client.widgets.layout.VLayout;
-import com.smartgwt.client.widgets.tab.TabSet;
-import com.smartgwt.client.widgets.toolbar.ToolStrip;
-import ${package}.client.i18n.ApplicationMessages;
-import ${package}.client.pages.AbstractTab;
-import ${package}.client.pages.FeatureListGridPage;
-import ${package}.client.pages.SearchPage;
-import org.geomajas.gwt.client.Geomajas;
-import org.geomajas.gwt.client.widget.LayerTree;
-import org.geomajas.gwt.client.widget.Legend;
-import org.geomajas.gwt.client.widget.LoadingScreen;
-import org.geomajas.gwt.client.widget.LocaleSelect;
-import org.geomajas.gwt.client.widget.MapWidget;
-import org.geomajas.gwt.client.widget.OverviewMap;
-import org.geomajas.gwt.client.widget.Toolbar;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /*
  * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
@@ -54,9 +44,8 @@ import java.util.List;
  */
 
 /**
- * Entry point and main class for GWT application. This class defines the layout and functionality of this
- * application.
- *
+ * Entry point and main class for GWT application. This class defines the layout and functionality of this application.
+ * 
  * @author geomajas-gwt-archetype
  */
 public class Application implements EntryPoint {
@@ -65,11 +54,7 @@ public class Application implements EntryPoint {
 
 	private Legend legend;
 
-	private TabSet tabSet = new TabSet();
-
-	private List<AbstractTab> tabs = new ArrayList<AbstractTab>();
-
-	private ApplicationMessages messages = GWT.create(ApplicationMessages.class);
+	// private ApplicationMessages messages = GWT.create(ApplicationMessages.class);
 
 	public Application() {
 	}
@@ -78,34 +63,13 @@ public class Application implements EntryPoint {
 		VLayout mainLayout = new VLayout();
 		mainLayout.setWidth100();
 		mainLayout.setHeight100();
-
-		// ---------------------------------------------------------------------
-		// Top bar:
-		// ---------------------------------------------------------------------
-		ToolStrip topBar = new ToolStrip();
-		topBar.setHeight(33);
-		topBar.setWidth100();
-		topBar.addSpacer(6);
-
-		Img icon = new Img("[ISOMORPHIC]/geomajas/geomajas_desktopicon_small.png");
-		icon.setSize(24);
-		topBar.addMember(icon);
-		topBar.addSpacer(6);
-
-		Label title = new Label(messages.applicationTitle("hello world"));
-		title.setStyleName("appTitle");
-		title.setWidth(300);
-		topBar.addMember(title);
-		topBar.addFill();
-		topBar.addMember(new LocaleSelect("English"));
-
-		mainLayout.addMember(topBar);
+		mainLayout.setBackgroundColor("#A0A0A0");
 
 		HLayout layout = new HLayout();
 		layout.setWidth100();
 		layout.setHeight100();
-		layout.setMembersMargin(5);
-		layout.setMargin(5);
+		layout.setMembersMargin(10);
+		layout.setMargin(10);
 
 		// ---------------------------------------------------------------------
 		// Create the left-side (map and tabs):
@@ -113,30 +77,43 @@ public class Application implements EntryPoint {
 		final MapWidget map = new MapWidget("mapMain", "app");
 		final Toolbar toolbar = new Toolbar(map);
 		toolbar.setButtonSize(Toolbar.BUTTON_SIZE_BIG);
+		toolbar.setBackgroundColor("#647386");
+		toolbar.setBackgroundImage("");
+		toolbar.setBorder("0px");
+
+		map.getMapModel().addMapModelHandler(new MapModelHandler() {
+
+			public void onMapModelChange(MapModelEvent event) {
+				Label title = new Label("Geomajas GWT: hello world");
+				title.setStyleName("appTitle");
+				title.setWidth(260);
+				toolbar.addFill();
+				toolbar.addMember(title);
+			}
+		});
 
 		VLayout mapLayout = new VLayout();
-		mapLayout.setShowResizeBar(true);
-		mapLayout.setResizeBarTarget("tabs");
 		mapLayout.addMember(toolbar);
 		mapLayout.addMember(map);
 		mapLayout.setHeight("65%");
-		tabSet.setTabBarPosition(Side.TOP);
-		tabSet.setWidth100();
-		tabSet.setHeight("35%");
-		tabSet.setID("tabs");
 
 		VLayout leftLayout = new VLayout();
-		leftLayout.setShowEdges(true);
+		leftLayout.setBorder("10px solid #777777");
+		leftLayout.setStyleName("round_corner");
 		leftLayout.addMember(mapLayout);
-		leftLayout.addMember(tabSet);
 
 		layout.addMember(leftLayout);
 
+		// Add a search panel to the top-right of the map:
+		SearchPanel searchPanel = new SearchPanel(map.getMapModel(), mapLayout);
+		mapLayout.addChild(searchPanel);
+		
 		// ---------------------------------------------------------------------
 		// Create the right-side (overview map, layer-tree, legend):
 		// ---------------------------------------------------------------------
 		final SectionStack sectionStack = new SectionStack();
-		sectionStack.setShowEdges(true);
+		sectionStack.setBorder("10px solid #777777");
+		sectionStack.setStyleName("round_corner");
 		sectionStack.setVisibilityMode(VisibilityMode.MULTIPLE);
 		sectionStack.setCanReorderSections(true);
 		sectionStack.setCanResizeSections(false);
@@ -146,6 +123,8 @@ public class Application implements EntryPoint {
 		SectionStackSection section1 = new SectionStackSection("Overview map");
 		section1.setExpanded(true);
 		overviewMap = new OverviewMap("mapOverview", "app", map, false, true);
+		overviewMap.setTargetMaxExtentRectangleStyle(new ShapeStyle("#888888", 0.3f, "#666666", 0.75f, 2));
+		overviewMap.setRectangleStyle(new ShapeStyle("#6699FF", 0.3f, "#6699CC", 1f, 2));
 		section1.addItem(overviewMap);
 		sectionStack.addSection(section1);
 
@@ -160,6 +139,7 @@ public class Application implements EntryPoint {
 		SectionStackSection section3 = new SectionStackSection("Legend");
 		section3.setExpanded(true);
 		legend = new Legend(map.getMapModel());
+		legend.setBackgroundColor("#FFFFFF");
 		section3.addItem(legend);
 		sectionStack.addSection(section3);
 
@@ -167,39 +147,22 @@ public class Application implements EntryPoint {
 		layout.addMember(sectionStack);
 
 		// ---------------------------------------------------------------------
-		// Bottom left: Add tabs here:
-		// ---------------------------------------------------------------------
-		FeatureListGridPage page1 = new FeatureListGridPage(map);
-		addTab(new SearchPage(map, tabSet, page1.getTable()));
-		addTab(page1);
-
-		// ---------------------------------------------------------------------
 		// Finally draw everything:
 		// ---------------------------------------------------------------------
 		mainLayout.addMember(layout);
 		mainLayout.draw();
 
-		// Install a loading screen
+		// Install a loading screen.
 		// This only works if the application initially shows a map with at least 1 vector layer:
-		LoadingScreen loadScreen = new LoadingScreen(map, "Simple GWT application using Geomajas "
-				+ Geomajas.getVersion());
-		loadScreen.draw();
+		// LoadingScreen loadScreen = new LoadingScreen(map, "Geomajas GWT application");
+		// loadScreen.draw();
 
 		// Then initialize:
 		initialize();
 	}
 
-	private void addTab(AbstractTab tab) {
-		tabSet.addTab(tab);
-		tabs.add(tab);
-	}
-
 	private void initialize() {
 		legend.setHeight(200);
 		overviewMap.setHeight(200);
-
-		for (AbstractTab tab : tabs) {
-			tab.initialize();
-		}
 	}
 }
