cf949df9589a4581ae8a53676db8ef5488f6d958
Jan De Moerloose <jan.demoerloose@geosparc.com>
2011-02-03 09:07:47 +0000

    GBE-105: missing class

---
+++ backend/impl/src/test/java/org/geomajas/internal/service/pipeline/StepIntercept.java
@@ -0,0 +1,39 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+package org.geomajas.internal.service.pipeline;
+
+import org.geomajas.global.GeomajasException;
+import org.geomajas.layer.feature.attribute.StringAttribute;
+import org.geomajas.service.pipeline.PipelineContext;
+import org.geomajas.service.pipeline.PipelineStep;
+
+/**
+ * Step for interception testing.
+ * 
+ * @author Jan De Moerloose
+ * 
+ */
+public class StepIntercept implements PipelineStep<StringAttribute> {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void execute(PipelineContext context, StringAttribute response) throws GeomajasException {
+		response.setValue(response.getValue() + id);
+	}
+}
