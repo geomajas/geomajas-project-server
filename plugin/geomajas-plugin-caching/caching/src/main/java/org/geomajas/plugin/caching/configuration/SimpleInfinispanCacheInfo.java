b6f359035e1d04c08504bbb2b8c5f566e934643f
Joachim Van der Auwera <joachim@progs.be>
2011-03-15 21:18:00 +0000

    CACHE-13 fix some sonar warnings

--- plugin/geomajas-plugin-caching/caching/src/main/java/org/geomajas/plugin/caching/configuration/SimpleInfinispanCacheInfo.java
+++ plugin/geomajas-plugin-caching/caching/src/main/java/org/geomajas/plugin/caching/configuration/SimpleInfinispanCacheInfo.java
@@ -91,7 +91,7 @@ public class SimpleInfinispanCacheInfo extends AbstractInfinispanConfiguration {
 			if (!dir.isDirectory()) {
 				if (dir.exists()) {
 					log.error("Location {} for 2nd level cache should be a directory.", location);
-					throw new RuntimeException("Invalid location for setLevel2CacheLocation, " + location +
+					throw new RuntimeException("Invalid location for setLevel2CacheLocation, " + location + //NOPMD
 							" has to be a directory."); //NOPMD
 				} else {
 					if (!dir.mkdirs()) {
--- plugin/geomajas-plugin-caching/caching/src/main/java/org/geomajas/plugin/caching/service/CacheContextImpl.java
+++ plugin/geomajas-plugin-caching/caching/src/main/java/org/geomajas/plugin/caching/service/CacheContextImpl.java
@@ -25,6 +25,8 @@ import java.util.Set;
  */
 public class CacheContextImpl implements CacheContext {
 
+	private static final long serialVersionUID = 100L;
+
 	private transient final Logger log = LoggerFactory.getLogger(CacheContextImpl.class);
 
 	private Map<String, Object> map = new HashMap<String, Object>();
