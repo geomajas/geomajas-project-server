12245cd536508448dbae74b61e9b3a09897cb6b9
Joachim Van der Auwera <joachim@progs.be>
2011-05-25 09:51:15 +0000

    SPRINT-32 add @ExpectAlternatives and complete javadoc

--- plugin/geomajas-plugin-printing/printing/src/main/java/org/geomajas/plugin/printing/component/PrintComponent.java
+++ plugin/geomajas-plugin-printing/printing/src/main/java/org/geomajas/plugin/printing/component/PrintComponent.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.geomajas.global.Api;
+import org.geomajas.global.ExpectAlternatives;
 import org.geomajas.plugin.printing.component.dto.PrintComponentInfo;
 
 import com.lowagie.text.Rectangle;
@@ -21,19 +22,20 @@ import com.lowagie.text.Rectangle;
 /**
  * A print component is a UI component that can be rendered in a PDF context. Examples are maps, images, labels, text
  * boxes, etc.. A print component can be composed of child components.
- * 
+ *
  * @author Jan De Moerloose
  * @since 2.0.0
- * 
+ *
  * @param <T> DTO object class
  */
 @Api(allMethods = true)
+@ExpectAlternatives
 public interface PrintComponent<T extends PrintComponentInfo> {
 
 	/**
 	 * Return the id of this component. Id's should be hierarchically constructed with "." as delimiter.
 	 * 
-	 * @return id
+	 * @return component id
 	 */
 	String getId();
 
@@ -47,7 +49,7 @@ public interface PrintComponent<T extends PrintComponentInfo> {
 	/**
 	 * Render the component using the specified context. This operation assumes that layout() has been called.
 	 * 
-	 * @param context
+	 * @param context pdf context
 	 */
 	void render(PdfContext context);
 
@@ -56,24 +58,29 @@ public interface PrintComponent<T extends PrintComponentInfo> {
 	 * included in the bounds property) in the component hierarchy have been determined. This operation assumes that
 	 * calculateSize() has been called.
 	 * 
-	 * @param context
+	 * @param context pdf context
 	 */
 	void layout(PdfContext context);
 
 	/**
 	 * Calculate the expected width/height of this component for the specified context.
 	 * 
-	 * @param context
+	 * @param context pdf context
 	 */
 	void calculateSize(PdfContext context);
 
 	/**
 	 * Return the rectangle in user space occupied by this component.
 	 * 
-	 * @return rectangle
+	 * @return component bounds
 	 */
 	Rectangle getBounds();
 
+	/**
+	 * Set the component bounds in user space for this component.
+	 *
+	 * @param bounds component bounds
+	 */
 	void setBounds(Rectangle bounds);
 
 	/**
@@ -83,35 +90,83 @@ public interface PrintComponent<T extends PrintComponentInfo> {
 	 */
 	LayoutConstraint getConstraint();
 
+	/**
+	 * Set the this component's parent.
+	 *
+	 * @param parent parent component
+	 */
 	void setParent(PrintComponent<?> parent);
 
 	/**
 	 * Return the parent component of this component.
 	 * 
-	 * @return
+	 * @return parent component
 	 */
 	PrintComponent<?> getParent();
 
+	/**
+	 * Get list with child components.
+	 *
+	 * @return list of children
+	 */
 	List<PrintComponent<?>> getChildren();
 
+	/**
+	 * Get child component with given id.
+	 *
+	 * @param id component id
+	 * @return requested child
+	 */
 	PrintComponent<?> getChild(String id);
 
+	/**
+	 * Add component as last child.
+	 *
+	 * @param child child
+	 */
 	void addComponent(PrintComponent<?> child);
 
+	/**
+	 * Add child component at given index.
+	 *
+	 * @param index index
+	 * @param child component
+	 */
 	void addComponent(int index, PrintComponent<?> child);
 
+	/**
+	 * Add collection of child components (at end of list).
+	 *
+	 * @param children child components
+	 */
 	void addComponents(Collection<? extends PrintComponent<?>> children);
-	
+
+	/**
+	 * Add collection of child components at specific index.
+	 *
+	 * @param index index
+	 * @param children child components
+	 */
 	void addComponents(int index, Collection<? extends PrintComponent<?>> children);
 
+	/**
+	 * Remove child component by id.
+	 *
+	 * @param child child to remove
+	 */
 	void removeComponent(PrintComponent<?> child);
 
 	/**
 	 * Call back visitor.
 	 * 
-	 * @param visitor
+	 * @param visitor visitor
 	 */
 	void accept(PrintComponentVisitor visitor);
 
+	/**
+	 * Get data from the dto.
+	 *
+	 * @param info dto to convert
+	 */
 	void fromDto(T info);
 }
