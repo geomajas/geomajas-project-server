0c452e28ef9545bdfc8015bfd012c12910b8fc5a
Jan De Moerloose <jan.demoerloose@geosparc.com>
2011-07-06 07:09:35 +0000

    GBE-202: checkstyle

--- backend/api-annotations/src/main/java/org/geomajas/annotations/Api.java
+++ backend/api-annotations/src/main/java/org/geomajas/annotations/Api.java
@@ -13,7 +13,6 @@ package org.geomajas.annotations;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
