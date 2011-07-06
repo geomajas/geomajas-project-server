4647db897370dcfb1751028e186104ed924e8376
Jan De Moerloose <jan.demoerloose@geosparc.com>
2011-07-06 06:46:02 +0000

    GBE-202: re-added for backwards compatibility

---
+++ backend/api/src/main/java/org/geomajas/global/ExpectAlternatives.java
@@ -0,0 +1,28 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.global;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Indicates that an interface is expected to have more than one implementation.
+ * This is used by the bean name generator to determine the default bean name.
+ *
+ * @author Joachim Van der Auwera
+ * @deprecated use {@link org.geomajas.annotations.ExpectAlternatives}
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Deprecated
+public @interface ExpectAlternatives {
+
+}
