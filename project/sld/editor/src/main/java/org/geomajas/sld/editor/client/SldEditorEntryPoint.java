21e4606a8303cc1c63d70088c352fc1108493f9b
Jan De Moerloose <jan.demoerloose@geosparc.com>
2011-07-19 06:31:56 +0000

    GBE-203: added label widget, removed css

--- project/sld/editor/src/main/java/org/geomajas/sld/editor/client/SldEditorEntryPoint.java
+++ project/sld/editor/src/main/java/org/geomajas/sld/editor/client/SldEditorEntryPoint.java
@@ -12,13 +12,19 @@ package org.geomajas.sld.editor.client;
 
 import java.util.List;
 
+import org.geomajas.sld.NamedLayerInfo;
+import org.geomajas.sld.StyledLayerDescriptorInfo;
 import org.geomajas.sld.client.SldGwtService;
 import org.geomajas.sld.client.SldGwtServiceAsync;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -31,17 +37,22 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class SldEditorEntryPoint implements EntryPoint {
 
 	public void onModuleLoad() {
-		// Make a new list box, adding a few items to it.
-		final ListBox lb = new ListBox();
+		AbsolutePanel panel = new AbsolutePanel();
+		panel.setSize("100%", "100%");
+		// list of sld's
+		final ListBox sldList = new ListBox();
+		sldList.setVisibleItemCount(5);
 
-		// Make enough room for all five items (setting this value to 1 turns it
-		// into a drop-down list).
-		lb.setVisibleItemCount(5);
+		// label
+		final Label nameLabel = new Label("<name of layer>");
+
+		panel.add(sldList, 100, 100);
+		panel.add(nameLabel, 100, 300);
 
 		// Add it to the root panel.
-		RootPanel.get().add(lb);
+		RootPanel.get().add(panel);
 
-		SldGwtServiceAsync service = GWT.create(SldGwtService.class);
+		final SldGwtServiceAsync service = GWT.create(SldGwtService.class);
 		ServiceDefTarget endpoint = (ServiceDefTarget) service;
 		endpoint.setServiceEntryPoint(GWT.getHostPageBaseURL() + "d/sld");
 		service.findAll(new AsyncCallback<List<String>>() {
@@ -49,12 +60,37 @@ public class SldEditorEntryPoint implements EntryPoint {
 			public void onSuccess(List<String> result) {
 				GWT.log("got " + result.size() + " SLDs");
 				for (String name : result) {
-					lb.addItem(name);
+					sldList.addItem(name);
 				}
 			}
 
 			public void onFailure(Throwable caught) {
 				GWT.log("could not access SLDs", caught);
+			}
+		});
+		sldList.addChangeHandler(new ChangeHandler() {
+
+			public void onChange(ChangeEvent event) {
+				int index = sldList.getSelectedIndex();
+				if (index >= 0) {
+					String name = sldList.getItemText(index);
+					service.findByName(name, new AsyncCallback<StyledLayerDescriptorInfo>() {
+
+						public void onSuccess(StyledLayerDescriptorInfo sld) {
+							if (!sld.getChoiceList().isEmpty()) {
+								StyledLayerDescriptorInfo.ChoiceInfo info = sld.getChoiceList().iterator().next();
+								if (info.ifNamedLayer()) {
+									NamedLayerInfo namedLayer = info.getNamedLayer();
+									nameLabel.setText(namedLayer.getName());
+								}
+							}
+						}
+
+						public void onFailure(Throwable caught) {
+							GWT.log("could not access SLDs", caught);
+						}
+					});
+				}
 			}
 		});
 	}
