675841b72c16cae9b93280b5d793ef24f2552c01
Joachim Van der Auwera <joachim@progs.be>
2011-07-29 08:59:11 +0000

    @DirtiesContext should not be needed, need to figure out cause if needed (-->jira)

--- backend/impl/src/test/java/org/geomajas/internal/security/SecurityManagerLoopTest.java
+++ backend/impl/src/test/java/org/geomajas/internal/security/SecurityManagerLoopTest.java
@@ -19,7 +19,6 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
