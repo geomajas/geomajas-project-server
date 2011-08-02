3ebd5c3f798b756ef2586206b715edf6f48a64a3
Joachim Van der Auwera <joachim@progs.be>
2011-08-02 19:21:15 +0000

    fix double copyright

--- plugin/geomajas-plugin-rasterizing/rasterizing-gwt-example/src/main/java/org/geomajas/client/gui/SearchPanel.java
+++ plugin/geomajas-plugin-rasterizing/rasterizing-gwt-example/src/main/java/org/geomajas/client/gui/SearchPanel.java
@@ -30,17 +30,6 @@ import com.smartgwt.client.widgets.events.ResizedHandler;
 import com.smartgwt.client.widgets.layout.LayoutSpacer;
 import com.smartgwt.client.widgets.layout.VLayout;
 
-/*
- * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
- *
- * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
- *
- * The program is available in open source according to the GNU Affero
- * General Public License. All contributions in this program are covered
- * by the Geomajas Contributors License Agreement. For full licensing
- * details, see LICENSE.txt in the project root.
- */
-
 /**
  * Layout for the search panel (see buttons top right on the map).
  * 
