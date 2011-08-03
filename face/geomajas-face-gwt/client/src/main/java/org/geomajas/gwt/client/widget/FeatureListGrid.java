621210ec61c2aac6ec3aaa6b9078de4d31ddde18
Jan De Moerloose <jan.demoerloose@geosparc.com>
2011-08-03 09:50:36 +0000

    GWT-264: restricted records to identifying attributes (performance improvement, should have no functional effect) GWT-254: added some documentation

--- face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/widget/FeatureListGrid.java
+++ face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/widget/FeatureListGrid.java
@@ -204,45 +204,47 @@ public class FeatureListGrid extends ListGrid implements FeatureSelectionHandler
 
 	private void copyToRecord(Feature feature, ListGridRecord record) {
 		for (AttributeInfo attributeInfo : layer.getLayerInfo().getFeatureInfo().getAttributes()) {
-			Attribute<?> attr = feature.getAttributes().get(attributeInfo.getName());
-			if (attr.isPrimitive()) {
-				Object value = attr.getValue();
-				if (value != null) {
-					if (value instanceof Boolean) {
-						record.setAttribute(attributeInfo.getName(), (Boolean) value); // "false" != false
+			if (allAttributesDisplayed || attributeInfo.isIdentifying()) {
+				Attribute<?> attr = feature.getAttributes().get(attributeInfo.getName());
+				if (attr.isPrimitive()) {
+					Object value = attr.getValue();
+					if (value != null) {
+						if (value instanceof Boolean) {
+							record.setAttribute(attributeInfo.getName(), (Boolean) value); // "false" != false
+						} else {
+							record.setAttribute(attributeInfo.getName(), value.toString());
+						}
 					} else {
-						record.setAttribute(attributeInfo.getName(), value.toString());
+						record.setAttribute(attributeInfo.getName(), "");
 					}
 				} else {
-					record.setAttribute(attributeInfo.getName(), "");
-				}
-			} else {
-				AssociationAttributeInfo associationAttributeInfo = (AssociationAttributeInfo) attributeInfo;
-				String displayName = associationAttributeInfo.getFeature().getDisplayAttributeName();
-				if (displayName == null) {
-					displayName = associationAttributeInfo.getFeature().getAttributes().get(0).getName();
-				}
-				switch (associationAttributeInfo.getType()) {
-					case MANY_TO_ONE:
-						ManyToOneAttribute manyToOneAttribute = (ManyToOneAttribute) attr;
-						Object value = manyToOneAttribute.getValue().getAllAttributes().get(displayName).getValue();
-						if (value != null) {
-							record.setAttribute(attributeInfo.getName(), value.toString());
-						} else {
-							record.setAttribute(attributeInfo.getName(), "");
-						}
-						break;
-					case ONE_TO_MANY:
-						OneToManyAttribute oneToManyAttribute = (OneToManyAttribute) attr;
-						List<String> values = new ArrayList<String>();
-						for (AssociationValue assoc : oneToManyAttribute.getValue()) {
-							Object o = assoc.getAllAttributes().get(displayName).getValue();
-							if (o != null) {
-								values.add(o.toString());
+					AssociationAttributeInfo associationAttributeInfo = (AssociationAttributeInfo) attributeInfo;
+					String displayName = associationAttributeInfo.getFeature().getDisplayAttributeName();
+					if (displayName == null) {
+						displayName = associationAttributeInfo.getFeature().getAttributes().get(0).getName();
+					}
+					switch (associationAttributeInfo.getType()) {
+						case MANY_TO_ONE:
+							ManyToOneAttribute manyToOneAttribute = (ManyToOneAttribute) attr;
+							Object value = manyToOneAttribute.getValue().getAllAttributes().get(displayName).getValue();
+							if (value != null) {
+								record.setAttribute(attributeInfo.getName(), value.toString());
+							} else {
+								record.setAttribute(attributeInfo.getName(), "");
 							}
-						}
-						record.setAttribute(attributeInfo.getName(), StringUtil.join(values, ","));
-						break;
+							break;
+						case ONE_TO_MANY:
+							OneToManyAttribute oneToManyAttribute = (OneToManyAttribute) attr;
+							List<String> values = new ArrayList<String>();
+							for (AssociationValue assoc : oneToManyAttribute.getValue()) {
+								Object o = assoc.getAllAttributes().get(displayName).getValue();
+								if (o != null) {
+									values.add(o.toString());
+								}
+							}
+							record.setAttribute(attributeInfo.getName(), StringUtil.join(values, ","));
+							break;
+					}
 				}
 			}
 		}
