208e01ee2f80c0e863ed92b5ebbe0ed3f075975d
Joachim Van der Auwera <joachim@progs.be>
2011-08-10 10:32:09 +0000

    GC-33 first iteration

---
+++ plugin/geomajas-plugin-geocoder/geocoder/src/main/java/org/geomajas/plugin/geocoder/service/TypeCoordinateService.java
@@ -0,0 +1,123 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.plugin.geocoder.service;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import org.geomajas.annotation.Api;
+import org.geomajas.geometry.Crs;
+import org.geomajas.layer.LayerException;
+import org.geomajas.plugin.geocoder.api.GeocoderService;
+import org.geomajas.plugin.geocoder.api.GetLocationResult;
+import org.geomajas.service.DtoConverterService;
+import org.geomajas.service.GeoService;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import javax.xml.transform.Result;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Geocoder service which simply jumps to the typed coordinate. The oordinates should be separated by whitespace.
+ * The default CRS for the coordinate can be configured. An alternate CRS can be chosen using " crs:" followed by the
+ * CRS at the end of the string.
+ *
+ * @author Joachim Van der Auwera
+ * @since 1.2.0
+ */
+@Api
+public class TypeCoordinateService implements GeocoderService {
+
+	private final Logger log = LoggerFactory.getLogger(TypeCoordinateService.class);
+
+	@Autowired
+	private GeoService geoService;
+
+	@Autowired
+	private DtoConverterService dtoConverterService;
+
+	@Autowired
+	private SplitCommaReverseService splitCommaReverseService;
+
+	private String defaultCrs = "EPSG:4326";
+	private Crs crs;
+
+	private String name = "TypeCoordinate";
+
+	@PostConstruct
+	protected void postConstruct() throws LayerException {
+		crs = geoService.getCrs2(defaultCrs);
+	}
+
+	/**
+	 * Set the default CRS which is used for the coordinates. When not set it defaults to EPSG:4326.
+	 *
+	 * @param defaultCrs default CRS
+	 */
+	public void setDefaultCrs(String defaultCrs) {
+		this.defaultCrs = defaultCrs;
+	}
+
+	/** {@inheritDoc} */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Set the name for this geocoder service.
+	 *
+	 * @param name name
+	 */
+	@Api
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/** {@inheritDoc} */
+	public CoordinateReferenceSystem getCrs() {
+		return crs;
+	}
+
+	/** {@inheritDoc} */
+	public GetLocationResult[] getLocation(List<String> location, int maxAlternatives, Locale locale) {
+		if (null == locale) {
+			locale = Locale.US;
+		}
+		GetLocationResult result = new GetLocationResult();
+		if (location.size() > 0) {
+			String[] parts = location.get(0).split("\\s+");
+			if (parts.length >= 2) {
+				NumberFormat format = NumberFormat.getInstance(locale);
+				try {
+					Double x = format.parse(parts[0]).doubleValue();
+					Double y = format.parse(parts[1]).doubleValue();
+
+					result.setCoordinate(new Coordinate(x, y));
+					List<String> canonical = new ArrayList<String>();
+					canonical.add(parts[0] + " " + parts[1]);
+					result.setCanonicalStrings(canonical);
+					return new GetLocationResult[] { result };
+				} catch (ParseException pe) {
+					log.debug("Could not parse " + location.get(0) + ", " + pe.getMessage());
+				}
+
+			}
+		}
+		return new GetLocationResult[0];
+	}
+}
---
+++ plugin/geomajas-plugin-geocoder/geocoder/src/test/java/org/geomajas/plugin/geocoder/service/TypeCoordinateServiceTest.java
@@ -0,0 +1,58 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.plugin.geocoder.service;
+
+import junit.framework.Assert;
+import org.geomajas.plugin.geocoder.api.GetLocationResult;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Test for {@link TypeCoordinateService}.
+ *
+ * @author Joachim Van der Auwera
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/org/geomajas/spring/geomajasContext.xml",
+		"/tcsContext.xml"})
+public class TypeCoordinateServiceTest {
+
+	private static final double DELTA = 1e-6;
+
+	@Autowired
+	private TypeCoordinateService geocoder;
+
+	@Test
+	public void testGeocoderSimple() {
+		List<String> list = new ArrayList<String>();
+		GetLocationResult[] result;
+
+		list.clear();
+		list.add("4.77397  51.05125  ");
+		result = geocoder.getLocation(list, 50, null);
+		Assert.assertNotNull(result);
+		Assert.assertEquals(1,result.length);
+		Assert.assertNotNull(result[0].getCoordinate());
+		Assert.assertEquals(4.77397, result[0].getCoordinate().x, DELTA);
+		Assert.assertEquals(51.05125, result[0].getCoordinate().y, DELTA);
+		Assert.assertEquals(1, result[0].getCanonicalStrings().size());
+		Assert.assertEquals("4.77397 51.05125", result[0].getCanonicalStrings().get(0));
+		Assert.assertNull(result[0].getEnvelope());
+	}
+
+}
