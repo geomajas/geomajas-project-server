504a27758de38276b266b05090403e6d57c7c609
Pieter De Graef <pieter.degraef@geosparc.com>
2011-08-23 11:29:46 +0000

    GBE-214: Have the ClientLayerTreeInfo configuration object implement the ClientWidgetInfo interface.

--- backend/api/src/main/java/org/geomajas/configuration/client/ClientLayerTreeInfo.java
+++ backend/api/src/main/java/org/geomajas/configuration/client/ClientLayerTreeInfo.java
@@ -10,7 +10,6 @@
  */
 package org.geomajas.configuration.client;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,12 +19,16 @@ import org.geomajas.annotation.Api;
 
 /**
  * Configuration of a layer tree.
+ * <p>
+ * Since version 1.10.0 this configuration object implements the <code>ClientWidgetInfo</code> interface, so that it can
+ * be easily retrieved like any other widget configuration object.
+ * </p>
  * 
  * @author Joachim Van der Auwera
  * @since 1.6.0
  */
 @Api(allMethods = true)
-public class ClientLayerTreeInfo implements Serializable {
+public class ClientLayerTreeInfo implements ClientWidgetInfo {
 
 	private static final long serialVersionUID = 151L;
 
@@ -49,7 +52,8 @@ public class ClientLayerTreeInfo implements Serializable {
 	/**
 	 * Set the unique id of this layer tree (auto-copied from Spring context).
 	 * 
-	 * @param id id
+	 * @param id
+	 *            id
 	 */
 	public void setId(String id) {
 		this.id = id;
@@ -67,7 +71,8 @@ public class ClientLayerTreeInfo implements Serializable {
 	/**
 	 * Set the root node.
 	 * 
-	 * @param treeNode root node
+	 * @param treeNode
+	 *            root node
 	 */
 	public void setTreeNode(ClientLayerTreeNodeInfo treeNode) {
 		this.treeNode = treeNode;
@@ -85,7 +90,8 @@ public class ClientLayerTreeInfo implements Serializable {
 	/**
 	 * Set the list of tools for this layer tree.
 	 * 
-	 * @param tools list of tools
+	 * @param tools
+	 *            list of tools
 	 */
 	public void setTools(List<ClientToolInfo> tools) {
 		this.tools = tools;
