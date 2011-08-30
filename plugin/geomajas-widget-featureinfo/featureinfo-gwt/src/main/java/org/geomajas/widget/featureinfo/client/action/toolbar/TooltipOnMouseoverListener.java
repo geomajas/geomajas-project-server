0adab19432be573e08f626010043cc867ea62f78
Kristof Heirwegh <kristof.heirwegh@dfc.be>
2011-08-30 14:24:30 +0000

    FIT-1: tooltipListener.getLabel(): added layer parameter, so you know what layer this is for (passing dto's, so no layer)

--- plugin/geomajas-widget-featureinfo/featureinfo-gwt/src/main/java/org/geomajas/widget/featureinfo/client/action/toolbar/TooltipOnMouseoverListener.java
+++ plugin/geomajas-widget-featureinfo/featureinfo-gwt/src/main/java/org/geomajas/widget/featureinfo/client/action/toolbar/TooltipOnMouseoverListener.java
@@ -184,7 +184,7 @@ public class TooltipOnMouseoverListener extends AbstractListener {
 							}
 							for (org.geomajas.layer.feature.Feature feature : features) {
 								if (count < maxLabelCount) {
-									writeFeature(sb, getLabel(feature));
+									writeFeature(sb, getLabel(feature, layer));
 									if (widest < feature.getLabel().length()) {
 										widest = feature.getLabel().length();
 									}
@@ -225,7 +225,7 @@ public class TooltipOnMouseoverListener extends AbstractListener {
 	 * @param f
 	 * @return
 	 */
-	protected String getLabel(org.geomajas.layer.feature.Feature f) {
+	protected String getLabel(org.geomajas.layer.feature.Feature f, VectorLayer layer) {
 		return f.getLabel();
 	}
 	
