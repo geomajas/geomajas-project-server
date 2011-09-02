104c501e067feab2e107bd799db3a6e19d39a5d1
Pieter De Graef <pieter.degraef@geosparc.com>
2011-09-02 13:52:57 +0000

    GC-35: Add PureGWT geocoder module with basic geocoding functionality (textbox)

---
+++ plugin/geomajas-plugin-geocoder/geocoder-puregwt/src/main/java/org/geomajas/plugin/geocoder/client/GeocoderPresenter.java
@@ -0,0 +1,178 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.plugin.geocoder.client;
+
+import java.util.List;
+
+import org.geomajas.command.CommandResponse;
+import org.geomajas.plugin.geocoder.client.event.SelectAlternativeEvent;
+import org.geomajas.plugin.geocoder.client.event.SelectAlternativeHandler;
+import org.geomajas.plugin.geocoder.client.event.SelectLocationEvent;
+import org.geomajas.plugin.geocoder.client.event.SelectLocationHandler;
+import org.geomajas.plugin.geocoder.command.dto.GetLocationForStringAlternative;
+import org.geomajas.plugin.geocoder.command.dto.GetLocationForStringRequest;
+import org.geomajas.plugin.geocoder.command.dto.GetLocationForStringResponse;
+import org.geomajas.puregwt.client.GeomajasGinjector;
+import org.geomajas.puregwt.client.command.Command;
+import org.geomajas.puregwt.client.command.CommandCallback;
+import org.geomajas.puregwt.client.map.MapPresenter;
+import org.geomajas.puregwt.client.spatial.Bbox;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.Window;
+
+/**
+ * Widget for starting a geocoder location search.
+ *
+ * @author Pieter De Graef
+ */
+public class GeocoderPresenter implements SelectLocationHandler, SelectAlternativeHandler {
+
+	private final GeomajasGinjector geomajasInjector = GWT.create(GeomajasGinjector.class);
+
+	private MapPresenter mapPresenter;
+
+	private GeocoderTextBox geocoderTextBox;
+
+	private String servicePattern = ".*";
+
+	private GeocoderMessages messages = GWT.create(GeocoderMessages.class);
+
+	private HandlerManager handlerManager;
+
+	/**
+	 * Create geocoder widget which allows searching a location from a string.
+	 *
+	 * @param mapPresenter map to apply search results
+	 * @param geocoderWidget geocoder widget
+	 */
+	GeocoderPresenter(MapPresenter mapPresenter, GeocoderTextBox geocoderTextBox) {
+		this.mapPresenter = mapPresenter;
+		this.geocoderTextBox = geocoderTextBox;
+
+		handlerManager = new HandlerManager(this);
+		setSelectAlternativeHandler(this);
+		setSelectLocationHandler(this);
+	}
+
+	public void clearLocation() {
+		geocoderTextBox.setValue("");
+	}
+
+	public void goToLocation(final String location) {
+		Command command = new Command(GetLocationForStringRequest.COMMAND);
+		GetLocationForStringRequest request = new GetLocationForStringRequest();
+		request.setCrs(mapPresenter.getViewPort().getCrs());
+		request.setLocation(location);
+		request.setServicePattern(servicePattern);
+		command.setCommandRequest(request);
+		geomajasInjector.getCommandService().execute(command, new CommandCallback() {
+
+			public void onSuccess(CommandResponse response) {
+				goToLocation(response, location);
+			}
+
+			public void onFailure(Throwable error) {
+				// Do something....
+			}
+		});
+	}
+
+	public void goToLocation(final CommandResponse commandResponse, final String location) {
+		if (commandResponse instanceof GetLocationForStringResponse) {
+			GetLocationForStringResponse response = (GetLocationForStringResponse) commandResponse;
+			if (response.isLocationFound()) {
+				handlerManager.fireEvent(new SelectLocationEvent(mapPresenter, response));
+			} else {
+				List<GetLocationForStringAlternative> alternatives = response.getAlternatives();
+				if (null != alternatives && alternatives.size() > 0) {
+					handlerManager.fireEvent(new SelectAlternativeEvent(mapPresenter, alternatives));
+				} else {
+					// TODO This should throw an event...
+					Window.alert(messages.locationNotFound(location));
+				}
+			}
+		}
+	}
+
+	/**
+	 * Get the regular expression which is used to select which geocoder services to use.
+	 *
+	 * @return geocoder selection regular expression
+	 */
+	public String getServicePattern() {
+		return servicePattern;
+	}
+
+	/**
+	 * Set the regular expression which is used to select which geocoder services to use.
+	 *
+	 * @param servicePattern geocoder selection regular expression
+	 */
+	public void setServicePattern(String servicePattern) {
+		this.servicePattern = servicePattern;
+	}
+
+	/**
+	 * Set the select alternative handler.
+	 * <p/>
+	 * There can only be one handler, the default displays the alternatives in a window on the map widget.
+	 *
+	 * @param handler select alternative handler
+	 * @return handler registration.
+	 */
+	public HandlerRegistration setSelectAlternativeHandler(SelectAlternativeHandler handler) {
+		if (handlerManager.getHandlerCount(SelectAlternativeHandler.TYPE) > 0) {
+			SelectAlternativeHandler previous = handlerManager.getHandler(SelectAlternativeHandler.TYPE, 0);
+			handlerManager.removeHandler(SelectAlternativeHandler.TYPE, previous);
+		}
+		return handlerManager.addHandler(SelectAlternativeHandler.TYPE, handler);
+	}
+
+	/**
+	 * Set the select location handler.
+	 * <p/>
+	 * There can only be one handler, the default zooms the map widget to the selected location.
+	 *
+	 * @param handler select location handler
+	 * @return handler registration.
+	 */
+	public HandlerRegistration setSelectLocationHandler(SelectLocationHandler handler) {
+		if (handlerManager.getHandlerCount(SelectLocationHandler.TYPE) > 0) {
+			SelectLocationHandler previous = handlerManager.getHandler(SelectLocationHandler.TYPE, 0);
+			handlerManager.removeHandler(SelectLocationHandler.TYPE, previous);
+		}
+		return handlerManager.addHandler(SelectLocationHandler.TYPE, handler);
+	}
+
+	public void onSelectAlternative(SelectAlternativeEvent event) {
+		// TODO implement me...
+	}
+
+	public void onSelectLocation(SelectLocationEvent event) {
+		Bbox bounds = geomajasInjector.getGeometryConverter().toGwt(event.getBbox());
+		mapPresenter.getViewPort().applyBounds(bounds);
+		geocoderTextBox.setValue(event.getCanonicalLocation());
+	}
+
+	/**
+	 * Fire an event.
+	 *
+	 * @param event event to fire
+	 */
+	public void fireEvent(GwtEvent<?> event) {
+		handlerManager.fireEvent(event);
+	}
+}
---
+++ plugin/geomajas-plugin-geocoder/geocoder-puregwt/src/main/java/org/geomajas/plugin/geocoder/client/GeocoderTextBox.java
@@ -0,0 +1,109 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.plugin.geocoder.client;
+
+import org.geomajas.plugin.geocoder.client.event.SelectAlternativeEvent;
+import org.geomajas.plugin.geocoder.client.event.SelectAlternativeHandler;
+import org.geomajas.plugin.geocoder.client.event.SelectLocationEvent;
+import org.geomajas.plugin.geocoder.client.event.SelectLocationHandler;
+import org.geomajas.puregwt.client.map.MapPresenter;
+
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.TextBox;
+
+/**
+ * Geocoder widget that navigates to a location when "enter" is pressed.
+ * 
+ * @author Pieter De Graef
+ */
+public class GeocoderTextBox extends TextBox {
+
+	private MapPresenter mapPresenter;
+
+	private GeocoderPresenter geocoderPresenter;
+
+	public GeocoderTextBox(final MapPresenter mapPresenter) {
+		geocoderPresenter = new GeocoderPresenter(mapPresenter, this);
+
+		addKeyPressHandler(new KeyPressHandler() {
+
+			public void onKeyPress(KeyPressEvent event) {
+				if (event.getNativeEvent().getKeyCode() == KeyCodes.KEY_ENTER) {
+					goToLocation();
+				}
+			}
+		});
+	}
+
+	public void goToLocation() {
+		geocoderPresenter.goToLocation(getValue());
+	}
+
+	public MapPresenter getMapPresenter() {
+		return mapPresenter;
+	}
+
+	/**
+	 * Get the regular expression which is used to select which geocoder services to use.
+	 *
+	 * @return geocoder selection regular expression
+	 */
+	public String getServicePattern() {
+		return geocoderPresenter.getServicePattern();
+	}
+
+	/**
+	 * Set the regular expression which is used to select which geocoder services to use.
+	 *
+	 * @param servicePattern geocoder selection regular expression
+	 */
+	public void setServicePattern(String servicePattern) {
+		geocoderPresenter.setServicePattern(servicePattern);
+	}
+
+	/**
+	 * Set the select alternative handler.
+	 * <p/>
+	 * There can only be one handler, the default displays the alternatives in a window on the map widget.
+	 *
+	 * @param handler select alternative handler
+	 * @return handler registration.
+	 */
+	public HandlerRegistration setSelectAlternativeHandler(SelectAlternativeHandler handler) {
+		return geocoderPresenter.setSelectAlternativeHandler(handler);
+	}
+
+	/**
+	 * Set the select location handler.
+	 * <p/>
+	 * There can only be one handler, the default zooms the map widget to the selected location.
+	 *
+	 * @param handler select location handler
+	 * @return handler registration.
+	 */
+	public HandlerRegistration setSelectLocationHandler(SelectLocationHandler handler) {
+		return geocoderPresenter.setSelectLocationHandler(handler);
+	}
+
+	@Override
+	public void fireEvent(GwtEvent<?> event) {
+		if (event instanceof SelectLocationEvent || event instanceof SelectAlternativeEvent) {
+			geocoderPresenter.fireEvent(event);
+		} else {
+			super.fireEvent(event);
+		}
+	}
+}
