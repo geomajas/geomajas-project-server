5a8a24c84e47924d7b2c8ee507227f2d76e0a0ec
Jan De Moerloose <jan.demoerloose@geosparc.com>
2011-09-20 10:56:07 +0000

    CGWT-5: GeomajasController added to dispatcher servlet config + GWT resource controller for GWT resources (images, js)

---
+++ face/geomajas-face-common-gwt/common-gwt/src/main/java/org/geomajas/gwt/server/mvc/GeomajasController.java
@@ -0,0 +1,76 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+package org.geomajas.gwt.server.mvc;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.geomajas.command.CommandDispatcher;
+import org.geomajas.command.CommandResponse;
+import org.geomajas.gwt.client.GeomajasService;
+import org.geomajas.gwt.client.command.GwtCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * Geomajas GWT controller, implements communication between GWT face and back-end.
+ * 
+ * @author Jan De Moerloose
+ * @since 1.0.0
+ */
+@Component("/geomajasService")
+public class GeomajasController extends RemoteServiceServlet implements Controller, GeomajasService {
+
+	private static final long serialVersionUID = 100L;
+
+	@Autowired
+	private CommandDispatcher commandDispatcher;
+
+	@Autowired
+	private ServletContext servletContext;
+
+	/**
+	 * Implements Spring Controller interface method.
+	 * 
+	 * Call GWT's RemoteService doPost() method and return null.
+	 * 
+	 * @param request current HTTP request
+	 * @param response current HTTP response
+	 * @return a ModelAndView to render, or null if handled directly
+	 * @throws Exception in case of errors
+	 */
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		doPost(request, response);
+		return null;
+	}
+
+	/**
+	 * Execute a GWT RPC command request, and return the response. These request come from the client, and the response
+	 * is sent back to the client. We use a {@link CommandDispatcher} to actually execute the command.
+	 */
+	public CommandResponse execute(GwtCommand request) {
+		if (request != null) {
+			return commandDispatcher.execute(request.getCommandName(), request.getCommandRequest(),
+					request.getUserToken(), request.getLocale());
+		}
+		return null;
+	}
+
+	public ServletContext getServletContext() {
+		return servletContext;
+	}
+
+}
---
+++ face/geomajas-face-common-gwt/common-gwt/src/test/java/org/geomajas/gwt/server/mvc/GwtResourceControllerTest.java
@@ -0,0 +1,78 @@
+package org.geomajas.gwt.server.mvc;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletException;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.core.io.AbstractResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletContext;
+
+public class GwtResourceControllerTest {
+
+	@Test
+	public void testResourceInClassPath() throws ServletException, IOException {
+		// create mock context that loads from the classpath
+		MockServletContext context = new MockServletContext();
+		MockHttpServletRequest request = new MockHttpServletRequest(context);
+		request.setPathInfo("/org/geomajas/servlet/geomajas_logo.png");
+		request.setMethod("GET");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		GwtResourceController resourceController = new GwtResourceController();
+		resourceController.setServletContext(context);
+		resourceController.getResource(request, response);
+		Resource resource = new ClassPathResource("/org/geomajas/servlet/geomajas_logo.png");
+		Assert.assertArrayEquals(IOUtils.toByteArray(resource.getInputStream()), response.getContentAsByteArray());
+	}
+
+	@Test
+	public void testResourceInContext() throws ServletException, IOException {
+		// create mock context that loads from the classpath
+		MockServletContext context = new MockServletContext();
+		MockHttpServletRequest request = new MockHttpServletRequest(context);
+		request.setServletPath("/org");
+		request.setPathInfo("/geomajas/servlet/geomajas_logo.png");
+		request.setMethod("GET");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		GwtResourceController resourceController = new GwtResourceController();
+		resourceController.setServletContext(context);
+		resourceController.getResource(request, response);
+		Resource resource = new ClassPathResource("/org/geomajas/servlet/geomajas_logo.png");
+		Assert.assertArrayEquals(IOUtils.toByteArray(resource.getInputStream()), response.getContentAsByteArray());
+	}
+
+	public class NonExistingResourceLoader implements ResourceLoader {
+
+		public Resource getResource(String location) {
+			return new AbstractResource() {
+
+				@Override
+				public boolean exists() {
+					return false;
+				}
+
+				public String getDescription() {
+					return null;
+				}
+
+				public InputStream getInputStream() throws IOException {
+					return null;
+				}
+			};
+		}
+
+		public ClassLoader getClassLoader() {
+			return getClass().getClassLoader();
+		}
+
+	}
+
+}
