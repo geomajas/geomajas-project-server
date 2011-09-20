86607a9b45ef46bd9ac71589719e3f4c80e479a2
Emiel Ackermann <emiela>
2011-09-20 09:21:24 +0000

    Some rework on HtmlBuilder; no boolean is needed, instead choose the correct method. Added more tests to HtmlBuilderTest.

--- face/geomajas-face-common-gwt/common-gwt/src/main/java/org/geomajas/gwt/client/util/HtmlBuilder.java
+++ face/geomajas-face-common-gwt/common-gwt/src/main/java/org/geomajas/gwt/client/util/HtmlBuilder.java
@@ -19,7 +19,7 @@ import org.geomajas.annotation.Api;
  *
  * @author Emiel Ackermann
  *
- * @since 1.0.0
+ * @since 1.10.0
  */
 @Api(allMethods = true)
 public final class HtmlBuilder {
@@ -38,7 +38,7 @@ public final class HtmlBuilder {
 	 * @return HTML table element as string
 	 */
 	public static String tableClassHtmlContent(String clazz, String... content) {
-		return tagClass(Html.Tag.TABLE, clazz, true, content);
+		return tagClassHtmlContent(Html.Tag.TABLE, clazz, content);
 	}
 	
 	/**
@@ -50,7 +50,7 @@ public final class HtmlBuilder {
 	 * @return HTML table element as string
 	 */
 	public static String tableStyleHtmlContent(String style, String... content) {
-		return tagStyle(Html.Tag.TABLE, style, true, content);
+		return tagStyleHtmlContent(Html.Tag.TABLE, style, content);
 	}
 	
 	/**
@@ -63,7 +63,7 @@ public final class HtmlBuilder {
 	 * @return HTML table element as string
 	 */
 	public static String tableClass(String clazz, String... content) {
-		return tagClass(Html.Tag.TABLE, clazz, false, content);
+		return tagClass(Html.Tag.TABLE, clazz, content);
 	}
 	
 	/**
@@ -76,7 +76,7 @@ public final class HtmlBuilder {
 	 * @return HTML table element as string
 	 */
 	public static String tableStyle(String style, String... content) {
-		return tagStyle(Html.Tag.TABLE, style, false, content);
+		return tagStyle(Html.Tag.TABLE, style, content);
 	}
 	
 	/**
@@ -87,7 +87,7 @@ public final class HtmlBuilder {
 	 * @return HTML tr element as string
 	 */
 	public static String trHtmlContent(String... content) {
-		return tag(Html.Tag.TR, true, content);
+		return tagHtmlContent(Html.Tag.TR, content);
 	}
 	
 	/**
@@ -99,7 +99,7 @@ public final class HtmlBuilder {
 	 * @return HTML tr element as string
 	 */
 	public static String tr(String... content) {
-		return tag(Html.Tag.TR, false, content);
+		return tag(Html.Tag.TR, content);
 	}
 	
 	/**
@@ -111,7 +111,7 @@ public final class HtmlBuilder {
 	 * @return HTML tr element as string
 	 */
 	public static String trClassHtmlContent(String clazz, String... content) {
-		return tagClass(Html.Tag.TR, clazz, true, content);
+		return tagClassHtmlContent(Html.Tag.TR, clazz, content);
 	}
 	
 	/**
@@ -123,7 +123,7 @@ public final class HtmlBuilder {
 	 * @return HTML tr element as string
 	 */
 	public static String trStyleHtmlContent(String style, String... content) {
-		return tagStyle(Html.Tag.TR, style, true, content);
+		return tagStyleHtmlContent(Html.Tag.TR, style, content);
 	}
 	
 	/**
@@ -136,7 +136,7 @@ public final class HtmlBuilder {
 	 * @return HTML tr element as string
 	 */
 	public static String trClass(String clazz, String... content) {
-		return tagClass(Html.Tag.TR, clazz, false, content);
+		return tagClass(Html.Tag.TR, clazz, content);
 	}
 	
 	/**
@@ -149,7 +149,7 @@ public final class HtmlBuilder {
 	 * @return HTML tr element as string
 	 */
 	public static String trStyle(String style, String... content) {
-		return tagStyle(Html.Tag.TR, style, false, content);
+		return tagStyle(Html.Tag.TR, style, content);
 	}
 	
 	/**
@@ -161,7 +161,7 @@ public final class HtmlBuilder {
 	 * @return HTML tr element as string
 	 */
 	public static String tdClassHtmlContent(String clazz, String... content) {
-		return tagClass(Html.Tag.TD, clazz, true, content);
+		return tagClassHtmlContent(Html.Tag.TD, clazz, content);
 	}
 	
 	/**
@@ -173,7 +173,7 @@ public final class HtmlBuilder {
 	 * @return HTML td element as string
 	 */
 	public static String tdStyleHtmlContent(String style, String... content) {
-		return tagStyle(Html.Tag.TD, style, true, content);
+		return tagStyleHtmlContent(Html.Tag.TD, style, content);
 	}
 	
 	/**
@@ -186,7 +186,7 @@ public final class HtmlBuilder {
 	 * @return HTML tr element as string
 	 */
 	public static String tdClass(String clazz, String... content) {
-		return tagClass(Html.Tag.TD, clazz, false, content);
+		return tagClass(Html.Tag.TD, clazz, content);
 	}
 	
 	/**
@@ -199,7 +199,7 @@ public final class HtmlBuilder {
 	 * @return HTML td element as string
 	 */
 	public static String tdStyle(String style, String... content) {
-		return tagStyle(Html.Tag.TD, style, false, content);
+		return tagStyle(Html.Tag.TD, style, content);
 	}
 	
 	/**
@@ -211,19 +211,18 @@ public final class HtmlBuilder {
 	 * @return HTML DIV element as string
 	 */
 	public static String divStyleHtmlContent(String style, String... content) {
-		return tagStyle(Html.Tag.DIV, style, true, content);
+		return tagStyleHtmlContent(Html.Tag.DIV, style, content);
 	}
 	/**
 	 * Build a HTML DIV with given CSS class for a string. 
 	 * Use this method if given content contains HTML snippets, prepared with {@link HtmlBuilder#htmlEncode(String...)}.
 	 *
 	 * @param clazz class for div element
-	 * @param contentIsHtml if false content is prepared with {@link HtmlBuilder#htmlEncode(String...)}.
 	 * @param content content string
 	 * @return HTML DIV element as string
 	 */
 	public static String divClassHtmlContent(String clazz, String... content) {
-		return tagClass(Html.Tag.DIV, clazz, true, content);
+		return tagClassHtmlContent(Html.Tag.DIV, clazz, content);
 	}
 	
 	/**
@@ -236,7 +235,7 @@ public final class HtmlBuilder {
 	 * @return HTML DIV element as string
 	 */
 	public static String divStyle(String style, String... content) {
-		return tagStyle(Html.Tag.DIV, style, false, content);
+		return tagStyle(Html.Tag.DIV, style, content);
 	}
 	/**
 	 * Build a HTML DIV with given CSS class for a string. 
@@ -244,12 +243,11 @@ public final class HtmlBuilder {
 	 * as such is being prepared with {@link HtmlBuilder#htmlEncode(String...)}.
 	 *
 	 * @param clazz class for div element
-	 * @param contentIsHtml if false content is prepared with {@link HtmlBuilder#htmlEncode(String...)}.
 	 * @param content content string
 	 * @return HTML DIV element as string
 	 */
 	public static String divClass(String clazz, String... content) {
-		return tagClass(Html.Tag.DIV, clazz, false, content);
+		return tagClass(Html.Tag.DIV, clazz, content);
 	}
 	
 	//Core methods.
@@ -258,70 +256,156 @@ public final class HtmlBuilder {
 	 * Build a String containing a HTML opening tag with no styling of its own, content and closing tag.
 	 *
 	 * @param tag String name of HTML tag
-	 * @param contentIsHtml if false content is prepared with {@link HtmlBuilder#htmlEncode(String...)}.
 	 * @param content content string
 	 * @return HTML tag element as string
 	 */
-	public static String tag(String tag, boolean contentIsHtml, String... content) {
-		return openTag(tag, null, null, contentIsHtml, content) + closeTag(tag);
+	public static String tag(String tag, String... content) {
+		return openTag(tag, null, null, content) + closeTag(tag);
+	}
+	
+	/**
+	 * Build a String containing a HTML opening tag with no styling of its own, content and closing tag.
+	 *
+	 * @param tag String name of HTML tag
+	 * @param content content string
+	 * @return HTML tag element as string
+	 */
+	public static String tagHtmlContent(String tag, String... content) {
+		return openTagHtmlContent(tag, null, null, content) + closeTag(tag);
 	}
 	
 	/**
 	 * Build a String containing a HTML opening tag with given CSS style attribute(s), content and closing tag.
+	 * Content should contain no HTML, because it is prepared with {@link WidgetLayout#htmlEncode(String)}.
 	 *
 	 * @param tag String name of HTML tag
 	 * @param style style for tag (plain CSS)
-	 * @param contentIsHtml if false content is prepared with {@link HtmlBuilder#htmlEncode(String...)}.
 	 * @param content content string
 	 * @return HTML tag element as string
 	 */
-	public static String tagStyle(String tag, String style, boolean contentIsHtml, String... content) {
-		return openTagStyle(tag, style, contentIsHtml, content) + closeTag(tag);
+	public static String tagStyle(String tag, String style, String... content) {
+		return openTagStyle(tag, style, content) + closeTag(tag);
 	}
 	
 	/**
 	 * Build a String containing a HTML opening tag with given CSS class, content and closing tag.
+	 * Content should contain no HTML, because it is prepared with {@link WidgetLayout#htmlEncode(String)}.
 	 *
 	 * @param tag String name of HTML tag
 	 * @param clazz CSS class of the tag
 	 * @param content content string
-	 * @param contentIsHtml if false content is prepared with {@link HtmlBuilder#htmlEncode(String...)}.
 	 * @return HTML tag element as string
 	 */
-	public static String tagClass(String tag, String clazz, boolean contentIsHtml, String... content) {
-		return openTagClass(tag, clazz, contentIsHtml, content) + closeTag(tag);
+	public static String tagClass(String tag, String clazz, String... content) {
+		return openTagClass(tag, clazz, content) + closeTag(tag);
+	}
+	
+	/**
+	 * Build a String containing a HTML opening tag with given CSS style attribute(s), HTML content and closing tag.
+	 *
+	 * @param tag String name of HTML tag
+	 * @param style style for tag (plain CSS)
+	 * @param content content string
+	 * @return HTML tag element as string
+	 */
+	public static String tagStyleHtmlContent(String tag, String style, String... content) {
+		return openTagStyleHtmlContent(tag, style, content) + closeTag(tag);
+	}
+	
+	/**
+	 * Build a String containing a HTML opening tag with given CSS class, HTML content and closing tag.
+	 *
+	 * @param tag String name of HTML tag
+	 * @param clazz CSS class of the tag
+	 * @param content content string
+	 * @return HTML tag element as string
+	 */
+	public static String tagClassHtmlContent(String tag, String clazz, String... content) {
+		return openTagClassHtmlContent(tag, clazz, content) + closeTag(tag);
+	}
+	
+	/**
+	 * Build a String containing a HTML opening tag with given CSS style attribute(s) and 
+	 * concats the given HTML content. 
+	 *
+	 * @param tag String name of HTML tag
+	 * @param style style for tag (plain CSS)
+	 * @param content content string
+	 * @return HTML tag element as string
+	 */
+	public static String openTagStyleHtmlContent(String tag, String style, String... content) {
+		return openTagHtmlContent(tag, null, style, content);
+	}
+	
+	/**
+	 * Build a String containing a HTML opening tag with given CSS class and concats the given HTML content. 
+	 *
+	 * @param tag String name of HTML tag
+	 * @param clazz CSS class of the tag
+	 * @param content content string
+	 * @return HTML tag element as string
+	 */
+	public static String openTagClassHtmlContent(String tag, String clazz, String... content) {
+		return openTagHtmlContent(tag, clazz, null, content);
 	}
 	
 	/**
 	 * Build a String containing a HTML opening tag with given CSS style attribute(s) and concats the given content. 
-	 * Content is prepared with {@link WidgetLayout#htmlEncode(String)}.
+	 * Content should contain no HTML, because it is prepared with {@link WidgetLayout#htmlEncode(String)}.
 	 *
 	 * @param tag String name of HTML tag
 	 * @param style style for tag (plain CSS)
-	 * @param contentIsHtml if false content is prepared with {@link HtmlBuilder#htmlEncode(String...)}.
 	 * @param content content string
 	 * @return HTML tag element as string
 	 */
-	public static String openTagStyle(String tag, String style, boolean contentIsHtml, String... content) {
-		return openTag(tag, null, style, contentIsHtml, content);
+	public static String openTagStyle(String tag, String style, String... content) {
+		return openTag(tag, null, style, content);
 	}
 	
 	/**
 	 * Build a String containing a HTML opening tag with given CSS class and concats the given content. 
-	 * Content is prepared with {@link WidgetLayout#htmlEncode(String)}.
+	 * Content should contain no HTML, because it is prepared with {@link WidgetLayout#htmlEncode(String)}.
 	 *
 	 * @param tag String name of HTML tag
 	 * @param clazz CSS class of the tag
-	 * @param contentIsHtml if false content is prepared with {@link HtmlBuilder#htmlEncode(String...)}.
 	 * @param content content string
 	 * @return HTML tag element as string
 	 */
-	public static String openTagClass(String tag, String clazz, boolean contentIsHtml, String... content) {
-		return openTag(tag, clazz, null, contentIsHtml, content);
+	public static String openTagClass(String tag, String clazz, String... content) {
+		return openTag(tag, clazz, null, content);
 	}
 	
 	/**
-	 * Build a String containing a HTML opening tag with given CSS class and/or style and concats the given content. 
+	 * Build a String containing a HTML opening tag with given CSS class and/or style and concats the given content.
+	 * Content should contain no HTML, because it is prepared with {@link WidgetLayout#htmlEncode(String)}. 
+	 *
+	 * @param tag String name of HTML tag
+	 * @param clazz CSS class of the tag
+	 * @param style style for tag (plain CSS)
+	 * @param content content string
+	 * @return HTML tag element as string
+	 */
+	public static String openTag(String tag, String clazz, String style, String... content) {
+		return openTag(tag, clazz, style, false, content);
+	}
+	
+	/**
+	 * Build a String containing a HTML opening tag with given CSS class and/or 
+	 * style and concats the given HTML content. 
+	 *
+	 * @param tag String name of HTML tag
+	 * @param clazz CSS class of the tag
+	 * @param style style for tag (plain CSS)
+	 * @param content content string
+	 * @return HTML tag element as string
+	 */
+	public static String openTagHtmlContent(String tag, String clazz, String style, String... content) {
+		return openTag(tag, clazz, style, true, content);
+	}
+	
+	/**
+	 * Build a String containing a HTML opening tag with given CSS class and/or 
+	 * style and concats the given content. 
 	 *
 	 * @param tag String name of HTML tag
 	 * @param clazz CSS class of the tag
@@ -330,13 +414,14 @@ public final class HtmlBuilder {
 	 * @param content content string
 	 * @return HTML tag element as string
 	 */
-	public static String openTag(String tag, String clazz, String style, boolean contentIsHtml, String... content) {
-		StringBuilder result = new StringBuilder("<" + tag);
+	private static String openTag(String tag, String clazz, String style, boolean contentIsHtml, String... content) {
+		StringBuilder result = new StringBuilder("<").append(tag);
 		if (clazz != null && !"".equals(clazz)) {
-			result.append(" class='" + clazz + "'");
+			result.append(" ").append(Html.Attribute.CLASS).append("='").append(clazz).append("'");
 		}
+		
 		if (style != null && !"".equals(style)) {
-			result.append(" style='" + style + "'");
+			result.append(" ").append(Html.Attribute.STYLE).append("='").append(style).append("'");
 		}
 		result.append(">");
 		if (content != null && content.length > 0) {
--- face/geomajas-face-common-gwt/common-gwt/src/test/java/org/geomajas/gwt/client/util/HtmlBuilderTest.java
+++ face/geomajas-face-common-gwt/common-gwt/src/test/java/org/geomajas/gwt/client/util/HtmlBuilderTest.java
@@ -1,34 +1,203 @@
+
 package org.geomajas.gwt.client.util;
 
-
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
-/**
- * Test for {@link HtmlBuilder}.
- *
- * @author Emiel Ackermann
- */
 public class HtmlBuilderTest {
 
 	@Test
 	public void openTagTest() {
-		String openP = HtmlBuilder.openTag(Html.Tag.P, "testClass", "extra:BIG", false, "blabla");
+		String openP = HtmlBuilder.openTag(Html.Tag.P, "testClass", "extra:BIG", "blabla");
 		Assert.assertTrue("<p class='testClass' style='extra:BIG'>blabla".equals(openP));
 	}
-	
+
 	@Test
 	public void closeTagTest() {
 		String closeP = HtmlBuilder.closeTag(Html.Tag.P);
 		Assert.assertTrue("</p>".equals(closeP));
 	}
-	
+
 	@Test
 	public void divClassTest() {
 		String div = HtmlBuilder.divClass("testClass", "blabla");
 		Assert.assertTrue("<div class='testClass'>blabla</div>".equals(div));
 	}
-	
+
+	@Test
+	public void divClassHtmlContentTest() {
+		String div = HtmlBuilder.divClassHtmlContent("testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<div class='testClass'><s>blabla</s></div>".equals(div));
+}
+
+	@Test
+	public void divStyleTest() {
+		String div = HtmlBuilder.divStyle("testClass", "blabla");
+		Assert.assertTrue("<div style='testClass'>blabla</div>".equals(div));
+}
+
+	@Test
+	public void divStyleHtmlContentTest() {
+		String div = HtmlBuilder.divStyleHtmlContent("testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<div style='testClass'><s>blabla</s></div>".equals(div));
+}
+
+	@Test
+	public void htmlEncodeTest() {
+		String htmlEncode = HtmlBuilder.htmlEncode("<&>");
+		Assert.assertTrue("&lt;&amp;&gt;".equals(htmlEncode));
+}
+
+	@Test
+	public void openTagClassTest() {
+		String openTagClass = HtmlBuilder.openTagClass(Html.Tag.SCRIPT, "testClass", "blabla");
+		Assert.assertTrue("<script class='testClass'>blabla".equals(openTagClass));
+}
+
+	@Test
+	public void openTagClassHtmlContentTest() {
+		String openTagClassHtmlContent = HtmlBuilder.openTagClassHtmlContent(Html.Tag.SCRIPT, "testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<script class='testClass'><s>blabla</s>".equals(openTagClassHtmlContent));
+}
+
+	@Test
+	public void openTagHtmlContentTest() {
+		String openTagHtmlContent = HtmlBuilder.openTagHtmlContent(Html.Tag.SCRIPT, "testClass", "testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<script class='testClass' style='testClass'><s>blabla</s>".equals(openTagHtmlContent));
+}
+
+	@Test
+	public void openTagStyleTest() {
+		String openTagStyle = HtmlBuilder.openTagStyle(Html.Tag.SCRIPT, "testClass", "blabla");
+		Assert.assertTrue("<script style='testClass'>blabla".equals(openTagStyle));
+}
+
+	@Test
+	public void openTagStyleHtmlContentTest() {
+		String openTagStyleHtmlContent = HtmlBuilder.openTagStyleHtmlContent(Html.Tag.SCRIPT, "testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<script style='testClass'><s>blabla</s>".equals(openTagStyleHtmlContent));
+}
+
+	@Test
+	public void tableClassTest() {
+		String tableClass = HtmlBuilder.tableClass("testClass", "blabla");
+		Assert.assertTrue("<table class='testClass'>blabla</table>".equals(tableClass));
+}
+
+	@Test
+	public void tableClassHtmlContentTest() {
+		String tableClassHtmlContent = HtmlBuilder.tableClassHtmlContent("testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<table class='testClass'><s>blabla</s></table>".equals(tableClassHtmlContent));
+}
+
+	@Test
+	public void tableStyleTest() {
+		String tableStyle = HtmlBuilder.tableStyle("testClass", "blabla");
+		Assert.assertTrue("<table style='testClass'>blabla</table>".equals(tableStyle));
+}
+
+	@Test
+	public void tableStyleHtmlContentTest() {
+		String tableStyleHtmlContent = HtmlBuilder.tableStyleHtmlContent("testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<table style='testClass'><s>blabla</s></table>".equals(tableStyleHtmlContent));
+}
+
+	@Test
+	public void tagTest() {
+		String tag = HtmlBuilder.tag(Html.Tag.S, "blabla");
+		Assert.assertTrue("<s>blabla</s>".equals(tag));
+}
+
+	@Test
+	public void tagClassTest() {
+		String tagClass = HtmlBuilder.tagClass(Html.Tag.SCRIPT, "testClass", "blabla");
+		Assert.assertTrue("<script class='testClass'>blabla</script>".equals(tagClass));
+}
+
+	@Test
+	public void tagClassHtmlContent() {
+		String tagClassHtmlContent = HtmlBuilder.tagClassHtmlContent(Html.Tag.SCRIPT, "testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<script class='testClass'><s>blabla</s></script>".equals(tagClassHtmlContent));
+}
+
+	@Test
+	public void tagHtmlContentTest() {
+		String tagHtmlContent = HtmlBuilder.tagHtmlContent(Html.Tag.SCRIPT, HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<script><s>blabla</s></script>".equals(tagHtmlContent));
+}
+
+	@Test
+	public void tagStyleTest() {
+		String tagStyle = HtmlBuilder.tagStyle(Html.Tag.SCRIPT, "testClass", "blabla");
+		Assert.assertTrue("<script style='testClass'>blabla</script>".equals(tagStyle));
+}
+
+	@Test
+	public void tagStyleHtmlContentTest() {
+		String tagStyleHtmlContent = HtmlBuilder.tagStyleHtmlContent(Html.Tag.SCRIPT, "testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<script style='testClass'><s>blabla</s></script>".equals(tagStyleHtmlContent));
+}
+
+	@Test
+	public void tdClassTest() {
+		String tdClass = HtmlBuilder.tdClass("testClass", "blabla");
+		Assert.assertTrue("<td class='testClass'>blabla</td>".equals(tdClass));
+}
+
+	@Test
+	public void tdClassHtmlContentTest() {
+		String tdClassHtmlContent = HtmlBuilder.tdClassHtmlContent("testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<td class='testClass'><s>blabla</s></td>".equals(tdClassHtmlContent));
+}
+
+	@Test
+	public void tdStyleTest() {
+		String tdStyle = HtmlBuilder.tdStyle("testClass", "blabla");
+		Assert.assertTrue("<td style='testClass'>blabla</td>".equals(tdStyle));
+}
+
+	@Test
+	public void tdStyleHtmlContentTest() {
+		String tdStyleHtmlContent = HtmlBuilder.tdStyleHtmlContent("testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<td style='testClass'><s>blabla</s></td>".equals(tdStyleHtmlContent));
+}
+
+	@Test
+	public void trTest() {
+		String tr = HtmlBuilder.tr("blabla");
+		Assert.assertTrue("<tr>blabla</tr>".equals(tr));
+}
+
+	@Test
+	public void trClassTest() {
+		String trClass = HtmlBuilder.trClass("testClass", "blabla");
+		Assert.assertTrue("<tr class='testClass'>blabla</tr>".equals(trClass));
+}
+
+	@Test
+	public void trClassHtmlContentTest() {
+		String trClassHtmlContent = HtmlBuilder.trClassHtmlContent("testClass", HtmlBuilder.tag(Html.Tag.S, "blabla"));
+		Assert.assertTrue("<tr class='testClass'><s>blabla</s></tr>".equals(trClassHtmlContent));
+}
+
+	@Test
+	public void trHtmlContentTest() {
+		String tr = HtmlBuilder.trHtmlContent(HtmlBuilder.tdStyle("testStyle", "blabla"));
+		Assert.assertTrue("<tr><td style='testStyle'>blabla</td></tr>".equals(tr));
+	}
+
+	@Test
+	public void trStyleTest() {
+		String tr = HtmlBuilder.trStyle("testStyle", "blabla");
+		Assert.assertTrue("<tr style='testStyle'>blabla</tr>".equals(tr));
+	}
+
+	@Test
+	public void trStyleHtmlContentTest() {
+		String tr = HtmlBuilder.trStyleHtmlContent("testStyle", HtmlBuilder.tdStyle("testStyle", "blabla"));
+		Assert.assertTrue("<tr style='testStyle'><td style='testStyle'>blabla</td></tr>".equals(tr));
+	}
+
 	@Test
 	public void tdTrTableTest() {
 		String td = HtmlBuilder.tdStyle("testStyle", "blabla");
