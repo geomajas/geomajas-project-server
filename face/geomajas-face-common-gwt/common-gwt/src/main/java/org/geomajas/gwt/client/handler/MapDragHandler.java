1cd064554602579b4fced80e8712c2e9a0530086
Pieter De Graef <pieter.degraef@geosparc.com>
2011-09-21 10:33:59 +0000

    GWT-309: Create a uniform way of supporting both touch events and mouse events in the controllers

---
+++ face/geomajas-face-common-gwt/common-gwt/src/main/java/org/geomajas/gwt/client/handler/MapDragHandler.java
@@ -0,0 +1,31 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.gwt.client.handler;
+
+import org.geomajas.geometry.Coordinate;
+
+import com.google.gwt.event.dom.client.HumanInputEvent;
+
+/**
+ * Handler for dragging on a map.<br>
+ * TODO Is this really needed??
+ * 
+ * @author Pieter De Graef
+ */
+public interface MapDragHandler {
+
+	void onDragStart(HumanInputEvent<?> event, Coordinate position);
+
+	void onDragMove(HumanInputEvent<?> event, Coordinate position);
+
+	void onDragEnd(HumanInputEvent<?> event, Coordinate position);
+}
---
+++ face/geomajas-face-common-gwt/common-gwt/src/main/java/org/geomajas/gwt/client/handler/MapHandler.java
@@ -0,0 +1,22 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.gwt.client.handler;
+
+import com.google.gwt.event.shared.EventHandler;
+
+/**
+ * Extension of the general event handler to support mouse and touch events under a single banner.
+ * 
+ * @author Pieter De Graef
+ */
+public interface MapHandler extends EventHandler {
+}
