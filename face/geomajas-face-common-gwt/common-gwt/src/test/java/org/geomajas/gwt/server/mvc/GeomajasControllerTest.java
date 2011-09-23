51d0c1462c6829e83ec74d936cfb34386e8d7207
Jan De Moerloose <jan.demoerloose@geosparc.com>
2011-09-23 07:20:42 +0000

    CGWT-5: compiler error + headers added

--- face/geomajas-face-common-gwt/common-gwt/src/test/java/org/geomajas/gwt/server/mvc/GeomajasControllerTest.java
+++ face/geomajas-face-common-gwt/common-gwt/src/test/java/org/geomajas/gwt/server/mvc/GeomajasControllerTest.java
@@ -1,3 +1,13 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
 package org.geomajas.gwt.server.mvc;
 
 import java.io.IOException;
@@ -17,7 +27,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/org/geomajas/spring/geomajasContext.xml",
-		"/org/geomajas/gwt/server/mvc/mockWebContext.xml", })
+		"/org/geomajas/gwt/server/mvc/mockWebContext.xml"})
 public class GeomajasControllerTest {
 
 	@Autowired
