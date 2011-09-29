4d13e11b41383a607de296e35c9e7eaf0d367179
Jan De Moerloose <jan.demoerloose@geosparc.com>
2011-09-29 07:22:39 +0000

    fix build

--- backend/command/src/test/java/org/geomajas/command/configuration/RefreshConfigurationCommandTest.java
+++ backend/command/src/test/java/org/geomajas/command/configuration/RefreshConfigurationCommandTest.java
@@ -15,11 +15,11 @@ import org.geomajas.command.CommandDispatcher;
 import org.geomajas.command.dto.RefreshConfigurationRequest;
 import org.geomajas.command.dto.RefreshConfigurationResponse;
 import org.geomajas.service.TestRecorder;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -41,6 +41,7 @@ public class RefreshConfigurationCommandTest {
 	private TestRecorder recorder;
 
 	@Test
+	@DirtiesContext
 	public void testRefreshConfiguration() throws Exception {
 		Assert.assertEquals("", recorder.matches("test", "postConstruct"));
 		recorder.clear();
