bfbc7662565827ccf111d0b24ee1d72fa232570d
Jan De Moerloose <jan.demoerloose@geosparc.com>
2011-10-05 06:35:44 +0000

    RAST-45: added RenderedImageIcon to convert renderedImage to icon

---
+++ plugin/geomajas-plugin-rasterizing/rasterizing/src/main/java/org/geomajas/plugin/rasterizing/legend/RenderedImageIcon.java
@@ -0,0 +1,58 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+package org.geomajas.plugin.rasterizing.legend;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.RenderedImage;
+
+import javax.swing.Icon;
+
+/**
+ * Fixed size Swing component that graphically represents an SLD rule.
+ * 
+ * @author Jan De Moerloose
+ * 
+ */
+public class RenderedImageIcon implements Icon {
+
+	private RenderedImage renderedImage;
+
+	private int width;
+
+	private int height;
+
+	public RenderedImageIcon(RenderedImage renderedImage, int width, int height) {
+		this.renderedImage = renderedImage;
+		this.width = width;
+		this.height = height;
+	}
+
+	public void paintIcon(Component c, Graphics g, int x, int y) {
+		Graphics2D g2d = (Graphics2D) g;
+		AffineTransform scale = AffineTransform.getScaleInstance((double) width / renderedImage.getWidth(),
+				(double) height / renderedImage.getHeight());
+		AffineTransform translate = AffineTransform.getTranslateInstance(x, y);
+		scale.concatenate(translate);
+		g2d.drawRenderedImage(renderedImage, scale);
+	}
+
+	public int getIconWidth() {
+		return width;
+	}
+
+	public int getIconHeight() {
+		return height;
+	}
+
+}
---
+++ plugin/geomajas-plugin-rasterizing/rasterizing/src/test/java/org/geomajas/plugin/rasterizing/legend/RenderedImageIconTest.java
@@ -0,0 +1,38 @@
+package org.geomajas.plugin.rasterizing.legend;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RenderedImageIconTest {
+
+	@Test
+	public void testPaint() throws IOException {
+		BufferedImage img = ImageIO.read(getClass().getResourceAsStream(
+				"/org/geomajas/plugin/rasterizing/images/testIcon.png"));
+		RenderedImageIcon icon = new RenderedImageIcon(img, 24, 24);
+
+		BufferedImage copy = new BufferedImage(24, 24, BufferedImage.TYPE_4BYTE_ABGR);
+		icon.paintIcon(null, copy.getGraphics(), 0, 0);
+		copy.getGraphics().dispose();
+
+		ImageIO.write(copy, "png", new FileOutputStream("my.png"));
+		
+		Raster r1 = img.getData();
+		Raster r2 = copy.getData();
+		for (int i = 0; i < 24; i++) {
+			for (int j = 0; j < 24; j++) {
+				int[] p1 = r1.getPixel(i, j, (int[]) null);
+				int[] p2 = r2.getPixel(i, j, (int[]) null);
+				Assert.assertArrayEquals(p2, p1);
+			}
+		}
+
+	}
+}
