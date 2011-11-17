dadfc30da40952b7b561ea332fcd18c18921f62b
Pieter De Graef <pieter.degraef@geosparc.com>
2011-11-17 11:22:29 +0000

    Renaming / refactoring / .... general cleanup for the JS plugin.

---
+++ plugin/geomajas-plugin-javascript-api/geomajas-plugin-javascript-api/src/main/java/org/geomajas/plugin/jsapi/client/spatial/GeometryService.java
@@ -0,0 +1,34 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.plugin.jsapi.client.spatial;
+
+import org.geomajas.annotation.Api;
+import org.timepedia.exporter.client.Exportable;
+
+/**
+ * Service that defines all possible methods on geometries.
+ * 
+ * @author Pieter De Graef
+ * @since 1.0.0
+ */
+@Api(allMethods = true)
+public interface GeometryService extends Exportable {
+
+	/**
+	 * Return the bounding box that defines the outer most border of a geometry.
+	 * 
+	 * @param geometry
+	 *            The geometry for which to calculate the bounding box.
+	 * @return The outer bounds for the given geometry.
+	 */
+	org.geomajas.geometry.Bbox getBounds(org.geomajas.geometry.Geometry geometry);
+}
