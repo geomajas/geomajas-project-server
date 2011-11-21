481f14a558fba6f49b12e57caf377f72d4c3150b
Joachim Van der Auwera <joachim@progs.be>
2011-11-21 08:28:13 +0000

    GWT-327 comply with coding style: package name should be singular, clean up messages a little

---
+++ face/geomajas-face-gwt/example/src/main/java/org/geomajas/gwt/example/client/sample/attribute/AttributeCustomForm.java
@@ -0,0 +1,58 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.gwt.example.client.sample.attribute;
+
+import org.geomajas.configuration.AttributeInfo;
+import org.geomajas.gwt.client.map.layer.VectorLayer;
+import org.geomajas.gwt.client.widget.attribute.DefaultFeatureForm;
+
+import com.smartgwt.client.data.DataSource;
+import com.smartgwt.client.widgets.form.fields.FormItem;
+import com.smartgwt.client.widgets.form.fields.RowSpacerItem;
+
+/**
+ * Custom attribute form.
+ * 
+ * @author Jan De Moerloose
+ */
+// @extract-start CustomFeatureForm, CustomFeatureForm
+// Creating the custom form:
+public class AttributeCustomForm extends DefaultFeatureForm {
+
+	public AttributeCustomForm(VectorLayer vectorLayer) {
+		super(vectorLayer);
+	}
+
+	@Override
+	protected FormItem createItem(AttributeInfo info) {
+		FormItem formItem = super.createItem(info);
+		formItem.setWidth("*");
+		if ("dateAttr".equals(info.getName())) {
+			// The data attribute will span all 4 columns:
+			formItem.setColSpan(4);
+		}
+		return formItem;
+	}
+
+	@Override
+	protected void prepareForm(FormItemList formItems, DataSource source) {
+		// Quickly insert a row spacer before the 'stringAttr' item (which is the text area).
+		formItems.insertBefore("stringAttr", new RowSpacerItem());
+		getWidget().setNumCols(4);
+		getWidget().setWidth(450);
+		getWidget().setColWidths(100, 180, 20, 150);
+		getWidget().setGroupTitle("Custom Attribute Form");
+		getWidget().setIsGroup(true);
+	}
+	// @extract-end
+
+}
---
+++ face/geomajas-face-gwt/example/src/main/java/org/geomajas/gwt/example/client/sample/layer/GeoToolsSample.java
@@ -0,0 +1,68 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.gwt.example.client.sample.layer;
+
+import com.google.gwt.core.client.GWT;
+import org.geomajas.gwt.example.base.SamplePanel;
+import org.geomajas.gwt.example.base.SamplePanelFactory;
+import org.geomajas.gwt.client.controller.PanController;
+import org.geomajas.gwt.client.widget.MapWidget;
+
+import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.layout.VLayout;
+import org.geomajas.gwt.example.client.sample.i18n.SampleMessages;
+
+/**
+ * <p>
+ * Sample that shows a map with a GeoTools layer.
+ * </p>
+ * 
+ * @author Frank Wynants
+ */
+public class GeoToolsSample extends SamplePanel {
+
+	private static final SampleMessages MESSAGES = GWT.create(SampleMessages.class);
+
+	public static final SamplePanelFactory FACTORY = new SamplePanelFactory() {
+
+		public SamplePanel createPanel() {
+			return new GeoToolsSample();
+		}
+	};
+
+	public Canvas getViewPanel() {
+		VLayout layout = new VLayout();
+		layout.setWidth100();
+		layout.setHeight100();
+
+		// Map with ID geotoolsMap is defined in the XML configuration. (mapGeoTools.xml)
+		final MapWidget map = new MapWidget("mapGeotools", "gwt-samples");
+
+		// Set a panning controller on the map:
+		map.setController(new PanController(map));
+		layout.addMember(map);
+
+		return layout;
+	}
+
+	public String getDescription() {
+		return MESSAGES.geotoolsDescription();
+	}
+
+	public String[] getConfigurationFiles() {
+		return new String[] { "WEB-INF/mapGeoTools.xml", "WEB-INF/layerCountries110m.xml" };
+	}
+
+	public String ensureUserLoggedIn() {
+		return "luc";
+	}
+}
