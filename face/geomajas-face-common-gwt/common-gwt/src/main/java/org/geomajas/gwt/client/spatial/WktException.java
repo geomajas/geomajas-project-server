77437c38f3bebb7cbc965fc767098e25a671ed8d
Pieter De Graef <pieter.degraef@geosparc.com>
2011-12-10 13:21:13 +0000

    CGWT-16: Create Well Known Text service for parsing and formatting WKT to and from Geometries.

---
+++ face/geomajas-face-common-gwt/common-gwt/src/main/java/org/geomajas/gwt/client/spatial/WktException.java
@@ -0,0 +1,26 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.gwt.client.spatial;
+
+/**
+ * Exception thrown from within the {@link WktService} in case of parsing or formatting errors.
+ * 
+ * @author Pieter De Graef
+ */
+public class WktException extends Exception {
+
+	private static final long serialVersionUID = 100L;
+
+	public WktException(String message) {
+		super(message);
+	}
+}
