b0010d3b5fcf9fc8407b0fd312e7ac5327d2a003
Joachim Van der Auwera <joachim@progs.be>
2011-12-15 14:06:24 +0000

    GBE-264 add SecurityRul which handles login/logout using a Junit rule

---
+++ backend/testdata/src/main/java/org/geomajas/testdata/rule/SecurityRule.java
@@ -0,0 +1,55 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.testdata.rule;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * JUnit rule which makes sure the user is logged in (you can set the token explicitly) and that the user is logged
+ * out at the end. This will work automatically when the AllowedAll security context is used.
+ *
+ * @author Joachim Van der Auwera
+ */
+public class SecurityRule implements TestRule {
+
+	@Autowired
+	private org.geomajas.security.SecurityManager securityManager;
+	
+	private String token;
+
+	public Statement apply(final Statement statement, final Description description) {
+		return new Statement() {
+			@Override
+			public void evaluate() throws Throwable {
+				securityManager.createSecurityContext(token);
+				try {
+					statement.evaluate();
+				} finally {
+				    securityManager.clearSecurityContext();
+				}
+			}
+		};
+	}
+
+	/**
+	 * Token which should be used to login.
+	 *
+	 * @param token token
+	 */
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
