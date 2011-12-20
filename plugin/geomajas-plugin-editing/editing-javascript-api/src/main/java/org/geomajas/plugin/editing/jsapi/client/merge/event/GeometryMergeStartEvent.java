b5b5ff8f2d7341d33a3aa5a798f1e62673814979
Pieter De Graef <pieter.degraef@geosparc.com>
2011-12-20 10:21:45 +0000

    Finishing JavaDoc and ensure consistent names for editing plugin.

---
+++ plugin/geomajas-plugin-editing/editing-javascript-api/src/main/java/org/geomajas/plugin/editing/jsapi/client/merge/event/GeometryMergeStartEvent.java
@@ -0,0 +1,41 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.plugin.editing.jsapi.client.merge.event;
+
+import org.geomajas.annotation.Api;
+import org.geomajas.plugin.jsapi.client.event.JsEvent;
+import org.timepedia.exporter.client.Export;
+import org.timepedia.exporter.client.ExportPackage;
+import org.timepedia.exporter.client.Exportable;
+
+/**
+ * Event that reports the merging process for geometries has begun.
+ * 
+ * @author Pieter De Graef
+ * @since 1.0.0
+ */
+@Api(allMethods = true)
+@Export
+@ExportPackage("org.geomajas.plugin.editing.merging.event")
+public class GeometryMergeStartEvent extends JsEvent<GeometryMergeStartHandler> implements Exportable {
+
+	public GeometryMergeStartEvent() {
+	}
+
+	public Class<GeometryMergeStartHandler> getType() {
+		return GeometryMergeStartHandler.class;
+	}
+
+	protected void dispatch(GeometryMergeStartHandler handler) {
+		handler.onGeometryMergingStart(this);
+	}
+}
