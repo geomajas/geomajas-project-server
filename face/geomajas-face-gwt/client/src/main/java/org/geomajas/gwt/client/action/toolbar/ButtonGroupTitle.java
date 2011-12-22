581623bafc1db2ff2bf154f4bc0e49cbaa22fe59
Emiel Ackermann <emiela>
2011-12-22 07:01:51 +0000

    GUW-25: CheckStyle fixes

--- face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/action/toolbar/ButtonGroupTitle.java
+++ face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/action/toolbar/ButtonGroupTitle.java
@@ -1,3 +1,14 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
 package org.geomajas.gwt.client.action.toolbar;
 
 import org.geomajas.gwt.client.action.ConfigurableAction;
@@ -7,7 +18,7 @@ import org.geomajas.gwt.client.action.ToolbarBaseAction;
  * 
  * @author Emiel Ackermann
  */
-public class ButtonGroupTitle extends ToolbarBaseAction implements ConfigurableAction{
+public class ButtonGroupTitle extends ToolbarBaseAction implements ConfigurableAction {
 
 	private String buttonLayout;
 
