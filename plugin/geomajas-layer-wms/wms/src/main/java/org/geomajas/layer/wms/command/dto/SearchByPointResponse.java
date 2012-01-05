87a7e2aa6c3528a16c952ecf95254707495cec74
Joachim Van der Auwera <joachim@progs.be>
2012-01-05 19:06:08 +0000

    javadoc and assure features are serialized by GWT

--- plugin/geomajas-layer-wms/wms/src/main/java/org/geomajas/layer/wms/command/dto/SearchByPointResponse.java
+++ plugin/geomajas-layer-wms/wms/src/main/java/org/geomajas/layer/wms/command/dto/SearchByPointResponse.java
@@ -31,19 +31,33 @@ public class SearchByPointResponse extends CommandResponse {
 
 	private static final long serialVersionUID = 180L;
 
-	private final Map<String, List<Feature>> featureMap = new HashMap<String, List<Feature>>();
+	private Map<String, List<Feature>> featureMap = new HashMap<String, List<Feature>>(); // NOSONAR GWT will not serialize when final
 
+	/** No arguments constructor. */
 	public SearchByPointResponse() {
 		super();
 	}
 
+	/**
+	 * Add features for a layer. Only adds the features for a server id once.
+	 *
+	 * @param layerId layer id
+	 * @param features features
+	 * @return true when features were added
+	 */
 	public boolean addLayer(String layerId, List<Feature> features) {
 		if (!featureMap.containsKey(layerId)) {
 			featureMap.put(layerId, features);
+			return true;
 		}
 		return false;
 	}
 
+	/**
+	 * Get the features map.
+	 *
+	 * @return map with layer id as key and list of features as value
+	 */
 	public Map<String, List<Feature>> getFeatureMap() {
 		return featureMap;
 	}
