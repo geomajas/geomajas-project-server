76c3bffa3737a7d31a4891ad7ed13a4f69e0ac05
Emiel Ackermann <emiela>
2012-01-06 10:47:22 +0000

    GUW-25: Added a ToolbarCanvas implementation for the ribbon. GeocoderWidget added to the Microsoft style ribbon.

---
+++ plugin/geomajas-widget-utility/utility-gwt/src/main/java/org/geomajas/widget/utility/gwt/client/ribbon/RibbonColumnCanvas.java
@@ -0,0 +1,111 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2012 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.widget.utility.gwt.client.ribbon;
+
+import org.geomajas.gwt.client.action.ConfigurableAction;
+import org.geomajas.gwt.client.action.ToolbarCanvas;
+import org.geomajas.widget.utility.common.client.ribbon.RibbonColumn;
+import org.geomajas.widget.utility.gwt.client.action.ToolbarButtonCanvas;
+
+import com.google.gwt.user.client.ui.Widget;
+import com.smartgwt.client.types.VerticalAlignment;
+import com.smartgwt.client.widgets.Canvas;
+/**
+ * ...
+ * 
+ * @author Emiel Ackermann
+ */
+public class RibbonColumnCanvas implements RibbonColumn {
+	
+	private final ToolbarButtonCanvas toolbarButtonCanvas;
+	private Canvas canvas;
+
+	public RibbonColumnCanvas(ToolbarButtonCanvas toolbarButtonCanvas) {
+		this.toolbarButtonCanvas = toolbarButtonCanvas;
+		canvas = ((ToolbarCanvas) toolbarButtonCanvas.getToolbarAction()).getCanvas();
+	}
+
+	/** {@inheritDoc} */
+	public Widget asWidget() {
+		canvas.setAutoHeight();
+		VerticalAlignment verticalAlignment = toolbarButtonCanvas.getVerticalAlignment();
+		canvas.setLayoutAlign(verticalAlignment);
+		return canvas;
+	}
+	
+	/** {@inheritDoc} */
+	public void configure(String key, String value) {
+		if (toolbarButtonCanvas instanceof ConfigurableAction) {
+			ConfigurableAction ca = (ConfigurableAction) toolbarButtonCanvas;
+			ca.configure(key, value);
+		}
+	}
+	
+	/** {@inheritDoc} */
+	public boolean isEnabled() {
+		return !canvas.isDisabled();
+	}
+	
+	/** {@inheritDoc} */
+	public void setEnabled(boolean enabled) {
+		canvas.setDisabled(!enabled);
+	}
+
+	/**
+	 * Widget creation is being handled in the canvas returned by 
+	 * {@link org.geomajas.gwt.client.action.ToolbarCanvas#getCanvas()},
+	 * so no title can be set.
+	 * 
+	 * @param does nothing
+	 */
+	public void setShowTitles(boolean showTitles) {
+	}
+
+	/**
+	 * Widget creation is being handled in the canvas returned by 
+	 * {@link org.geomajas.gwt.client.action.ToolbarCanvas#getCanvas()},
+	 * so null is returned.
+	 * 
+	 * @return null
+	 */
+	public boolean isShowTitles() {
+		return (Boolean) null;
+	}
+	/**
+	 * Widget creation is being handled in the canvas returned by 
+	 * {@link org.geomajas.gwt.client.action.ToolbarCanvas#getCanvas()},
+	 * so no title can be set.
+	 * 
+	 * @param does nothing
+	 */
+	public void setTitleAlignment(TitleAlignment titleAlignment) {
+	}
+	/**
+	 * Widget creation is being handled in the canvas returned by 
+	 * {@link org.geomajas.gwt.client.action.ToolbarCanvas#getCanvas()},
+	 * so null is returned.
+	 * 
+	 * @return null
+	 */
+	public TitleAlignment getTitleAlignment() {
+		return null;
+	}
+	/**
+	 * Widget creation is being handled in the canvas returned by 
+	 * {@link org.geomajas.gwt.client.action.ToolbarCanvas#getCanvas()},
+	 * so no title can be set.
+	 * 
+	 * @param does nothing
+	 */
+	public void setButtonBaseStyle(String buttonBaseStyle) {
+	}
+}
