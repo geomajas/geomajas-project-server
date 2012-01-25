ef270fad2f78cd46d2f11aa5e8568912d9fc4b4f
Joachim Van der Auwera <joachim@progs.be>
2012-01-25 20:08:07 +0000

    update API reference files

--- plugin/geomajas-plugin-geocoder/geocoder-gwt/src/main/java/org/geomajas/plugin/geocoder/client/GeocoderLayout.java
+++ plugin/geomajas-plugin-geocoder/geocoder-gwt/src/main/java/org/geomajas/plugin/geocoder/client/GeocoderLayout.java
@@ -20,7 +20,7 @@ import org.geomajas.annotation.Api;
  * so (if needed) adjust them before you instantiate a {@link GeocoderWidget}
  *
  * @author Emiel Ackermann
- * @since 1.10.0
+ * @since 1.2.0
  */
 @Api(allMethods = true)
 public final class GeocoderLayout {
