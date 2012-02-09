24456d26e0086276c194c648d95519c163f24872
Pieter De Graef <pieter.degraef@geosparc.com>
2012-02-09 10:37:27 +0000

    GEP-35: Implement rendering for editing points in the PureGWT face

---
+++ plugin/geomajas-plugin-editing/editing-puregwt-example/src/main/java/org/geomajas/plugin/editing/puregwt/example/client/Showcase.java
@@ -0,0 +1,142 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2012 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.plugin.editing.puregwt.example.client;
+
+import org.geomajas.geometry.Geometry;
+import org.geomajas.plugin.editing.client.service.GeometryEditState;
+import org.geomajas.plugin.editing.client.service.GeometryIndex;
+import org.geomajas.plugin.editing.client.service.GeometryIndexType;
+import org.geomajas.plugin.editing.puregwt.client.GeometryEditor;
+import org.geomajas.plugin.editing.puregwt.example.client.button.CancelButton;
+import org.geomajas.puregwt.client.GeomajasGinjector;
+import org.geomajas.puregwt.client.map.MapPresenter;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.AttachEvent;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.ResizeLayoutPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Entry point and main class for pure GWT example application.
+ * 
+ * @author Pieter De Graef
+ */
+public class Showcase implements EntryPoint {
+
+	private static final GeomajasGinjector INJECTOR = GWT.create(GeomajasGinjector.class);
+
+	private MapPresenter mapPresenter;
+
+	private GeometryEditor editor;
+
+	public void onModuleLoad() {
+		mapPresenter = INJECTOR.getMapPresenter();
+		editor = new GeometryEditor(mapPresenter);
+
+		DockLayoutPanel layout = new DockLayoutPanel(Unit.PX);
+		layout.setSize("100%", "100%");
+
+		// North: buttons
+		HorizontalPanel buttonPanel = new HorizontalPanel();
+		layout.addNorth(buttonPanel, 40);
+
+		// Center: map
+		final ResizeLayoutPanel mapLayout = new ResizeLayoutPanel();
+		mapLayout.setSize("100%", "100%");
+		mapLayout.add(mapPresenter.asWidget());
+
+		// Add an automatic resize handler to set the correct size when the window resizes:
+		Window.addResizeHandler(new ResizeHandler() {
+
+			public void onResize(ResizeEvent event) {
+				mapPresenter.setSize(mapLayout.getOffsetWidth(), mapLayout.getOffsetHeight());
+			}
+		});
+
+		// Calculate the correct size on load:
+		mapLayout.addAttachHandler(new AttachEvent.Handler() {
+
+			public void onAttachOrDetach(AttachEvent event) {
+				Timer timer = new Timer() {
+
+					@Override
+					public void run() {
+						int width = mapLayout.getOffsetWidth();
+						int height = mapLayout.getOffsetHeight();
+						if (width > 100 && height > 100) {
+							mapPresenter.setSize(width, height);
+						} else {
+							schedule(50);
+						}
+					}
+				};
+				timer.run();
+			}
+		});
+		layout.add(mapLayout);
+
+		// Add buttons to the button panel:
+		buttonPanel.add(getBtnFreePoint());
+		buttonPanel.add(getBtnFreeLine());
+		buttonPanel.add(new CancelButton(editor.getEditService()));
+
+		// Initialize the map:
+		mapPresenter.initialize("showcase", "mapOsm");
+		RootPanel.get().add(layout);
+	}
+
+	private Widget getBtnFreePoint() {
+		Button btn = new Button("Draw point");
+		btn.addClickHandler(new ClickHandler() {
+
+			public void onClick(ClickEvent event) {
+				Geometry point = new Geometry(Geometry.POINT, 0, -1);
+				GeometryIndex index = editor.getEditService().getIndexService()
+						.create(GeometryIndexType.TYPE_VERTEX, 0);
+
+				editor.getEditService().start(point);
+				editor.getEditService().setEditingState(GeometryEditState.INSERTING);
+				editor.getEditService().setInsertIndex(index);
+			}
+		});
+		return btn;
+	}
+
+	private Widget getBtnFreeLine() {
+		Button btn = new Button("Draw LineString");
+		btn.addClickHandler(new ClickHandler() {
+
+			public void onClick(ClickEvent event) {
+				Geometry point = new Geometry(Geometry.LINE_STRING, 0, -1);
+				GeometryIndex index = editor.getEditService().getIndexService()
+						.create(GeometryIndexType.TYPE_VERTEX, 0);
+
+				editor.getEditService().start(point);
+				editor.getEditService().setEditingState(GeometryEditState.INSERTING);
+				editor.getEditService().setInsertIndex(index);
+			}
+		});
+		return btn;
+	}
+}
