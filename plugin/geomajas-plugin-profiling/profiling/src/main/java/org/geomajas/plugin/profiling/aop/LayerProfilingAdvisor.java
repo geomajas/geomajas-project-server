bc3502747ecd143a7a134766fe54979b9d7c6858
Joachim Van der Auwera <joachim@progs.be>
2012-02-09 22:01:20 +0000

    GPR-2 initial layer interceptors, not working yet

---
+++ plugin/geomajas-plugin-profiling/profiling/src/main/java/org/geomajas/plugin/profiling/aop/LayerProfilingAdvisor.java
@@ -0,0 +1,60 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2012 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.plugin.profiling.aop;
+
+import org.geomajas.layer.Layer;
+import org.geomajas.layer.feature.FeatureModel;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
+
+import java.lang.reflect.Method;
+
+/**
+ * Pointcut to indicate which layer/feature model methods need to be advised.
+ *
+ * @author Joachim Van der Auwera
+ */
+public class LayerProfilingAdvisor extends StaticMethodMatcherPointcutAdvisor {
+
+	public LayerProfilingAdvisor() {
+		setClassFilter(new ImplementsLayerFilter());
+	}
+	
+	public boolean matches(Method method, Class<?> targetClass) {
+		String name = method.getName();
+		return "paint".equals(name) // RasterLayers
+				|| "create".equals(name) // VectorLayer methods...
+				|| "saveOrUpdate".equals(name)
+				|| "read".equals(name)
+				|| "delete".equals(name)
+				|| "getElements".equals(name)
+				|| "getBounds".equals(name)
+				|| "getAttributes".equals(name) // FeatureModel or VectorLayerAssociationSupport
+				|| "getAttribute".equals(name) // FeatureModel methods...
+				|| "getGeometry".equals(name)
+				|| "setAttributes".equals(name)
+				|| "setGeometry".equals(name)
+				|| "newInstance".equals(name)
+				;
+	}
+
+	/**
+	 * Class filter which selects classes which implement {@link Layer}.
+	 */
+	private static class ImplementsLayerFilter implements ClassFilter {
+
+		/**{@inheritDoc} */
+		public boolean matches(Class<?> clazz) {
+			return clazz.isAssignableFrom(Layer.class) || clazz.isAssignableFrom(FeatureModel.class);
+		}
+	}
+}
---
+++ plugin/geomajas-plugin-profiling/profiling/src/main/java/org/geomajas/plugin/profiling/aop/LayerProfilingInterceptor.java
@@ -0,0 +1,56 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2012 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.plugin.profiling.aop;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.geomajas.annotation.Api;
+import org.geomajas.layer.Layer;
+import org.geomajas.project.profiling.service.ProfilingContainer;
+
+/**
+ * Interceptor which automatically handles the profiling for a method.
+ *
+ * @author Joachim Van der Auwera
+ * @since 1.0.0
+ */
+@Api
+public class LayerProfilingInterceptor implements MethodInterceptor {
+	
+	private ProfilingContainer profilingContainer;
+
+	/**
+	 * Set the {@link ProfilingContainer} to use for the profiling.
+	 *
+	 * @param profilingContainer profiling container
+	 * @since 1.0.0
+	 */
+	@Api
+	public void setProfilingContainer(ProfilingContainer profilingContainer) {
+		this.profilingContainer = profilingContainer;
+	}
+
+	/** {@inheritDoc} */
+	// CHECKSTYLE THROWS_THROWABLE: OFF
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		long start = System.currentTimeMillis();
+		Object result = invocation.proceed();
+		Object thisObject = invocation.getThis();
+		if (thisObject instanceof Layer)  {
+			String group = ((Layer) thisObject).getId();
+			profilingContainer.register(group, System.currentTimeMillis() - start);
+		}
+		return result;
+	}
+	// CHECKSTYLE THROWS_THROWABLE: ON
+
+}
