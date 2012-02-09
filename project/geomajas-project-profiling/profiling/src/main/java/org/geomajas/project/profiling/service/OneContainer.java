047fa66f3d15ee5971df46a8ff9c967d03b00e5a
Joachim Van der Auwera <joachim@progs.be>
2012-02-09 11:54:01 +0000

    PPR-1 ProfilingBean and ProfilingContainer

---
+++ project/geomajas-project-profiling/profiling/src/main/java/org/geomajas/project/profiling/service/OneContainer.java
@@ -0,0 +1,51 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2012 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the Apache
+ * License, Version 2.0. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.project.profiling.service;
+
+import org.geomajas.project.profiling.jmx.ProfilingData;
+
+/**
+ * Set of profiling data.
+ *
+ * @author Joachim Van der Auwera
+ */
+public class OneContainer implements ProfilingData {
+
+	private final long invocationCount;
+	private final long totalRunTime;
+
+	/**
+	 * Construct container for given number of invocations and total run time.
+	 *
+	 * @param invocationCount invocation count
+	 * @param totalRunTime total run time
+	 */
+	public OneContainer(long invocationCount, long totalRunTime) {
+		this.invocationCount = invocationCount;
+		this.totalRunTime = totalRunTime;
+	}
+
+	/** {@inheritDoc} */
+	public long getInvocationCount() {
+		return invocationCount;
+	}
+
+	/** {@inheritDoc} */
+	public long getTotalRunTime() {
+		return totalRunTime;
+	}
+
+	/** {@inheritDoc} */
+	public double getAverageRunTime() {
+		return ((double) totalRunTime) / invocationCount;
+	}
+}
