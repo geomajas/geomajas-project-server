6a8f912e66504bd72625cdda935715bc435c8c80
Pieter De Graef <pieter.degraef@geosparc.com>
2012-03-14 10:22:58 +0000

    PURE-77: Create map specific event bus with registration methods for all available events

--- face/geomajas-face-pure-gwt/client-impl/src/main/java/org/geomajas/puregwt/client/GeomajasGinjector.java
+++ face/geomajas-face-pure-gwt/client-impl/src/main/java/org/geomajas/puregwt/client/GeomajasGinjector.java
@@ -13,6 +13,8 @@ package org.geomajas.puregwt.client;
 
 import org.geomajas.puregwt.client.gfx.GfxUtil;
 import org.geomajas.puregwt.client.map.MapPresenter;
+import org.geomajas.puregwt.client.service.CommandService;
+import org.geomajas.puregwt.client.service.EndPointService;
 
 import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
@@ -25,7 +27,33 @@ import com.google.gwt.inject.client.Ginjector;
 @GinModules(GeomajasGinModule.class)
 public interface GeomajasGinjector extends Ginjector {
 
+	/**
+	 * Create a new empty map. This map still needs to be initialized (so it fetches a configuration).
+	 * 
+	 * @return An empty map.
+	 */
 	MapPresenter getMapPresenter();
 
+	/**
+	 * Get the {@link GfxUtil} singleton. Utility service that helps out when rendering custom shapes on the map.
+	 * 
+	 * @return The {@link GfxUtil} singleton.
+	 */
 	GfxUtil getGfxUtil();
+
+	/**
+	 * Get the {@link EndPointService} singleton. Has pointers to the Geomajas services on the back-end, and allows
+	 * those end-points to be altered in case your server lies elsewhere (or behind a proxy, or...).
+	 * 
+	 * @return The {@link EndPointService} singleton.
+	 */
+	EndPointService getEndPointService();
+
+	/**
+	 * Get the {@link EndPointService} singleton. This service allows for executing commands on the back-end. It is the
+	 * base for all Geomajas client-server communication.
+	 * 
+	 * @return The {@link EndPointService} singleton.
+	 */
+	CommandService getCommandService();
 }
