38f306bea4f568b53aa1524a4deffcd79a130d16
Joachim Van der Auwera <joachim@progs.be>
2012-04-16 23:12:16 +0000

    checkstyle

--- application/geomajas-puregwt-example/src/main/java/org/geomajas/puregwt/client/general/ServerErrorPanel.java
+++ application/geomajas-puregwt-example/src/main/java/org/geomajas/puregwt/client/general/ServerErrorPanel.java
@@ -1,3 +1,14 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2012 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
 package org.geomajas.puregwt.client.general;
 
 import org.geomajas.command.CommandResponse;
@@ -17,7 +28,11 @@ import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-
+/**
+ * Panel showing server errors.
+ *
+ * @author Emiel Ackermann
+ */
 public class ServerErrorPanel extends ContentPanel {
 	
 	protected CommandService commandService;
