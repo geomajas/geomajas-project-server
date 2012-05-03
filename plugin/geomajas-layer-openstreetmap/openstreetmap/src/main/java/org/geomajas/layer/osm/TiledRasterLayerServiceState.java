db3f7bdd4b98d5f584bf60708d391e57a8228e17
Joachim Van der Auwera <joachim@progs.be>
2012-05-03 22:08:54 +0000

    add missing javadoc on api

--- plugin/geomajas-layer-openstreetmap/openstreetmap/src/main/java/org/geomajas/layer/osm/TiledRasterLayerServiceState.java
+++ plugin/geomajas-layer-openstreetmap/openstreetmap/src/main/java/org/geomajas/layer/osm/TiledRasterLayerServiceState.java
@@ -49,58 +49,125 @@ public class TiledRasterLayerServiceState {
 
 	private double[] resolutions;
 
+	/**
+	 * Create base object.
+	 *
+	 * @param tileUrls tile URLs
+	 * @param tileSize tile size in pixels
+	 * @param maxZoomLevel maximum zoom level
+	 */
 	public TiledRasterLayerServiceState(List<String> tileUrls, int tileSize, int maxZoomLevel) {
 		setTileUrls(tileUrls);
 		this.tileSize = tileSize;
 		this.maxZoomLevel = maxZoomLevel;
 	}
 
+	/**
+	 * Get layer id.
+	 *
+	 * @return layer id
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Set layer id.
+	 *
+	 * @param id layer id
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * Get layer configuration.
+	 *
+	 * @return layer configuration
+	 */
 	public RasterLayerInfo getLayerInfo() {
 		return layerInfo;
 	}
 
+	/**
+	 * Set layer configuration.
+	 *
+	 * @param layerInfo layer configuration
+	 */
 	public void setLayerInfo(RasterLayerInfo layerInfo) {
 		this.layerInfo = layerInfo;
 	}
 
+	/**
+	 * Get layer CRS.
+	 *
+	 * @return CRS
+	 */
 	public CoordinateReferenceSystem getCrs() {
 		return crs;
 	}
 
+	/**
+	 * Set layer CRS.
+	 *
+	 * @param crs CRS
+	 */
 	public void setCrs(CoordinateReferenceSystem crs) {
 		this.crs = crs;
 	}
 
+	/**
+	 * Get maximum zoom level.
+	 *
+	 * @return maximum zoom level
+	 */
 	public int getMaxZoomLevel() {
 		return maxZoomLevel;
 	}
 
+	/**
+	 * Set maximum zoom level.
+	 *
+	 * @param maxZoomLevel maximum zoom level
+	 */
 	public void setMaxZoomLevel(int maxZoomLevel) {
 		this.maxZoomLevel = maxZoomLevel < TiledRasterLayerService.MAX_ZOOM_LEVEL ? maxZoomLevel :
 				TiledRasterLayerService.MAX_ZOOM_LEVEL;
 	}
 
+	/**
+	 * Get URL selection strategy.
+	 *
+	 * @return URL selection strategy
+	 */
 	public UrlSelectionStrategy getUrlSelectionStrategy() {
 		return urlStrategy;
 	}
 
+	/**
+	 * Set URL selection strategy.
+	 *
+	 * @param urlStrategy URL selection strategy
+	 */
 	public void setUrlSelectionStrategy(UrlSelectionStrategy urlStrategy) {
 		this.urlStrategy = urlStrategy;
 		urlStrategy.setUrls(tileUrls);
 	}
 
+	/**
+	 * Get possible tile URLs.
+	 *
+	 * @return tile URLs
+	 */
 	public List<String> getTileUrls() {
 		return tileUrls;
 	}
 
+	/**
+	 * Set possible tile URLs.
+	 *
+	 * @param tileUrls tile URLs
+	 */
 	public void setTileUrls(List<String> tileUrls) {
 		this.tileUrls = tileUrls;
 		if (null != urlStrategy) {
@@ -108,14 +175,31 @@ public class TiledRasterLayerServiceState {
 		}
 	}
 
+	/**
+	 * Get the layer maximum bounds.
+	 *
+	 * @return layer maximum bounds
+	 */
 	public Envelope getMaxBounds() {
 		return maxBounds;
 	}
 
+	/**
+	 * Set the layer maximum bounds.
+	 *
+	 * @return layer maximum bounds
+	 */
 	public double[] getResolutions() {
 		return resolutions;
 	}
 
+	/**
+	 * Finish initialization of state object.
+	 *
+	 * @param geoService geo service
+	 * @param converterService converter service
+	 * @throws GeomajasException oops
+	 */
 	public void postConstruct(GeoService geoService, DtoConverterService converterService) throws GeomajasException {
 		if (null == layerInfo) {
 			layerInfo = new RasterLayerInfo();
