1e5a24a1242f75a29ac7fffbca313e1f0cd024eb
Joachim Van der Auwera <joachim@progs.be>
2012-05-08 11:41:00 +0000

    remove unused import

--- backend/impl/src/main/java/org/geomajas/internal/service/ConfigurationServiceImpl.java
+++ backend/impl/src/main/java/org/geomajas/internal/service/ConfigurationServiceImpl.java
@@ -30,7 +30,6 @@ import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanNotOfRequiredTypeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
