886944dfdfb457db9767ee316eba28ae49147c1b
Jan De Moerloose <jan.demoerloose@geosparc.com>
2012-06-12 11:49:08 +0000

    GWT-413: use panToFeatures() of mapModel + apply on all features in analogy to zoomToFeatures()

--- face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/action/toolbar/PanToSelectionAction.java
+++ face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/action/toolbar/PanToSelectionAction.java
@@ -11,17 +11,13 @@
 
 package org.geomajas.gwt.client.action.toolbar;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.geomajas.global.GeomajasConstant;
 import org.geomajas.gwt.client.action.ToolbarAction;
 import org.geomajas.gwt.client.i18n.I18nProvider;
 import org.geomajas.gwt.client.map.feature.Feature;
-import org.geomajas.gwt.client.map.feature.LazyLoadCallback;
-import org.geomajas.gwt.client.map.layer.Layer;
 import org.geomajas.gwt.client.map.layer.VectorLayer;
-import org.geomajas.gwt.client.map.store.VectorLayerStore;
-import org.geomajas.gwt.client.spatial.Bbox;
 import org.geomajas.gwt.client.util.WidgetLayout;
 import org.geomajas.gwt.client.widget.MapWidget;
 
@@ -46,30 +42,13 @@ public class PanToSelectionAction extends ToolbarAction {
 	 * Pan to the selected features.
 	 */
 	public void onClick(ClickEvent clickEvent) {
-		// get the selected VectorLayer
-		Layer<?> selectedLayer = mapWidget.getMapModel().getSelectedLayer();
-		if (selectedLayer instanceof VectorLayer) {
-			// iterate all features of the selected layer
-			VectorLayerStore featureStore = ((VectorLayer) selectedLayer).getFeatureStore();
-			featureStore.getFeatures(GeomajasConstant.FEATURE_INCLUDE_GEOMETRY, new LazyLoadCallback() {
-
-				public void execute(List<Feature> response) {
-					boolean success = false;
-					Bbox selectionBounds = new Bbox(0, 0, 0, 0);
-					for (Feature feature : response) {
-						// if the feature is selected union the bounding box
-						if (feature.isSelected()) {
-							selectionBounds = selectionBounds.union(feature.getGeometry().getBounds());
-							success = true;
-						}
-					}
-
-					// only pan when their where really some items selected
-					if (success) {
-						mapWidget.getMapModel().getMapView().setCenterPosition(selectionBounds.getCenterPoint());
-					}
-				}
-			});
+		List<Feature> features = new ArrayList<Feature>();
+ 		for (VectorLayer layer : mapWidget.getMapModel().getVectorLayers()) {
+			features.addAll(layer.getSelectedFeatureValues());
+		}
+		// only zoom when there where really some items selected
+		if (features.size() > 0) {
+			mapWidget.getMapModel().panToFeatures(features);
 		}
 	}
 }
