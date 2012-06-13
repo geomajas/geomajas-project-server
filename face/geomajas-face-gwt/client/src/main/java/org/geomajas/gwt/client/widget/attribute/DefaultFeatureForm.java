792599d4c51b33ac3396c663eac7c3198baca7b6
Jan De Moerloose <jan.demoerloose@geosparc.com>
2012-06-13 14:16:24 +0000

    GWT-416: added useEditableFromInfo flag

--- face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/widget/attribute/DefaultFeatureForm.java
+++ face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/widget/attribute/DefaultFeatureForm.java
@@ -115,6 +115,8 @@ public class DefaultFeatureForm implements FeatureForm<DynamicForm> {
 	private HandlerManager manager = new HandlerManager(this);
 	
 	private Set<String> editableItemNames = new HashSet<String>(); 
+	
+	private boolean useEditableFromInfo;
 
 	// -------------------------------------------------------------------------
 	// Constructors:
@@ -260,6 +262,18 @@ public class DefaultFeatureForm implements FeatureForm<DynamicForm> {
 		}
 		return validate;
 	}
+	
+	/**
+	 * If this flag is set to true, the editability of an attribute is determined by the attribute info only. If set
+	 * to false, the editability is determined by the actual attribute value.
+	 * 
+	 * @see Attribute#isEditable()
+	 * @see AbstractReadOnlyAttributeInfo#isEditable()
+	 * @param useEditableFromInfo
+	 */
+	public void setUseEditableFromInfo(boolean useEditableFromInfo) {
+		this.useEditableFromInfo = useEditableFromInfo;
+	}
 
 	public boolean silentValidate() {
 		return formWidget.valuesAreValid(false);
@@ -306,7 +320,7 @@ public class DefaultFeatureForm implements FeatureForm<DynamicForm> {
 			return;
 		}
 		FormItem item = formWidget.getField(info.getName());
-		boolean editable = (attribute != null && attribute.isEditable());
+		boolean editable = (attribute != null && attribute.isEditable()) || (useEditableFromInfo && info.isEditable());
 		if (editable) {
 			editableItemNames.add(info.getName());
 		} else {
