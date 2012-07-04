5b163a48d5ea2798382c470adc7d17aa4aa975a3
Jan De Moerloose <jan.demoerloose@geosparc.com>
2012-07-04 13:46:47 +0000

    GT-44: compile problem fixed by using factorybean

--- plugin/geomajas-layer-geotools/geotools/src/main/java/org/geomajas/layer/geotools/GeoDbDataSource.java
+++ plugin/geomajas-layer-geotools/geotools/src/main/java/org/geomajas/layer/geotools/GeoDbDataSource.java
@@ -13,7 +13,6 @@ package org.geomajas.layer.geotools;
 import geodb.GeoDB;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -22,16 +21,17 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.core.io.Resource;
 
 /**
- * This class wraps an existing h2 datasource and turns the  {@link GeoDB} datasource which can be directly used by
+ * This class takes an existing h2 datasource and turns it into a {@link GeoDB} datasource which can be directly used by
  * {@link GeoToolsLayer}.
  * 
  * @author Jan De Moerloose
  * 
  */
-public class GeoDbDataSource implements DataSource {
+public class GeoDbDataSource implements FactoryBean<DataSource> {
 
 	private DataSource dataSource;
 
@@ -53,41 +53,10 @@ public class GeoDbDataSource implements DataSource {
 		this.dataSource = dataSource;
 	}
 
-	public PrintWriter getLogWriter() throws SQLException {
-		return dataSource.getLogWriter();
-	}
-
-	public <T> T unwrap(Class<T> iface) throws SQLException {
-		return dataSource.unwrap(iface);
-	}
-
-	public void setLogWriter(PrintWriter out) throws SQLException {
-		dataSource.setLogWriter(out);
-	}
-
-	public boolean isWrapperFor(Class<?> iface) throws SQLException {
-		return dataSource.isWrapperFor(iface);
-	}
-
-	public void setLoginTimeout(int seconds) throws SQLException {
-		dataSource.setLoginTimeout(seconds);
-	}
-
-	public Connection getConnection() throws SQLException {
-		return dataSource.getConnection();
-	}
-
-	public Connection getConnection(String username, String password) throws SQLException {
-		return dataSource.getConnection(username, password);
-	}
-
-	public int getLoginTimeout() throws SQLException {
-		return dataSource.getLoginTimeout();
-	}
 
 	@PostConstruct
 	void postConstruct() throws SQLException, IOException {
-		Connection connection = getConnection();
+		Connection connection = dataSource.getConnection();
 		GeoDB.InitGeoDB(connection);
 		if (script != null) {
 			String sql = IOUtils.toString(script.getInputStream());
@@ -95,6 +64,21 @@ public class GeoDbDataSource implements DataSource {
 			statement.execute(sql);
 		}
 		connection.close();
+	}
+
+	@Override
+	public DataSource getObject() throws Exception {
+		return dataSource;
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return DataSource.class;
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return true;
 	}
 
 }
