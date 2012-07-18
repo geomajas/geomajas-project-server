3e1dd476d729a87009d781cfa7a027b32b0e501c
Joachim Van der Auwera <joachim@progs.be>
2012-07-18 18:44:25 +0000

    fix puregwt archetype

--- build-tools/geomajas-puregwt-archetype/src/main/resources/archetype-resources/src/main/java/client/ResizableMapLayout.java
+++ build-tools/geomajas-puregwt-archetype/src/main/resources/archetype-resources/src/main/java/client/ResizableMapLayout.java
@@ -55,7 +55,7 @@ public class ResizableMapLayout implements IsWidget {
 	 */
 	public ResizableMapLayout(final MapPresenter mapPresenter) {
 		this.mapPresenter = mapPresenter;
-		mapPresenter.getEventBus().addHandler(MapInitializationEvent.TYPE, new RedrawMapInitializationHandler());
+		mapPresenter.getEventBus().addHandler(MapInitializationHandler.TYPE, new RedrawMapInitializationHandler());
 
 		layout = new ResizeLayoutPanel();
 		layout.setSize("100%", "100%");
