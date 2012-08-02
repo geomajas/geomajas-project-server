ed514ed6d93b0a538b444129a87865b95e5bde1a
Oliver May <oliver.may@dfc.be>
2012-08-02 07:50:14 +0000

    GAV-26 Create postconfigurationprocessor for ScaleInfo's used in the theme configuration

--- plugin/geomajas-widget-advancedviews/advancedviews/src/main/java/org/geomajas/widget/advancedviews/service/AdvancedviewsConfigurationDtoPostProcessor.java
+++ plugin/geomajas-widget-advancedviews/advancedviews/src/main/java/org/geomajas/widget/advancedviews/service/AdvancedviewsConfigurationDtoPostProcessor.java
@@ -15,7 +15,7 @@ import java.util.Map;
 import javax.annotation.PostConstruct;
 
 import org.geomajas.configuration.client.ClientMapInfo;
-import org.geomajas.internal.configuration.ConfigurationDtoPostProcessor;
+import org.geomajas.configuration.client.ScaleInfo;
 import org.geomajas.widget.advancedviews.configuration.client.ThemesInfo;
 import org.geomajas.widget.advancedviews.configuration.client.themes.RangeConfig;
 import org.geomajas.widget.advancedviews.configuration.client.themes.ViewConfig;
@@ -31,9 +31,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class AdvancedviewsConfigurationDtoPostProcessor {
 
-	@Autowired
-	protected ConfigurationDtoPostProcessor geomajasPostProcessor;
-	
 	@Autowired(required = false)
 	protected Map<String, ClientMapInfo> mapInfos;
 
@@ -51,12 +48,39 @@ public class AdvancedviewsConfigurationDtoPostProcessor {
 				if (info != null) {
 					for (ViewConfig view : info.getThemeConfigs()) {
 						for (RangeConfig range : view.getRangeConfigs()) {
-							geomajasPostProcessor.completeScale(range.getMaximumScale(), pixPerUnit);
-							geomajasPostProcessor.completeScale(range.getMinimumScale(), pixPerUnit);
+							completeScale(range.getMaximumScale(), pixPerUnit);
+							completeScale(range.getMinimumScale(), pixPerUnit);
 						}
 					}
 				}
 			}
 		}
 	}
+
+	/**
+	 * Convert the scale in pixels per unit or relative values, which ever is missing.
+	 * 
+	 * @param scaleInfo scaleInfo object which needs to be completed
+	 * @param mapUnitInPixels the number of pixels in a map unit
+	 */
+	public void completeScale(ScaleInfo scaleInfo, double mapUnitInPixels) {
+		if (0 == mapUnitInPixels) {
+			throw new IllegalArgumentException("ScaleInfo.completeScale mapUnitInPixels should never be zero.");
+		}
+		double denominator = scaleInfo.getDenominator();
+		double numerator = scaleInfo.getNumerator();
+		if (denominator != 0) {
+			scaleInfo.setPixelPerUnit(numerator / denominator * mapUnitInPixels);
+		} else {
+			double pixelPerUnit = scaleInfo.getPixelPerUnit();
+			if (pixelPerUnit > mapUnitInPixels) {
+				scaleInfo.setNumerator(pixelPerUnit / mapUnitInPixels);
+				scaleInfo.setDenominator(1);
+			} else {
+				scaleInfo.setNumerator(1);
+				scaleInfo.setDenominator(mapUnitInPixels / pixelPerUnit);
+			}
+		}
+	}
+
 }
