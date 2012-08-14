ffd73a91c1707300ed4fbe1b218586338a3783c3
Jan De Moerloose <jan.demoerloose@geosparc.com>
2012-08-14 07:01:14 +0000

    GAV-26: removed duplicate scale code

--- plugin/geomajas-widget-advancedviews/advancedviews/src/main/java/org/geomajas/widget/advancedviews/service/AdvancedviewsConfigurationDtoPostProcessor.java
+++ plugin/geomajas-widget-advancedviews/advancedviews/src/main/java/org/geomajas/widget/advancedviews/service/AdvancedviewsConfigurationDtoPostProcessor.java
@@ -14,8 +14,8 @@ import java.util.Map;
 
 import org.geomajas.configuration.client.ClientApplicationInfo;
 import org.geomajas.configuration.client.ClientMapInfo;
-import org.geomajas.configuration.client.ScaleInfo;
 import org.geomajas.global.ConfigurationDtoPostProcess;
+import org.geomajas.global.ConfigurationHelper;
 import org.geomajas.widget.advancedviews.configuration.client.ThemesInfo;
 import org.geomajas.widget.advancedviews.configuration.client.themes.RangeConfig;
 import org.geomajas.widget.advancedviews.configuration.client.themes.ViewConfig;
@@ -32,7 +32,8 @@ public class AdvancedviewsConfigurationDtoPostProcessor implements Configuration
 	@Autowired(required = false)
 	protected Map<String, ClientApplicationInfo> applicationMap;
 	
-	public void processConfiguration() {
+	@Override
+	public void processConfiguration(ConfigurationHelper configurationHelper) {
 		if (applicationMap != null) {
 			for (ClientApplicationInfo applicationInfo : applicationMap.values()) {
 				for (ClientMapInfo map : applicationInfo.getMaps()) {
@@ -41,40 +42,12 @@ public class AdvancedviewsConfigurationDtoPostProcessor implements Configuration
 					if (info != null) {
 						for (ViewConfig view : info.getThemeConfigs()) {
 							for (RangeConfig range : view.getRangeConfigs()) {
-								completeScale(range.getMaximumScale(), pixPerUnit);
-								completeScale(range.getMinimumScale(), pixPerUnit);
+								configurationHelper.completeScale(range.getMaximumScale(), pixPerUnit);
+								configurationHelper.completeScale(range.getMinimumScale(), pixPerUnit);
 							}
 						}
 					}
 				}
-			}
-		}
-	}
-
-	/**
-	 * Convert the scale in pixels per unit or relative values, which ever is missing.
-	 * 
-	 * @param scaleInfo
-	 *            scaleInfo object which needs to be completed
-	 * @param mapUnitInPixels
-	 *            the number of pixels in a map unit
-	 */
-	public void completeScale(ScaleInfo scaleInfo, double mapUnitInPixels) {
-		if (0 == mapUnitInPixels) {
-			throw new IllegalArgumentException("ScaleInfo.completeScale mapUnitInPixels should never be zero.");
-		}
-		double denominator = scaleInfo.getDenominator();
-		double numerator = scaleInfo.getNumerator();
-		if (denominator != 0) {
-			scaleInfo.setPixelPerUnit(numerator / denominator * mapUnitInPixels);
-		} else {
-			double pixelPerUnit = scaleInfo.getPixelPerUnit();
-			if (pixelPerUnit > mapUnitInPixels) {
-				scaleInfo.setNumerator(pixelPerUnit / mapUnitInPixels);
-				scaleInfo.setDenominator(1);
-			} else {
-				scaleInfo.setNumerator(1);
-				scaleInfo.setDenominator(mapUnitInPixels / pixelPerUnit);
 			}
 		}
 	}
