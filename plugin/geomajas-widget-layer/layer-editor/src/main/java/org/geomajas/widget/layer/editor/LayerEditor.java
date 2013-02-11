a5d5c415387be9dfb82a77eb729ae4787c1c0479
Oliver May <oliver.may@dfc.be>
2013-02-11 14:33:02 +0000

    GLW-8: Create configuration gui for the deskmanager plugin

---
+++ plugin/geomajas-widget-layer/layer-editor/src/main/java/org/geomajas/widget/layer/editor/LayerEditor.java
@@ -0,0 +1,30 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2013 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+package org.geomajas.widget.layer.editor;
+
+import org.geomajas.plugin.deskmanager.client.gwt.common.WidgetEditorFactoryRegistry;
+import org.geomajas.widget.advancedviews.editor.client.LayerTreeEditorFactory;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+/**
+ * @author Oliver May
+ *
+ */
+public class LayerEditor implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		WidgetEditorFactoryRegistry.getInstance().register(new LayerTreeEditorFactory());
+	}
+
+}
