daf429db2447870319be45e64a4d204a3c9aa912
Oliver May <oliver.may@dfc.be>
2013-02-26 16:00:49 +0000

    GDM-12: add unit tests

--- plugin/geomajas-plugin-deskmanager/deskmanager/src/test/java/org/geomajas/plugin/deskmanager/test/command/common/GetConfigurationCommandTest.java
+++ plugin/geomajas-plugin-deskmanager/deskmanager/src/test/java/org/geomajas/plugin/deskmanager/test/command/common/GetConfigurationCommandTest.java
@@ -27,7 +27,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Oliver May
