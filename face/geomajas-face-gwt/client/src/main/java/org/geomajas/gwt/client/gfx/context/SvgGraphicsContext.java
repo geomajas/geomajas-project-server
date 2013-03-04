7bdfe6bcf6c23e0a41d2fb286ac5ca7d54ad3896
Jan De Moerloose <jan.demoerloose@geosparc.com>
2013-03-04 14:43:32 +0000

    GWT-442: fix of bringToFront() for IE9

--- face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/gfx/context/SvgGraphicsContext.java
+++ face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/gfx/context/SvgGraphicsContext.java
@@ -28,6 +28,8 @@ import org.geomajas.gwt.client.spatial.geometry.Polygon;
 import org.geomajas.gwt.client.util.Dom;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.Style.Overflow;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Widget;
@@ -770,8 +772,19 @@ public class SvgGraphicsContext implements GraphicsContext {
 	}
 
 	/** {@inheritDoc} */
-	public void bringToFront(Object object, String name) {
-		helper.bringToFront(object, name);
+	public void bringToFront(final Object object, final String name) {
+		// IE has the annoying habit of not capturing events during move of elements !
+		if (Dom.isIE()) {
+			Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+
+				@Override
+				public void execute() {
+					helper.bringToFront(object, name);
+				}
+			});
+		} else {
+			helper.bringToFront(object, name);
+		}
 	}
 
 	/** {@inheritDoc} */
