b8bda5ea201496d28a49f52a97ed9312b2850ff4
Oliver May <oliver.may@dfc.be>
2013-03-05 11:24:00 +0000

    GDM-12: unit tests

--- plugin/geomajas-plugin-deskmanager/deskmanager/src/test/java/org/geomajas/plugin/deskmanager/test/general/MyClientWidgetInfo.java
+++ plugin/geomajas-plugin-deskmanager/deskmanager/src/test/java/org/geomajas/plugin/deskmanager/test/general/MyClientWidgetInfo.java
@@ -17,6 +17,16 @@ public class MyClientWidgetInfo implements ClientWidgetInfo {
 	private static final long serialVersionUID = 1L;
 	private String key;
 	
+	
+	public String getKey() {
+		return key;
+	}
+
+	
+	public void setKey(String key) {
+		this.key = key;
+	}
+
 	public MyClientWidgetInfo(String key) {
 		this.key = key;
 	}
