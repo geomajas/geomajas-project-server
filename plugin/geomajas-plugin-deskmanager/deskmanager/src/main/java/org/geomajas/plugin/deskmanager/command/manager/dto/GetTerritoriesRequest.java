aabf00d1569655b2a97fee130b48e3b1762aa616
Oliver May <oliver.may@dfc.be>
2013-03-05 14:43:21 +0000

    GDM-12: make command names more unique GDM-12: unit test assertion order

--- plugin/geomajas-plugin-deskmanager/deskmanager/src/main/java/org/geomajas/plugin/deskmanager/command/manager/dto/GetTerritoriesRequest.java
+++ plugin/geomajas-plugin-deskmanager/deskmanager/src/main/java/org/geomajas/plugin/deskmanager/command/manager/dto/GetTerritoriesRequest.java
@@ -22,7 +22,7 @@ public class GetTerritoriesRequest implements CommandRequest {
 
 	private static final long serialVersionUID = 1L;
 
-	public static final String COMMAND = "command.deskmanager.manager.GetGroups";
+	public static final String COMMAND = "command.deskmanager.manager.GetTerritories";
 
 	public String toString() {
 		return COMMAND;
