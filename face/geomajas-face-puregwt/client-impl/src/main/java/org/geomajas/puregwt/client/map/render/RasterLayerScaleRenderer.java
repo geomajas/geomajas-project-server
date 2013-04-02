4d20658107d9b6cf158330bab94af2eaf2d8e49d
Pieter De Graef <pieter.degraef@geosparc.com>
2013-04-02 14:04:45 +0000

    PURE-133: Changing the opacity of a server raster layer sometimes has no effect

--- face/geomajas-face-puregwt/client-impl/src/main/java/org/geomajas/puregwt/client/map/render/RasterLayerScaleRenderer.java
+++ face/geomajas-face-puregwt/client-impl/src/main/java/org/geomajas/puregwt/client/map/render/RasterLayerScaleRenderer.java
@@ -203,7 +203,7 @@ public class RasterLayerScaleRenderer implements TiledScaleRenderer {
 
 	protected void renderTile(RasterTile tile, Callback<String, String> callback) {
 		HtmlImage image = htmlImageFactory.create(tile.getUrl(), tile.getBounds(), callback);
-		image.setOpacity(rasterLayer.getOpacity());
+		//image.setOpacity(rasterLayer.getOpacity());
 		container.add(image);
 	}
 
