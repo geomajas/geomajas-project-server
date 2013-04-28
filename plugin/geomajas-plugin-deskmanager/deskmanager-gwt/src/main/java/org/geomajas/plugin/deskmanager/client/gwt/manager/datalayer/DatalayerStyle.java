f4e5a93e561fa11f0f4e998fafe4d51005c55cdf
Kristof Heirwegh <kristof.heirwegh@dfc.be>
2013-04-28 18:54:26 +0000

    GDM-20: expert layerstyle editing in datalayer panel.

--- plugin/geomajas-plugin-deskmanager/deskmanager-gwt/src/main/java/org/geomajas/plugin/deskmanager/client/gwt/manager/datalayer/DatalayerStyle.java
+++ plugin/geomajas-plugin-deskmanager/deskmanager-gwt/src/main/java/org/geomajas/plugin/deskmanager/client/gwt/manager/datalayer/DatalayerStyle.java
@@ -12,18 +12,21 @@ package org.geomajas.plugin.deskmanager.client.gwt.manager.datalayer;
 
 //import org.geomajas.configuration.FeatureStyleInfo;
 //import org.geomajas.configuration.client.ClientVectorLayerInfo;
+import org.geomajas.gwt.client.util.WidgetLayout;
 import org.geomajas.plugin.deskmanager.client.gwt.manager.common.AbstractConfigurationLayout;
 import org.geomajas.plugin.deskmanager.client.gwt.manager.common.SaveButtonBar;
 import org.geomajas.plugin.deskmanager.client.gwt.manager.i18n.ManagerMessages;
-//import org.geomajas.plugin.deskmanager.client.gwt.manager.service.ManagerCommandService;
+import org.geomajas.plugin.deskmanager.client.gwt.manager.service.ManagerCommandService;
+import org.geomajas.plugin.deskmanager.client.gwt.manager.util.ExpertSldEditorHelper;
+import org.geomajas.plugin.deskmanager.command.manager.dto.DynamicVectorLayerConfiguration;
+import org.geomajas.plugin.deskmanager.command.manager.dto.SaveLayerModelRequest;
 import org.geomajas.plugin.deskmanager.domain.dto.LayerModelDto;
 
 import com.google.gwt.core.client.GWT;
 import com.smartgwt.client.types.Overflow;
+import com.smartgwt.client.widgets.IButton;
 import com.smartgwt.client.widgets.events.ClickEvent;
-//import com.smartgwt.client.widgets.form.DynamicForm;
-//import com.smartgwt.client.widgets.form.fields.ColorPickerItem;
-//import com.smartgwt.client.widgets.form.fields.events.ChangedEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 /**
@@ -34,6 +37,10 @@ public class DatalayerStyle extends AbstractConfigurationLayout {
 	private static final ManagerMessages MESSAGES = GWT.create(ManagerMessages.class);
 
 	private LayerModelDto lmd;
+
+	private ExpertSldEditorHelper styleHelper;
+
+	private IButton openStyleEditor;
 
 	public DatalayerStyle() {
 		super();
@@ -49,21 +56,59 @@ public class DatalayerStyle extends AbstractConfigurationLayout {
 		group.setGroupTitle(MESSAGES.datalayerStyleFormGroup());
 		group.setOverflow(Overflow.AUTO);
 
+		openStyleEditor = new IButton(MESSAGES.layerConfigExpertEditorBtn());
+		openStyleEditor.setIcon(WidgetLayout.iconEdit);
+		openStyleEditor.setAutoFit(true);
+		openStyleEditor.setVisible(false);
+		openStyleEditor.setDisabled(true);
+		openStyleEditor.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				if (styleHelper != null) {
+					styleHelper.showExpertStyleEditor();
+				}
+			}
+		});
+		group.addMember(openStyleEditor);
+
 		addMember(group);
 	}
 
 	public void setLayerModel(LayerModelDto lmd) {
+		this.lmd = lmd;
+		openStyleEditor.setVisible((lmd.getLayerConfiguration() instanceof DynamicVectorLayerConfiguration));
+		openStyleEditor.setDisabled(true);
+		if (styleHelper != null) {
+			styleHelper.destroy();
+			styleHelper = null;
+		}
 		fireChangedHandler();
 	}
 
 	// -- SaveButtonBar events --------------------------------------------------------
 
 	public boolean onEditClick(ClickEvent event) {
+		if (styleHelper == null) {
+			styleHelper = new ExpertSldEditorHelper(
+					((DynamicVectorLayerConfiguration) lmd.getLayerConfiguration()).getClientVectorLayerInfo());
+		}
+		openStyleEditor.setDisabled(false);
 		return true;
 	}
 
 	public boolean onSaveClick(ClickEvent event) {
-		return validate();
+		if (validate()) {
+			// update
+			openStyleEditor.setDisabled(true);
+			if (styleHelper != null) {
+				styleHelper.apply(((DynamicVectorLayerConfiguration) lmd.getLayerConfiguration())
+						.getClientVectorLayerInfo());
+			}
+			// persist
+			ManagerCommandService.saveLayerModel(lmd, SaveLayerModelRequest.SAVE_SETTINGS);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public boolean onCancelClick(ClickEvent event) {
