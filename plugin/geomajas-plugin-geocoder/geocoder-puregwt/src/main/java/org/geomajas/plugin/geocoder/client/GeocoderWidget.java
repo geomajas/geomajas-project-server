e905b682485623f2027a091e51d1953421f13916
Pieter De Graef <pieter.degraef@geosparc.com>
2013-05-31 11:45:42 +0000

    GC-46: Upgrade to stable pure GWT 1.0.0 api

--- plugin/geomajas-plugin-geocoder/geocoder-puregwt/src/main/java/org/geomajas/plugin/geocoder/client/GeocoderWidget.java
+++ plugin/geomajas-plugin-geocoder/geocoder-puregwt/src/main/java/org/geomajas/plugin/geocoder/client/GeocoderWidget.java
@@ -12,7 +12,6 @@
 package org.geomajas.plugin.geocoder.client;
 
 import org.geomajas.puregwt.client.map.MapPresenter;
-import org.geomajas.puregwt.client.widget.AbstractMapWidget;
 
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -29,55 +28,52 @@ import com.google.gwt.user.client.ui.Widget;
 
 /**
  * Map gadget that contains a {@link GeocoderTextBox} with a clickable magnifying glass.
- * <p>Alternatives for ambiguous searches are shown in a drop-down panel below the text box.</p>
- * <p><b>Tip</b>: To position next to the {@link org.geomajas.puregwt.client.map.gadget.ZoomToRectangleGadget}, 
- * set top to 5 and left to 90 with <code>new GeocoderGadget(5, 90)</code></p>
+ * <p>
+ * Alternatives for ambiguous searches are shown in a drop-down panel below the text box.
+ * </p>
+ * <p>
+ * <b>Tip</b>: To position next to the {@link org.geomajas.puregwt.client.map.gadget.ZoomToRectangleGadget}, set top to
+ * 5 and left to 90 with <code>new GeocoderGadget(5, 90)</code>
+ * </p>
+ * 
  * @author Emiel Ackermann
- *
  */
-public class GeocoderWidget extends AbstractMapWidget {
-	
+public class GeocoderWidget extends FlowPanel {
+
 	private static final String GM_GEOCODER_GADGET_GLASS = "gm-GeocoderGadget-glass";
+
 	private static final String GM_GEOCODER_GADGET = "gm-GeocoderGadget";
+
 	static final String GM_GEOCODER_GADGET_TEXT_BOX_WITH_ALTS = "gm-GeocoderGadget-textBox-withAlts";
-	
-	/**
-	 * Extra css to style the tip.
-	 */
+
+	/** Extra CSS to style the tip. */
 	static final String GM_GEOCODER_GADGET_ALT_PANEL = "gm-GeocoderGadget-altPanel";
+
 	static final String GM_GEOCODER_GADGET_ALT_LABEL = "gm-GeocoderGadget-altLabel";
-	
-	private FlowPanel layout;
+
+	private final MapPresenter mapPresenter;
+
 	private GeocoderTextBox box;
 
 	public GeocoderWidget(MapPresenter mapPresenter, int top, int left) {
-		super(mapPresenter);
+		this.mapPresenter = mapPresenter;
 		asWidget().getElement().getStyle().setTop(top, Unit.PX);
 		asWidget().getElement().getStyle().setLeft(left, Unit.PX);
+
+		buildGui();
 	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public Widget asWidget() {
-		if (layout == null) {
-			buildGui();
-		}
-		return layout;
-	}
-	
+
 	private void buildGui() {
-		layout = new FlowPanel();
-		layout.setStyleName(GM_GEOCODER_GADGET);
+		setStyleName(GM_GEOCODER_GADGET);
 		box = new GeocoderTextBox(mapPresenter);
-		layout.add(box);
-		layout.add(createMagnifyingGlass());
+		add(box);
+		add(createMagnifyingGlass());
 		StopPropagationHandler stopHandler = new StopPropagationHandler();
-		layout.addDomHandler(stopHandler, MouseDownEvent.getType());
-		layout.addDomHandler(stopHandler, MouseUpEvent.getType());
-		layout.addDomHandler(stopHandler, ClickEvent.getType());
-		layout.addDomHandler(stopHandler, DoubleClickEvent.getType());
-		
+		addDomHandler(stopHandler, MouseDownEvent.getType());
+		addDomHandler(stopHandler, MouseUpEvent.getType());
+		addDomHandler(stopHandler, ClickEvent.getType());
+		addDomHandler(stopHandler, DoubleClickEvent.getType());
+
 		// Create drop-down panel below text box with alternatives
 		new AlternativesPanel(box);
 	}
@@ -86,7 +82,7 @@ public class GeocoderWidget extends AbstractMapWidget {
 		Button glass = new Button();
 		glass.setStyleName(GM_GEOCODER_GADGET_GLASS);
 		glass.addClickHandler(new ClickHandler() {
-			
+
 			public void onClick(ClickEvent event) {
 				box.goToLocation();
 				event.stopPropagation();
@@ -94,14 +90,15 @@ public class GeocoderWidget extends AbstractMapWidget {
 		});
 		return glass;
 	}
-	
+
 	/**
 	 * Combination of different handlers with a single goal: stop all the events from propagating to the map. This is
 	 * meant to be used for clickable widgets.
 	 * 
 	 * @author Pieter De Graef
 	 */
-	public class StopPropagationHandler implements MouseDownHandler, MouseUpHandler, ClickHandler, DoubleClickHandler {
+	public class StopPropagationHandler implements MouseDownHandler, MouseUpHandler, ClickHandler, 
+			DoubleClickHandler {
 
 		public void onDoubleClick(DoubleClickEvent event) {
 			event.stopPropagation();
@@ -119,5 +116,4 @@ public class GeocoderWidget extends AbstractMapWidget {
 			event.stopPropagation();
 		}
 	}
-
 }
