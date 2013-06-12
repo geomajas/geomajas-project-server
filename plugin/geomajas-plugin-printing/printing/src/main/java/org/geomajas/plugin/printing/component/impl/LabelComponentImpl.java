3da72cd7ca09e8d6f05664f09324147a8c87d6e9
Pieter De Graef <pieter.degraef@geosparc.com>
2013-06-12 12:24:20 +0000

    SPRINT-64: Do not show unneeded fractions of labels of the scale bar on the print

--- plugin/geomajas-plugin-printing/printing/src/main/java/org/geomajas/plugin/printing/component/impl/LabelComponentImpl.java
+++ plugin/geomajas-plugin-printing/printing/src/main/java/org/geomajas/plugin/printing/component/impl/LabelComponentImpl.java
@@ -8,6 +8,7 @@
  * by the Geomajas Contributors License Agreement. For full licensing
  * details, see LICENSE.txt in the project root.
  */
+
 package org.geomajas.plugin.printing.component.impl;
 
 import java.awt.Font;
@@ -30,56 +31,45 @@ import com.thoughtworks.xstream.annotations.XStreamOmitField;
  * Inclusion of label in printed document.
  * 
  * @author Jan De Moerloose
+ * @author An Buyle
  */
 @Component()
 @Scope(value = "prototype")
 public class LabelComponentImpl extends AbstractPrintComponent<LabelComponentInfo> implements LabelComponent {
 
-	/**
-	 * The font for the text.
-	 */
+	private static final String ELLIPSES_STRING = "...";
+
+	/** The font for the text. */
 	@XStreamConverter(FontConverter.class)
 	private Font font;
 
-	/**
-	 * Label text.
-	 */
+	/** Label text. */
 	private String text;
 
-	/**
-	 * Color of the text.
-	 */
+	/** Color of the text. */
 	private String fontColor;
 
-	/**
-	 * Background color.
-	 */
+	/** Background color. */
 	private String backgroundColor;
 
-	/**
-	 * Line color.
-	 */
+	/** Line color. */
 	private String borderColor;
 
-	/**
-	 * Only text, no border ?
-	 */
+	/** Only text, no border? */
 	private boolean textOnly;
 
-	/**
-	 * Border line width.
-	 */
+	/** Border line width. */
 	private float lineWidth;
 
-	/**
-	 * Margin around text.
-	 */
+	/** Margin around text. */
 	private float margin;
-	
+
+	/** Max. width of rendered text (in point units) Default is 0.0, meaning there's no limit imposed by the client. */
+	private float maxWidthText;
+
 	@Autowired
 	@XStreamOmitField
 	private PrintDtoConverterService converterService;
-
 
 	public LabelComponentImpl() {
 		this("white", "black", new Font("Dialog", Font.PLAIN, 12), "black", "<missing text>", false, 1f);
@@ -103,7 +93,8 @@ public class LabelComponentImpl extends AbstractPrintComponent<LabelComponentInf
 	/**
 	 * Call back visitor.
 	 * 
-	 * @param visitor visitor
+	 * @param visitor
+	 *            visitor
 	 */
 	public void accept(PrintComponentVisitor visitor) {
 		visitor.visit(this);
@@ -185,7 +176,22 @@ public class LabelComponentImpl extends AbstractPrintComponent<LabelComponentInf
 		this.lineWidth = lineWidth;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.geomajas.plugin.printing.component.impl.LabelComponent#getMaxWidthText()
+	 */
+	public float getMaxWidthText() {
+		return this.maxWidthText;
+
+	}
+
+	public void setMaxWidthText(float maxWidthText) {
+		this.maxWidthText = maxWidthText;
+	}
+
 	public void render(PdfContext context) {
+		assert (null != getText()) : "getText() must be non-null";
 		if (!isTextOnly()) {
 			context.fillRectangle(getSize(), context.getColor(getBackgroundColor(), 1f));
 			context.strokeRectangle(getSize(), context.getColor(getBorderColor(), 1f), getLineWidth());
@@ -197,9 +203,15 @@ public class LabelComponentImpl extends AbstractPrintComponent<LabelComponentInf
 
 	@Override
 	public void calculateSize(PdfContext context) {
+		assert (null != getText()) : "getText() must be non-null";
+
+		if (getMaxWidthText() > 0.0) {
+			limitTextWidth(context, getMaxWidthText());
+		}
 		Rectangle textSize = context.getTextSize(getText(), getFont());
-		margin = 0.25f * getFont().getSize();
-		setBounds(new Rectangle(textSize.getWidth() + 2 * margin, textSize.getHeight() + 2 * margin));
+		margin = 0.5f * getFont().getSize(); // TODO: make margin configurable
+
+		setBounds(new Rectangle(textSize.getWidth() + 2.0f * margin, textSize.getHeight() + 2 * margin));
 	}
 
 	public void fromDto(LabelComponentInfo labelInfo) {
@@ -211,6 +223,24 @@ public class LabelComponentImpl extends AbstractPrintComponent<LabelComponentInf
 		setLineWidth(labelInfo.getLineWidth());
 		setTextOnly(labelInfo.isTextOnly());
 		setText(labelInfo.getText());
+		// support for max-width of rendered text (in point units?)
+		setMaxWidthText(labelInfo.getMaxWidthText());
 	}
 
+	private void limitTextWidth(PdfContext context, float maxWidth) {
+		assert (null != getText()) : "getText() must be non-null";
+		if (getMaxWidthText() > 0.0) {
+
+			float elipsesWidth = context.getTextSize(ELLIPSES_STRING, getFont()).getWidth();
+			if (context.getTextSize(getText(), getFont()).getWidth() > getMaxWidthText() && getText().length() > 2) {
+				StringBuffer text = new StringBuffer(getText());
+				do {
+					text.deleteCharAt(text.length() - 1);
+				} while (context.getTextSize(text.toString(), getFont()).getWidth() + elipsesWidth > getMaxWidthText()
+						&& text.length() > 2);
+
+				setText(text.append("...").toString());
+			}
+		}
+	}
 }
