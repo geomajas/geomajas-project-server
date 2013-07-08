6c688d818c1432a046095f773f353dc9f4ae2738
Oliver May <oliver.may@dfc.be>
2013-07-08 13:07:21 +0000

    GBE-321 Expose the methods from the deprecated AttributeInfo trough AbstractReadOnlyAttribute

--- backend/api/src/main/java/org/geomajas/configuration/AbstractReadOnlyAttributeInfo.java
+++ backend/api/src/main/java/org/geomajas/configuration/AbstractReadOnlyAttributeInfo.java
@@ -29,5 +29,94 @@ public abstract class AbstractReadOnlyAttributeInfo extends AttributeInfo {
 	public AbstractReadOnlyAttributeInfo() {
 		super();
 	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 1.13.0 don't expose deprecated methods from AttributeInfo directly.
+	 */
+	@Override
+	public boolean isEditable() { // NOSONAR override needed for @Api
+		return super.isEditable();
+	}
 
+	/**
+	 * {@inheritDoc}
+	 * @since 1.13.0 don't expose deprecated methods from AttributeInfo directly.
+	 */
+	@Override
+	public void setEditable(boolean editable) { // NOSONAR override needed for @Api
+		super.setEditable(editable);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 1.13.0 don't expose deprecated methods from AttributeInfo directly.
+	 */
+	@Override
+	public String getLabel() {
+		return super.getLabel();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 1.13.0 don't expose deprecated methods from AttributeInfo directly.
+	 */
+	@Override
+	public void setLabel(String label) {
+		super.setLabel(label);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 1.13.0 don't expose deprecated methods from AttributeInfo directly.
+	 */
+	@Override
+	public boolean isIdentifying() {
+		return super.isIdentifying();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 1.13.0 don't expose deprecated methods from AttributeInfo directly.
+	 */
+	@Override
+	public void setIdentifying(boolean identifying) {
+		super.setIdentifying(identifying);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 1.13.0 don't expose deprecated methods from AttributeInfo directly.
+	 */
+	@Override
+	public boolean isHidden() {
+		return super.isHidden();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 1.13.0 don't expose deprecated methods from AttributeInfo directly.
+	 */
+	@Override
+	public void setHidden(boolean hidden) {
+		super.setHidden(hidden);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 1.13.0 don't expose deprecated methods from AttributeInfo directly.
+	 */
+	@Override
+	public String getFormInputType() {
+		return super.getFormInputType();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 1.13.0 don't expose deprecated methods from AttributeInfo directly.
+	 */
+	@Override
+	public void setFormInputType(String formInputType) {
+		super.setFormInputType(formInputType);
+	}
 }
