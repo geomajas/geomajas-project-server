3afd9c3bfe25922a0cbd292a17c95c37ebd2266c
Pieter De Graef <pieter.degraef@geosparc.com>
2013-07-11 13:18:23 +0000

    PURE-171: Add an example showcase application as part of the Geomajas Client.

---
+++ face/geomajas-face-puregwt/example-base/src/main/java/org/geomajas/puregwt/example/base/client/ExampleBase.java
@@ -0,0 +1,70 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2013 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.puregwt.example.base.client;
+
+import org.geomajas.puregwt.client.GeomajasGinjector;
+import org.geomajas.puregwt.example.base.client.page.sample.SamplePage;
+import org.geomajas.puregwt.example.base.client.resource.ShowcaseResource;
+import org.geomajas.puregwt.example.base.client.sample.ShowcaseSampleDefinition;
+import org.geomajas.puregwt.example.base.client.widget.ShowcaseDialogBox;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+
+/**
+ * Entry point and main class for PureGWT example application.
+ * 
+ * @author Pieter De Graef
+ */
+public class ExampleBase implements EntryPoint {
+
+	private static final GeomajasGinjector GEOMAJASINJECTOR = GWT.create(GeomajasGinjector.class);
+
+	private static final ShowcaseResource RESOURCE = GWT.create(ShowcaseResource.class);
+
+	private static final ShowcaseLayout LAYOUT = new ShowcaseLayout();
+
+	public void onModuleLoad() {
+		// Prepare styling:
+		RESOURCE.css().ensureInjected();
+	}
+
+	public static ShowcaseLayout getLayout() {
+		return LAYOUT;
+	}
+
+	public static GeomajasGinjector getInjector() {
+		return GEOMAJASINJECTOR;
+	}
+
+	public static ShowcaseResource getShowcaseResource() {
+		return RESOURCE;
+	}
+
+	public static void showSample(ShowcaseSampleDefinition sample) {
+		ShowcaseDialogBox dialogBox = new ShowcaseDialogBox();
+		dialogBox.setAnimationEnabled(true);
+		dialogBox.setAutoHideEnabled(false);
+		dialogBox.setModal(true);
+		dialogBox.setGlassEnabled(true);
+		dialogBox.setText(sample.getTitle());
+		SamplePage page = new SamplePage();
+		int width = Window.getClientWidth() - 200;
+		int height = Window.getClientHeight() - 160;
+		page.setSize(width + "px", height + "px");
+		page.setSamplePanelFactory(sample);
+		dialogBox.setWidget(page);
+		dialogBox.center();
+		dialogBox.show();
+	}
+}
---
+++ face/geomajas-face-puregwt/example-base/src/main/java/org/geomajas/puregwt/example/base/client/ShowcaseLayout.java
@@ -0,0 +1,60 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2013 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.puregwt.example.base.client;
+
+import org.geomajas.puregwt.example.base.client.page.overview.SampleOverviewPage;
+import org.geomajas.puregwt.example.base.client.resource.ShowcaseResource;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiFactory;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Top layout for the Geomajas PureGWT showcase.
+ * 
+ * @author Pieter De Graef
+ */
+public class ShowcaseLayout extends Composite {
+
+	/**
+	 * UI binder interface this widget.
+	 * 
+	 * @author Pieter De Graef
+	 */
+	interface MyUiBinder extends UiBinder<Widget, ShowcaseLayout> {
+	}
+
+	private static final MyUiBinder UIBINDER = GWT.create(MyUiBinder.class);
+
+	@UiField
+	protected SimplePanel contentPanel;
+
+	public ShowcaseLayout() {
+		initWidget(UIBINDER.createAndBindUi(this));
+		contentPanel.add(new SampleOverviewPage());
+	}
+
+	public void setPage(IsWidget page) {
+		contentPanel.clear();
+		contentPanel.add(page);
+	}
+
+	@UiFactory
+	public ShowcaseResource getResourceBundle() {
+		return ExampleBase.getShowcaseResource();
+	}
+}
