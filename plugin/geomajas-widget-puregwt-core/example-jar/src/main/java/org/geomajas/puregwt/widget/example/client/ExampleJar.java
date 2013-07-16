34c4eca2f9d177135698ff0c5623f94877c7cc89
Pieter De Graef <pieter.degraef@geosparc.com>
2013-07-16 11:59:19 +0000

    PUREWIDGET-11: Include Showcase in this plugin

---
+++ plugin/geomajas-widget-puregwt-core/example-jar/src/main/java/org/geomajas/puregwt/widget/example/client/ExampleJar.java
@@ -0,0 +1,95 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2013 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.puregwt.widget.example.client;
+
+import org.geomajas.puregwt.example.base.client.sample.SamplePanel;
+import org.geomajas.puregwt.example.base.client.sample.SamplePanelRegistry;
+import org.geomajas.puregwt.example.base.client.sample.ShowcaseSampleDefinition;
+import org.geomajas.puregwt.widget.example.client.i18n.SampleMessages;
+import org.geomajas.puregwt.widget.example.client.sample.LayerLegendPanelSample;
+import org.geomajas.puregwt.widget.example.client.sample.LegendDropDownWidgetSample;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+/**
+ * Entry point and main class for the widget core example application.
+ * 
+ * @author Pieter De Graef
+ */
+public class ExampleJar implements EntryPoint {
+
+	private static final SampleMessages MESSAGES = GWT.create(SampleMessages.class);
+
+	public static final String CATEGORY_WIDGET = "Widgets";
+
+	public void onModuleLoad() {
+		// Register all samples:
+		registerSamples();
+	}
+
+	public static SampleMessages getMessages() {
+		return MESSAGES;
+	}
+
+	private void registerSamples() {
+		SamplePanelRegistry.registerCategory(CATEGORY_WIDGET, 90);
+		SamplePanelRegistry.registerFactory(CATEGORY_WIDGET, new ShowcaseSampleDefinition() {
+
+			public SamplePanel create() {
+				return new LayerLegendPanelSample();
+			}
+
+			@Override
+			public String getTitle() {
+				return "Legend";
+			}
+
+			@Override
+			public String getDescription() {
+				return "Showcase that show the layer legend view.";
+			}
+
+			public String getShortDescription() {
+				return "Showcase that show the layer legend view.";
+			}
+
+			public String getCategory() {
+				return CATEGORY_WIDGET;
+			}
+		});
+		SamplePanelRegistry.registerFactory(CATEGORY_WIDGET, new ShowcaseSampleDefinition() {
+
+			public SamplePanel create() {
+				return new LegendDropDownWidgetSample();
+			}
+
+			@Override
+			public String getTitle() {
+				return "LegendDropDownWidget";
+			}
+
+			@Override
+			public String getDescription() {
+				return "Showcase that show the layer legend view.";
+			}
+
+			public String getShortDescription() {
+				return "Showcase that show the layer legend view.";
+			}
+
+			public String getCategory() {
+				return CATEGORY_WIDGET;
+			}
+		});
+	}
+}
