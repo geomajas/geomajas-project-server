31b997255553f9bc3b0bfc2e41e3d72363d8e1a3
Pieter De Graef <pieter.degraef@geosparc.com>
2013-07-16 11:56:01 +0000

    PUREWIDGET-11: Include Showcase in this plugin

---
+++ plugin/geomajas-widget-puregwt-core/example/src/main/java/org/geomajas/puregwt/widget/example/client/Example.java
@@ -0,0 +1,29 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2013 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.puregwt.widget.example.client;
+
+import org.geomajas.puregwt.example.base.client.ExampleBase;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+
+/**
+ * Entry point and main class for core widget example application.
+ * 
+ * @author Pieter De Graef
+ */
+public class Example implements EntryPoint {
+
+	public void onModuleLoad() {
+		RootLayoutPanel.get().add(ExampleBase.getLayout());
+	}
+}
