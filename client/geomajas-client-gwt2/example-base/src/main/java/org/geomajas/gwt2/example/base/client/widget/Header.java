8ac4fd60494132a112feb42f0357250ce81ef736
Pieter De Graef <pieter.degraef@geosparc.com>
2013-10-04 13:00:44 +0000

    Adding GWT client 2.x

---
+++ client/geomajas-client-gwt2/example-base/src/main/java/org/geomajas/gwt2/example/base/client/widget/Header.java
@@ -0,0 +1,39 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2013 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.gwt2.example.base.client.widget;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Page header for this showcase.
+ * 
+ * @author Pieter De Graef
+ */
+public class Header extends Composite {
+
+	/**
+	 * UI binder interface this widget.
+	 * 
+	 * @author Pieter De Graef
+	 */
+	interface MyUiBinder extends UiBinder<Widget, Header> {
+	}
+
+	private static final MyUiBinder UIBINDER = GWT.create(MyUiBinder.class);
+
+	public Header() {
+		initWidget(UIBINDER.createAndBindUi(this));
+	}
+}
