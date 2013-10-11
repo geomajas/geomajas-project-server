5ad9c4072e992c9f21f91254865796a2aea176df
Oliver May <oliver.may@dfc.be>
2013-10-11 18:38:03 +0000

    SMARTSHOW-1: move gwt showcase again

---
+++ application/geomajas-gwt-showcase/src/main/java/org/geomajas/application/gwt/showcase/client/i18n/ShowcaseMessages.java
@@ -0,0 +1,96 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2013 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.application.gwt.showcase.client.i18n;
+
+import com.google.gwt.i18n.client.Messages;
+
+/**
+ * <p>
+ * Localization messages for the GWT showcase.
+ * </p>
+ * <p>
+ * Most messages come from the combined example jars, but these are the messages for the samples which are directly
+ * included.
+ * </p>
+ *
+ * @author Pieter De Graef
+ */
+public interface ShowcaseMessages extends Messages {
+
+	/** @return title for the SampleTree. */
+	String treeTitle();
+
+	String treeGroupSecurity();
+
+	// Login sample:
+
+	String loginTitle();
+
+	String loginDescription();
+
+	String loginSuccess(String userToken);
+
+	String loginFailure();
+
+	String logoutSuccess();
+
+	String logoutFailure();
+
+	// LayerSecurity sample:
+
+	String layerSecurityTitle();
+
+	String layerSecurityDescription();
+
+	String securityLogInWith(String name);
+
+	// FeatureSecurity sample:
+
+	String featureSecurityTitle();
+
+	String featureSecurityDescription();
+
+	// FilterSecurity sample:
+
+	String filterSecurityTitle();
+
+	String filterSecurityDescription();
+
+	// CommandSecurity sample:
+
+	String commandSecurityTitle();
+
+	String commandSecurityDescription();
+
+	// ToolSecurity sample:
+
+	String toolSecurityTitle();
+
+	String toolSecurityDescription();
+
+	// AttributeSecurity sample:
+
+	String attributeSecurityTitle();
+
+	String attributeSecurityDescription();
+
+	String attributeSecurityButtonTitle();
+
+	// layer samples
+
+	String openCycleMapTitle();
+	String openCycleMapDescription();
+
+	String tmsTitle();
+	String tmsDescription();
+
+}
---
+++ application/geomajas-gwt-showcase/src/main/java/org/geomajas/application/gwt/showcase/client/layer/OpenCycleMapSample.java
@@ -0,0 +1,71 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2013 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.application.gwt.showcase.client.layer;
+
+import com.google.gwt.core.client.GWT;
+import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.layout.VLayout;
+import org.geomajas.application.gwt.showcase.client.i18n.ShowcaseMessages;
+import org.geomajas.gwt.client.controller.PanController;
+import org.geomajas.gwt.client.widget.MapWidget;
+import org.geomajas.gwt.example.base.SamplePanel;
+import org.geomajas.gwt.example.base.SamplePanelFactory;
+
+/**
+ * <p>
+ * Sample that shows a map with an OpenStreetMap layer using OpenCycleMap tiles.
+ * </p>
+ *
+ * @author Pieter De Graef
+ * @author Joachim Van der Auwera
+ */
+public class OpenCycleMapSample extends SamplePanel {
+
+	private static final ShowcaseMessages MESSAGES = GWT.create(ShowcaseMessages.class);
+
+	public static final String TITLE = "OpenCycleMap";
+
+	public static final SamplePanelFactory FACTORY = new SamplePanelFactory() {
+
+		public SamplePanel createPanel() {
+			return new OpenCycleMapSample();
+		}
+	};
+
+	public Canvas getViewPanel() {
+		VLayout layout = new VLayout();
+		layout.setWidth100();
+		layout.setHeight100();
+
+		// Map with ID mapOsm is defined in the XML configuration. (mapOsm.xml)
+		final MapWidget map = new MapWidget("mapOpenCycleMap", "gwtShowcase");
+
+		// Set a panning controller on the map:
+		map.setController(new PanController(map));
+		layout.addMember(map);
+		return layout;
+	}
+
+	public String getDescription() {
+		return MESSAGES.openCycleMapDescription();
+	}
+
+	public String[] getConfigurationFiles() {
+		return new String[] {
+				"WEB-INF/mapOpenCycleMap.xml",
+				"WEB-INF/layerOpenCycleMap.xml" };
+	}
+
+	public String ensureUserLoggedIn() {
+		return "luc";
+	}
+}
