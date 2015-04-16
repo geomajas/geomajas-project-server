42b052547564e1402ee550bcf5fb9d13683573bd
Jan De Moerloose <jan.demoerloose@geosparc.com>
2015-04-16 11:49:30 +0200

    GBE-538: fix tests on jenkins, writing images first

--- plugin/rasterizing/rasterizing/src/test/java/org/geomajas/plugin/rasterizing/mvc/TmsControllerTest.java
+++ plugin/rasterizing/rasterizing/src/test/java/org/geomajas/plugin/rasterizing/mvc/TmsControllerTest.java
@@ -38,7 +38,7 @@ public class TmsControllerTest {
 	private static final double DELTA = 0.07;
 	
 	// changing this to true and running the test from the base directory will generate the images !
-	private boolean writeImages = false;
+	private boolean writeImages = true;
 	
 	@Before
 	public void login() {
